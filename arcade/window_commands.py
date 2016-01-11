6922ce3d74a270dd70e4b32de733b1b3e559fb36
Paul Vincent Craven <paul@cravenfamily.com>
2016-01-10 19:32:11 -0600

    Add function for the keyboard handler.

--- arcade/window_commands.py
+++ arcade/window_commands.py
@@ -162,3 +162,9 @@ def set_background_color(color):
         alpha = 255
 
     GL.glClearColor(color[0]/255, color[1]/255, color[2]/255, alpha/255)
+
+def set_keyboard_handler_function(function_pointer):
+    """
+    Sets a function to be run everytime a key even occurs.
+    """
+    GLUT.glutKeyboardFunc(function_pointer)
