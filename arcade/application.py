fb06f43360187f52c96de082916de548fc316296
Paul Vincent Craven <paul@cravenfamily.com>
2016-03-03 06:59:43 -0600

    Fine tune the 'animate' method in the application class.

--- arcade/application.py
+++ arcade/application.py
@@ -9,16 +9,23 @@ class Window(pyglet.window.Window):
 
     >>> import arcade
     >>> window = arcade.Window(800, 600)
-    >>> window.animate()
+    >>> window.animate(0.25)
     >>> window.close()
     """
     def __init__(self, width, height):
         super().__init__(width=width, height=height)
-        self.set_update_rate(1/60)
+        self.set_update_rate(1/80)
         # set_viewport(0, self.width, 0, self.height)
 
-    def animate(self):
-        """ Move everything. """
+    def animate(self, dt):
+        """
+        Move everything.
+
+        Args:
+            :dt (float): Time interval since the last time the function
+            was called.
+
+        """
         pass
 
     def set_update_rate(self, rate):
