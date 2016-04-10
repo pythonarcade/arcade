d0c437d6a2ef5fb2a7ad4de83026aa401c8b1eca
Paul Vincent Craven <paul@cravenfamily.com>
2016-04-10 11:48:19 -0500

    Change tiled example to use common platformer physics engine.

--- doc/source/examples/sprite_tiled_map.py
+++ doc/source/examples/sprite_tiled_map.py
@@ -24,87 +24,6 @@ GRAVITY = 0.5
 window = None
 
 
-class PhysicsEngine():
-    """
-    This class will move everything, and take care of collisions.
-    """
-    def __init__(self, player_sprite, walls):
-        """
-        Constructor.
-        """
-        self.player_sprite = player_sprite
-        self.walls = walls
-
-    def can_jump(self):
-
-        # --- Move in the y direction
-        self.player_sprite.center_y -= 2
-
-        # Check for wall hit
-        hit_list = \
-            arcade.check_for_collision_with_list(self.player_sprite,
-                                                 self.walls)
-
-        result = False
-
-        if len(hit_list) > 0:
-            result = True
-
-        self.player_sprite.center_y += 2
-        return result
-
-    def update(self):
-        """
-        Move everything and resolve collisions.
-        """
-
-        # Add gravity
-        self.player_sprite.change_y -= GRAVITY
-
-        # --- Move in the x direction
-        self.player_sprite.center_x += self.player_sprite.change_x
-
-        # Check for wall hit
-        hit_list = \
-            arcade.check_for_collision_with_list(self.player_sprite,
-                                                 self.walls)
-
-        # If we hit a wall, move so the edges are at the same point
-        if len(hit_list) > 0:
-            if self.player_sprite.change_x > 0:
-                for item in hit_list:
-                    self.player_sprite.right = min(item.left,
-                                                   self.player_sprite.right)
-            elif self.player_sprite.change_x < 0:
-                for item in hit_list:
-                    self.player_sprite.left = max(item.right,
-                                                  self.player_sprite.left)
-            else:
-                print("Error, collision while player wasn't moving.")
-
-        # --- Move in the y direction
-        self.player_sprite.center_y += self.player_sprite.change_y
-
-        # Check for wall hit
-        hit_list = \
-            arcade.check_for_collision_with_list(self.player_sprite,
-                                                 self.walls)
-
-        # If we hit a wall, move so the edges are at the same point
-        if len(hit_list) > 0:
-            if self.player_sprite.change_y > 0:
-                for item in hit_list:
-                    self.player_sprite.top = min(item.bottom,
-                                                 self.player_sprite.top)
-            elif self.player_sprite.change_y < 0:
-                for item in hit_list:
-                    self.player_sprite.bottom = max(item.top,
-                                                    self.player_sprite.bottom)
-            else:
-                print("Error, collision while player wasn't moving.")
-            self.player_sprite.change_y = 0
-
-
 def get_map():
     map_file = open("map.csv")
     map_array = []
@@ -160,7 +79,10 @@ class MyApplication(arcade.Window):
                 self.all_sprites_list.append(wall)
                 self.wall_list.append(wall)
 
-        self.physics_engine = PhysicsEngine(self.player_sprite, self.wall_list)
+        self.physics_engine = \
+            arcade.PhysicsEnginePlatformer(self.player_sprite,
+                                           self.wall_list,
+                                           gravity_constant=GRAVITY)
 
         # Set the background color
         arcade.set_background_color(arcade.color.AMAZON)
