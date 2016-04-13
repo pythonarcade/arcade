f4c9c8275aa0bbdf265c373016aa08ee28d4bf64
Paul Vincent Craven <paul.craven@simpson.edu>
2016-04-13 11:44:21 -0500

    Fix missing file in moving platforms example documentation.

--- arcade/version.py
+++ arcade/version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-BUILD = 29
+BUILD = 31
 VERSION = "0.0.6"
 RELEASE = VERSION + "a" + str(BUILD)
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-BUILD = 29
+BUILD = 31
 VERSION = "0.0.6"
 RELEASE = VERSION + "a" + str(BUILD)
 
