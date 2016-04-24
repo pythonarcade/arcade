f483a45e18e45e4af1167f3ece5f34f5cfb64619
Paul Vincent Craven <paul@cravenfamily.com>
2016-04-24 15:01:04 -0500

    Update version number

--- arcade/version.py
+++ arcade/version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-BUILD = 29
+BUILD = 30
 VERSION = "0.0.6"
 RELEASE = VERSION + "a" + str(BUILD)
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-BUILD = 29
+BUILD = 30
 VERSION = "0.0.6"
 RELEASE = VERSION + "a" + str(BUILD)
 
