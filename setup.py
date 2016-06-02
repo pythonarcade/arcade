5ba3c8d1e45bd95ab582ef1a713cf25d448a458c
unknown <craven@CVR1834.sc.loc>
2016-06-02 13:49:58 -0700

    Remove Python 2.7 build from AppVeyor.yml and setup.py. Going 3.5+ only.

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ if __name__ == "__main__":
               "License :: OSI Approved :: MIT License",
               "Operating System :: OS Independent",
               "Programming Language :: Python",
-              "Programming Language :: Python :: 2.7",
               "Programming Language :: Python :: 3.5",
               "Programming Language :: Python :: Implementation :: CPython",
               "Topic :: Software Development :: Libraries :: Python Modules",
