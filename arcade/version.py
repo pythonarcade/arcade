a8e8e16cfa02dd3419b522b9c758cfc65c239948
Paul Vincent Craven <paul.craven@simpson.edu>
2017-03-14 12:08:43 -0500

    Update version number

--- arcade/version.py
+++ arcade/version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-BUILD = 6
+BUILD = 7
 VERSION = "1.0.1"
 RELEASE = VERSION + ".post" + str(BUILD)
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-BUILD = 6
+BUILD = 7
 VERSION = "1.0.1"
 RELEASE = VERSION + ".post" + str(BUILD)
 
