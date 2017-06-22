02738ecf1ac427caf322761b95248907de57faaa
Paul Vincent Craven <paul@cravenfamily.com>
2017-06-22 10:15:39 -0500

    Update thumbnail batch file to use newer image magick commandline method.

--- doc/examples/update_thumbnails.bat
+++ doc/examples/update_thumbnails.bat
@@ -1,3 +1,3 @@
-mogrify -resize 200x158 -extent 200x158 -background transparent -path thumbs *.png
-mogrify -resize 200x158 -extent 200x158 -background transparent -path thumbs *.gif
+magick mogrify -resize 200x158 -extent 200x158 -background transparent -path thumbs *.png
+magick mogrify -resize 200x158 -extent 200x158 -background transparent -path thumbs *.gif
 
