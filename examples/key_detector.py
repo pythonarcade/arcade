600cbd2d8e68611868667fb2a6b566e3fceb3023
Stefan Rothe <stefan.rothe@gymkirchenfeld.ch>
2017-11-19 12:13:32 +0100

    Fixed key_detector example.

--- examples/key_detector.py
+++ examples/key_detector.py
@@ -35,16 +35,23 @@ def load_arcade_key_dict():
 class MyWindow(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
-        self.key_dict = load_arcade_key_dict()
+        self._key_dict = load_arcade_key_dict()
+        self._pressed_keys = set()
         arcade.set_background_color(BACKGROUND_COLOR)
 
 
+    def on_key_press(self, key, modifiers):
+        self._pressed_keys.add(key)
+        
+    def on_key_release(self, key, modifiers):
+        self._pressed_keys.remove(key)
+        
     def on_draw(self):
         arcade.start_render()
         y = SCREEN_HEIGHT - LINE_HEIGHT
-        for pressed_key in self.pressed_keys:
-            if pressed_key in self.key_dict:
-                text = self.key_dict[pressed_key]
+        for pressed_key in self._pressed_keys:
+            if pressed_key in self._key_dict:
+                text = self._key_dict[pressed_key]
             else:
                 text = 'unknown key (' + str(pressed_key) + ')'
 
