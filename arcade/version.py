bb1613924bb501a92e5b0092d042c720c16eacda
Paul Vincent Craven <paul@cravenfamily.com>
2017-12-23 11:32:31 -0600

    Update release number to 1.2.4.

--- arcade/version.py
+++ arcade/version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 BUILD = 0
-VERSION = "1.2.3"
+VERSION = "1.2.4"
 RELEASE = VERSION + "." + str(BUILD)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 BUILD = 0
-VERSION = "1.2.3"
+VERSION = "1.2.4"
 RELEASE = VERSION + "." + str(BUILD)
 
 from os import path
