4997b1e9353209a107ad5d7f98e51ee7377b0e21
Paul Vincent Craven <paul@cravenfamily.com>
2018-01-03 10:29:37 -0600

    Update version number to 1.3.0a2

--- arcade/version.py
+++ arcade/version.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
 BUILD = 0
-VERSION = "1.3.0a1"
+VERSION = "1.3.0a2"
 RELEASE = VERSION
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 BUILD = 0
-VERSION = "1.3.0a1"
+VERSION = "1.3.0a2"
 RELEASE = VERSION
 
 from os import path
