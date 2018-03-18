1f98ae556e8b0451275a9acce922093e6bc328a0
Paul Vincent Craven <paul@cravenfamily.com>
2018-03-18 16:31:41 -0500

    Work on fixing UNIX sound error during build

--- arcade/sound.py
+++ arcade/sound.py
@@ -44,29 +44,18 @@ def _playsoundWin(sound):
     winCommand(f'play {alias} from 0 to {durationInMS.decode()}')
 
 def _playsoundOSX(sound):
-    '''
-    Utilizes AppKit.NSSound. Tested and known to work with MP3 and WAVE on
-    OS X 10.11 with Python 2.7. Probably works with anything QuickTime supports.
-    Probably works on OS X 10.5 and newer. Probably works with all versions of
-    Python.
-    Inspired by (but not copied from) Aaron's Stack Overflow answer here:
-    http://stackoverflow.com/a/34568298/901641
-    I never would have tried using AppKit.NSSound without seeing his code.
-    '''
-    from AppKit     import NSSound
-    from Foundation import NSURL
-    from time       import sleep
+    import NSURL
+    import NSSound
 
-    if '://' not in sound:
+    if '://' in sound:
+        url = NSURL.URLWithString_(sound)  # don't think this works
+    else:
         if not sound.startswith('/'):
             from os import getcwd
             sound = getcwd() + '/' + sound
-        sound = 'file://' + sound
-    url   = NSURL.URLWithString_(sound)
+        url = NSURL.fileURLWithPath_(sound)  # this seems to work
+
     nssound = NSSound.alloc().initWithContentsOfURL_byReference_(url, True)
-    if not nssound:
-        raise IOError('Unable to load sound named: ' + sound)
-    nssound.play()
 
 def _playsoundNix(sound):
     """Play a sound using GStreamer.
@@ -75,38 +64,38 @@ def _playsoundNix(sound):
     """
     # pathname2url escapes non-URL-safe characters
 
+    # try:
+    import os
     try:
-        import os
-        try:
-            from urllib.request import pathname2url
-        except ImportError:
-            # python 2
-            from urllib import pathname2url
+        from urllib.request import pathname2url
+    except ImportError:
+        # python 2
+        from urllib import pathname2url
 
-        import gi
-        gi.require_version('Gst', '1.0')
-        from gi.repository import Gst
+    import gi
+    gi.require_version('Gst', '1.0')
+    from gi.repository import Gst
 
-        Gst.init(None)
+    Gst.init(None)
 
-        playbin = Gst.ElementFactory.make('playbin', 'playbin')
-        if sound.startswith(('http://', 'https://')):
-            playbin.props.uri = sound
-        else:
-            playbin.props.uri = 'file://' + pathname2url(os.path.abspath(sound))
+    playbin = Gst.ElementFactory.make('playbin', 'playbin')
+    if sound.startswith(('http://', 'https://')):
+        playbin.props.uri = sound
+    else:
+        playbin.props.uri = 'file://' + pathname2url(os.path.abspath(sound))
 
-        set_result = playbin.set_state(Gst.State.PLAYING)
-        if set_result != Gst.StateChangeReturn.ASYNC:
-            raise PlaysoundException(
-                "playbin.set_state returned " + repr(set_result))
+    set_result = playbin.set_state(Gst.State.PLAYING)
+    if set_result != Gst.StateChangeReturn.ASYNC:
+        raise PlaysoundException(
+            "playbin.set_state returned " + repr(set_result))
 
-        # FIXME: use some other bus method than poll() with block=False
-        # https://lazka.github.io/pgi-docs/#Gst-1.0/classes/Bus.html
-        bus = playbin.get_bus()
-        bus.poll(Gst.MessageType.EOS, Gst.CLOCK_TIME_NONE)
-        playbin.set_state(Gst.State.NULL)
-    except:
-        print("Error playing sound.")
+    # FIXME: use some other bus method than poll() with block=False
+    # https://lazka.github.io/pgi-docs/#Gst-1.0/classes/Bus.html
+    bus = playbin.get_bus()
+    bus.poll(Gst.MessageType.EOS, Gst.CLOCK_TIME_NONE)
+    playbin.set_state(Gst.State.NULL)
+    # except:
+    #     print("Error playing sound.")
 
 
 from platform import system
