af96c48096bd1949a0d10bf1177cbbca64d89e41
Paul Vincent Craven <paul@cravenfamily.com>
2018-05-05 10:13:18 -0500

    #184, work on sound support. Specifically for UNIX.

--- arcade/sound.py
+++ arcade/sound.py
@@ -91,8 +91,44 @@ def _playsound_osx(sound):
 
 
 def _playsound_unix(sound):
-    player = pyglet.media.load(sound)
-    player.play()
+    """Play a sound using GStreamer.
+    Inspired by this:
+    https://gstreamer.freedesktop.org/documentation/tutorials/playback/playbin-usage.html
+    """
+    # pathname2url escapes non-URL-safe characters
+
+    try:
+        import os
+        try:
+            from urllib.request import pathname2url
+        except ImportError:
+            # python 2
+            from urllib import pathname2url
+
+        import gi
+        gi.require_version('Gst', '1.0')
+        from gi.repository import Gst
+
+        Gst.init(None)
+
+        playbin = Gst.ElementFactory.make('playbin', 'playbin')
+        if sound.startswith(('http://', 'https://')):
+            playbin.props.uri = sound
+        else:
+            playbin.props.uri = 'file://' + pathname2url(os.path.abspath(sound))
+
+        set_result = playbin.set_state(Gst.State.PLAYING)
+        if set_result != Gst.StateChangeReturn.ASYNC:
+            raise PlaysoundException(
+                "playbin.set_state returned " + repr(set_result))
+
+        # FIXME: use some other bus method than poll() with block=False
+        # https://lazka.github.io/pgi-docs/#Gst-1.0/classes/Bus.html
+        bus = playbin.get_bus()
+        bus.poll(Gst.MessageType.EOS, Gst.CLOCK_TIME_NONE)
+        playbin.set_state(Gst.State.NULL)
+    except:
+        print("Error playing sound.")
 
 
 def _load_sound_other(filename: str) -> typing.Any:
