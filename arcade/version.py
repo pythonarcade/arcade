7b9b865008896a2f39c473459b7bea6d94ba769a
Paul Vincent Craven <paul@cravenfamily.com>
2018-05-14 10:26:33 -0500

    Update version number

--- arcade/version.py
+++ arcade/version.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
 BUILD = 0
-VERSION = "1.3.3"
+VERSION = "1.3.4"
 RELEASE = VERSION
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 BUILD = 0
-VERSION = "1.3.3"
+VERSION = "1.3.4"
 RELEASE = VERSION
 
 from os import path
