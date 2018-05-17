c505c74db05c6e19bd6aa22ab4ffd642cb71bb74
Paul Everitt <pauleveritt@me.com>
2018-05-17 13:18:16 -0400

    For now, run the doctests separate from the unit tests.

---
+++ tests/unit/test_sound.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+def test_import(mock_window):
+    import arcade
+    assert 'loadsound' in arcade.load_sound.__name__
+
+
+@pytest.mark.not_ci
+@pytest.mark.parametrize('sound_format', ('wav', 'mp3', 'ogg'))
+def test_play_sound(mock_window, sound_format):
+    import arcade
+    fn = 'tests/unit/laser1.' + sound_format
+    my_sound = arcade.load_sound(fn)
+    arcade.play_sound(my_sound)
