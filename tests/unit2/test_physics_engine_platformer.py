4927a5589d12ac35c77e868f2957b1d5bbb69b1d
Paul Vincent Craven <paul@cravenfamily.com>
2018-11-06 11:17:51 -0600

    Work at fixing pytest unit tests

--- tests/unit2/test_physics_engine_platformer.py
+++ tests/unit2/test_physics_engine_platformer.py
@@ -5,6 +5,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 LINE_HEIGHT = 20
 CHARACTER_SCALING = 0.5
+GRAVITY = 0.5
 
 
 class MyTestWindow(arcade.Window):
@@ -19,19 +20,18 @@ class MyTestWindow(arcade.Window):
 
         self.character_list = arcade.SpriteList()
         self.character_sprite = arcade.Sprite("../../arcade/examples/images/character.png", CHARACTER_SCALING)
-        self.character_sprite.center_x = 50
-        self.character_sprite.center_y = 50
-        self.character_sprite.change_x = 5
-        self.character_sprite.change_y = 5
+        self.character_sprite.center_x = 150
+        self.character_sprite.center_y = 110
         self.character_list.append(self.character_sprite)
 
         self.wall_list = arcade.SpriteList()
-        sprite = arcade.Sprite("../../arcade/examples/images/boxCrate_double.png", CHARACTER_SCALING)
-        sprite.position = (130, 130)
-        sprite.angle = 90
-        self.wall_list.append(sprite)
+        for x in range(0, 1200, 64):
+            sprite = arcade.Sprite("../../arcade/examples/images/boxCrate_double.png", CHARACTER_SCALING)
+            sprite.center_x = x
+            sprite.center_y = 32
+            self.wall_list.append(sprite)
 
-        self.physics_engine = arcade.PhysicsEngineSimple(self.character_sprite, self.wall_list)
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.character_sprite, self.wall_list, gravity_constant=GRAVITY)
 
     def on_draw(self):
         arcade.start_render()
@@ -40,6 +40,8 @@ class MyTestWindow(arcade.Window):
 
     def update(self, delta_time):
         self.physics_engine.update()
+        can_jump = self.physics_engine.can_jump()
+        # print(can_jump)
 
 
 def test_main():
