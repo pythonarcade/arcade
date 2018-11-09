8a295524e17942722c9dafcc6b54a391aaa0cec6
Paul Vincent Craven <paul@cravenfamily.com>
2018-11-08 18:51:22 -0600

    Fix spelling error in gradients.

--- arcade/examples/gradients.py
+++ arcade/examples/gradients.py
@@ -5,7 +5,7 @@ python -m arcade.examples.gradients
 """
 import arcade
 
-# Do the math to figure out oiur screen dimensions
+# Do the math to figure out our screen dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
