038dc8b994a4d87f0a233ad2f3e9259af808d62d
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2019-04-20 18:17:07 -0400

    use pathlib for directory changing

--- tests/unit2/test_sound.py
+++ tests/unit2/test_sound.py
@@ -1,6 +1,7 @@
 import arcade
 import os
 
+from pathlib import Path
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -19,7 +20,7 @@ class MyGame(arcade.Window):
         # code, but it is needed to easily run the examples using "python -m"
         # as mentioned at the top of this program.
         file_path = os.path.dirname(os.path.abspath(__file__))
-        os.chdir(f"{file_path}\\..\\..\\arcade\\examples")
+        os.chdir(file_path / Path("../../arcade/examples"))
 
         arcade.set_background_color(arcade.color.WHITE)
         self.laser_wav = arcade.load_sound("sounds/laser1.wav")
