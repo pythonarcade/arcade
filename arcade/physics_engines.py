35f8b3f93c88d40d9adf8a2f821e8b14a87ea84f
Caleb Cameron <ccameron@linkedin.com>
2019-05-01 14:51:44 -0400

    Add platforming multi-jump support

--- arcade/physics_engines.py
+++ arcade/physics_engines.py
@@ -83,6 +83,9 @@ class PhysicsEnginePlatformer:
         self.player_sprite = player_sprite
         self.platforms = platforms
         self.gravity_constant = gravity_constant
+        self.jumps_since_ground = 0
+        self.allowed_jumps = 1
+        self.support_multi_jump = False
 
     def can_jump(self) -> bool:
         """
@@ -100,10 +103,22 @@ class PhysicsEnginePlatformer:
 
         self.player_sprite.center_y += 2
 
-        if len(hit_list) > 0:
+        if len(hit_list) > 0 or (self.allow_multi_jump == True and
+                                 self.jumps_since_ground < self.allowed_jumps):
             return True
         else:
             return False
+    
+    def allow_multi_jump(self, allowed_jumps: int):
+        self.allowed_jumps = allowed_jumps
+        self.allow_multi_jump = True
+        
+    def increment_jump_counter(self):
+        """
+        Updates the jump counter for double-jump tracking
+        """
+        if self.allow_multi_jump:
+            self.jumps_since_ground += 1
 
     def update(self):
         """
@@ -128,6 +143,8 @@ class PhysicsEnginePlatformer:
                                                  self.player_sprite.top)
                 # print(f"Spot X ({self.player_sprite.center_x}, {self.player_sprite.center_y})")
             elif self.player_sprite.change_y < 0:
+                # Reset number of jumps
+                self.jumps_since_ground = 0
                 for item in hit_list:
                     while check_for_collision(self.player_sprite, item):
                         # self.player_sprite.bottom = item.top <- Doesn't work for ramps
