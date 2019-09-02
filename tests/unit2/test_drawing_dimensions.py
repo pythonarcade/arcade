d5eb168ab7db60a2798ae0a1c5f66f18ecd97e41
Paul Vincent Craven <paul@cravenfamily.com>
2019-09-02 13:38:25 -0500

    Fix for issue #479, wrong dimensions for ellipses

---
+++ tests/unit2/test_drawing_dimensions.py
@@ -0,0 +1,74 @@
+import arcade
+import os
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self, width, height):
+        """
+        Initializer
+        """
+        super().__init__(width, height)
+
+        # Set the working directory (where we expect to find files) to the same
+        # directory this .py file is in. You can leave this out of your own
+        # code, but it is needed to easily run the examples using "python -m"
+        # as mentioned at the top of this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        new_path = os.path.join(file_path, '..', '..', 'arcade', 'examples')
+        os.chdir(new_path)
+
+        arcade.set_background_color(arcade.color.WHITE)
+
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # Start the render process. This must be done before any drawing commands.
+        arcade.start_render()
+
+        radius = 50
+        width = radius * 2
+        x = 200
+        y = 100
+        arcade.draw_rectangle_outline(x, y, width, width, arcade.color.BLACK, 2)
+        arcade.draw_circle_outline(x, y, radius, arcade.color.BLACK, 2)
+        arcade.draw_line(x - radius, y, x + radius, y, arcade.color.BLACK, 2)
+
+        x = 200
+        y = 300
+        width = 150
+        half_width = width / 2
+        arcade.draw_rectangle_outline(x, y, width, 50, arcade.color.BLACK, 2)
+        arcade.draw_ellipse_outline(x, y, width, 50, arcade.color.AFRICAN_VIOLET, 2)
+        arcade.draw_line(x - half_width, y, x + half_width, y, arcade.color.RED, 2)
+
+        radius = 50
+        width = radius * 2
+        x = 400
+        y = 100
+        arcade.draw_rectangle_outline(x, y, width, width, arcade.color.BLACK, 2)
+        arcade.draw_circle_filled(x, y, radius, arcade.color.AFRICAN_VIOLET)
+        arcade.draw_line(x - radius, y, x + radius, y, arcade.color.BLACK, 2)
+
+        x = 400
+        y = 300
+        width = 150
+        half_width = width / 2
+        arcade.draw_rectangle_outline(x, y, width, 50, arcade.color.BLACK, 2)
+        arcade.draw_ellipse_filled(x, y, width, 50, arcade.color.AFRICAN_VIOLET, 2)
+        arcade.draw_line(x - half_width, y, x + half_width, y, arcade.color.RED, 2)
+
+
+
+def test_main():
+    """ Main method """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window.test(100)
+    window.close()
