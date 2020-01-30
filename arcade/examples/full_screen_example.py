6debc428514f677d1f03f89216622eecc1ce82e3
Paul Vincent Craven <paul@cravenfamily.com>
2020-01-30 14:03:52 -0600

    Fix texture issue with full screen example.

--- arcade/examples/full_screen_example.py
+++ arcade/examples/full_screen_example.py
@@ -73,7 +73,9 @@ class MyGame(arcade.Window):
         # Draw some boxes on the bottom so we can see how they change
         for x in range(64, 800, 128):
             y = 64
-            arcade.draw_texture_rectangle(x, y, self.example_image)
+            width = 128
+            height = 128
+            arcade.draw_texture_rectangle(x, y, width, height, self.example_image)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
