81b3363d40f2ef40b0f84986ad5ae67135232977
Paul Vincent Craven <paul@cravenfamily.com>
2020-03-01 18:24:03 -0600

    Work on audio streaming support, and sound demo code.

--- arcade/sound.py
+++ arcade/sound.py
@@ -2,7 +2,6 @@
 Sound library.
 """
 
-import pyglet
 from pathlib import Path
 import soloud.soloud as soloud
 
@@ -34,10 +33,44 @@ class Sound:
     def play(self, volume=1.0, pan=0.0):
 
         _audiolib.play(self.wav_file,
-                       aVolume = 1.0,
-                       aPan = 0.0,
+                       aVolume = volume,
+                       aPan = pan,
                        aPaused = 0,
                        aBus = 0)
+
+class AudioStream:
+
+    def __init__(self, file_name: str):
+        # If we should pull from local resources, replace with proper path
+        if file_name.startswith(":resources:"):
+            import os
+            path = os.path.dirname(os.path.abspath(__file__))
+            file_name = f"{path}/resources/{file_name[11:]}"
+
+        if not Path(file_name).is_file():
+            raise FileNotFoundError(f"The sound file '{file_name}' is not a file or can't be read")
+        self.file_name = file_name
+        self.wav_file = soloud.WavStream()
+        self.wav_file.load(self.file_name)
+        print(self.wav_file.get_length())
+        self.handle = None
+
+    def play(self, volume=1.0, pan=0.0):
+        self.wav_file.stop()
+        self.handle = _audiolib.play(self.wav_file,
+                                     aVolume = volume,
+                                     aPan = pan,
+                                     aPaused = 0,
+                                     aBus = 0)
+
+    def stop(self):
+        # _audiolib.stop(self.wav_file)
+        self.wav_file.stop()
+
+    def get_length(self):
+        # if self.handle:
+        #     return _audiolib.get_stream_time(self.wav_file.objhandle)
+        return self.wav_file.get_length()
 
 class PlaysoundException(Exception):
     pass
