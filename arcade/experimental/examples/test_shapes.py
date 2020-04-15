d74adb7c2dd2b4ebd782edc980ba739bdb647596
Einar Forselv <eforselv@gmail.com>
2020-04-15 02:54:11 +0200

    Update shape testing script

--- arcade/experimental/examples/test_shapes.py
+++ arcade/experimental/examples/test_shapes.py
@@ -1,7 +1,7 @@
 """
 This is for testing geometry shader shapes. Please keep.
 """
-
+import time
 import random
 import arcade
 from pyglet import gl
@@ -10,12 +10,16 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 TTTLE = "Line Test"
 
+# 16.04
+# 0.004
+
 
 class TestWindow(arcade.Window):
 
     def __init__(self, width, height, title):
         super().__init__(800, 600, "Test", antialiasing=True, resizable=True, fullscreen=False)
-        self.lines = [
+        # Single lines
+        self.single_lines_calls = [
             (
                 random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT),
                 random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT),
@@ -28,16 +32,36 @@ class TestWindow(arcade.Window):
             )
             for _ in range(600)
         ]
+        # Line list
+        self.line_list = [(random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)) for _ in range(2 * 5000)]
+
+        self.frames = 0
+        self.execution_time = 0
 
     def on_draw(self):
         self.clear()
         # arcade.draw_point(155, 150, arcade.color.WHITE, 4)
         # arcade.draw_circle_filled(400, 300, 250, arcade.color.GREEN)
-        for l in self.lines:
-            arcade.draw_line(l[0], l[1], l[2], l[3], l[4], 10)
+
+        # Single lines
+        # for l in self.single_lines_calls:
+        #     arcade.draw_line(l[0], l[1], l[2], l[3], l[4], 10)
+
+        start = time.time()
+        arcade.draw_lines(self.line_list, arcade.color.AERO_BLUE)
+        self.execution_time += time.time() - start
+        self.frames += 1
+
+        if self.execution_time > 1.0 and self.frames > 0:
+            print(self.frames, round(self.execution_time, 3), round(self.execution_time / self.frames, 3))
+            self.execution_time = 0
+            self.frames = 0
 
     def on_resize(self, width, height):
         gl.glViewport(0, 0, *self.get_framebuffer_size())
+
+    def on_update(self, dt):
+        pass
 
 
 if __name__ == '__main__':
