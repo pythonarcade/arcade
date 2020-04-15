47da92658a975906335cbb05d75f56a7485b8ebf
Einar Forselv <eforselv@gmail.com>
2020-04-16 00:18:29 +0200

    Bug: Multisampling did not work on windows

--- arcade/__init__.py
+++ arcade/__init__.py
@@ -9,6 +9,7 @@ A Python simple, easy to use module for creating 2D games.
 
 # Error out if we import Arcade with an incompatible version of Python.
 import sys
+import platform
 
 if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 6):
     sys.exit("The Arcade Library requires Python 3.6 or higher.")
@@ -16,7 +17,8 @@ if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] 
 # noinspection PyPep8
 import pyglet
 
-pyglet.options['shadow_window'] = False
+if platform.system() != 'Windows':
+    pyglet.options['shadow_window'] = False
 
 # noinspection PyPep8
 from arcade import color
