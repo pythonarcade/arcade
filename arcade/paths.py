15b8ff1a97bd21e16af65a002ce88891cb387e7e
Paul Vincent Craven <paul@cravenfamily.com>
2020-04-17 16:05:54 -0500

    Support max_distance parameter

--- arcade/paths.py
+++ arcade/paths.py
@@ -11,6 +11,7 @@ from arcade import get_sprites_at_point
 def has_line_of_sight(point_1: Point,
                       point_2: Point,
                       walls: SpriteList,
+                      max_distance: int = -1,
                       check_resolution: int = 2):
     """
     Determine if we have line of sight between two points. Try to make sure
@@ -20,6 +21,7 @@ def has_line_of_sight(point_1: Point,
     :param Point point_1: Start position
     :param Point point_2: End position position
     :param SpriteList walls: List of all blocking sprites
+    :param int max_distance: Max distance point 1 can see
     :param int check_resolution: Check every x pixels for a sprite. Trade-off
                                  between accuracy and speed.
     """
@@ -29,6 +31,8 @@ def has_line_of_sight(point_1: Point,
     for step in range(steps + 1):
         u = step / distance * check_resolution
         midpoint = lerp_vec(point_1, point_2, u)
+        if max_distance != -1 and step > max_distance:
+            return True
         # print(p1, p2, step, midpoint)
         sprite_list = get_sprites_at_point(midpoint, walls)
         if len(sprite_list) > 0:
