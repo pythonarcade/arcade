18dd3f37dfcd2352a95e44f476788a3aabc26737
Paul Vincent Craven <paul@cravenfamily.com>
2020-04-28 11:43:03 -0500

    Separate out the creation of an image with text on it, into a separate function.

--- arcade/text.py
+++ arcade/text.py
@@ -99,6 +99,114 @@ def render_text(text: CreateText, start_x: float, start_y: float):
               anchor_y=text.anchor_y,
               rotation=text.rotation)
 
+def get_text_image(text: str,
+                   color: Color,
+                   font_size: float = 12,
+                   width: int = 0,
+                   align: str = "left",
+                   font_name: Union[str, Tuple[str, ...]] = ('calibri', 'arial'),
+                   ):
+    # Scale the font up, so it matches with the sizes of the old code back
+    # when Pyglet drew the text.
+    font_size *= 1.25
+
+    # Text isn't anti-aliased, so we'll draw big, and then shrink
+    scale_up = 2
+    scale_down = 2
+
+    font_size *= scale_up
+
+    # Figure out the font to use
+    font = None
+
+    # Font was specified with a string
+    if isinstance(font_name, str):
+        font_name = font_name,
+
+    font_names = chain(*[
+        [font_string_name, f"{font_string_name}.ttf"]
+        for font_string_name in font_name
+    ], DEFAULT_FONT_NAMES)
+
+    font_found = False
+    for font_string_name in font_names:
+        try:
+            font = PIL.ImageFont.truetype(font_string_name, int(font_size))
+        except OSError:
+            continue
+        else:
+            font_found = True
+            break
+
+    if not font_found:
+        try:
+            import pyglet.font
+            font_config = pyglet.font.fontconfig.get_fontconfig()
+            result = font_config.find_font('Arial')
+            font = PIL.ImageFont.truetype(result.name, int(font_size))
+        except Exception:
+            # NOTE: Will catch OSError from loading font and missing fontconfig in pyglet
+            pass
+        else:
+            font_found = True
+
+    # Final fallback just getting PIL's default font if possible
+    if not font_found:
+        try:
+            font = PIL.ImageFont.load_default()
+            font_found = True
+        except Exception:
+            pass
+
+    if not font_found:
+        raise RuntimeError("Unable to find a default font on this system. Please specify an available font.")
+
+    # This is stupid. We have to have an image to figure out what size
+    # the text will be when we draw it. Of course, we don't know how big
+    # to make the image. Catch-22. So we just make a small image we'll trash
+    text_image_size = (10, 10)
+    image = PIL.Image.new("RGBA", text_image_size)
+    draw = PIL.ImageDraw.Draw(image)
+
+    # Get size the text will be
+    text_image_size = draw.multiline_textsize(text, font=font)
+    # Add some extra pixels at the bottom to account for letters that drop below the baseline.
+    text_image_size = text_image_size[0], text_image_size[1] + int(font_size * 0.25)
+
+    # Create image of proper size
+    text_height = text_image_size[1]
+    text_width = text_image_size[0]
+
+    image_start_x = 0
+    if width == 0:
+        width = text_image_size[0]
+    else:
+        # Wait! We were given a field width.
+        if align == "center":
+            # Center text on given field width
+            field_width = width * scale_up
+            text_image_size = field_width, text_height
+            image_start_x = (field_width - text_width) // 2
+            width = field_width
+        else:
+            image_start_x = 0
+
+    # Find y of top-left corner
+    image_start_y = 0
+
+    # Create image
+    image = PIL.Image.new("RGBA", text_image_size)
+    draw = PIL.ImageDraw.Draw(image)
+
+    # Convert to tuple if needed, because the multiline_text does not take a
+    # list for a color
+    if isinstance(color, list):
+        color = cast(RGBA, tuple(color))
+    draw.multiline_text((image_start_x, image_start_y), text, color, align=align, font=font)
+    image = image.resize((max(1, width // scale_down), text_height // scale_down), resample=PIL.Image.LANCZOS)
+    return image
+
+
 
 def draw_text(text: str,
               start_x: float,
@@ -144,16 +252,6 @@ def draw_text(text: str,
     """
     global draw_text_cache
 
-    # Scale the font up, so it matches with the sizes of the old code back
-    # when Pyglet drew the text.
-    font_size *= 1.25
-
-    # Text isn't anti-aliased, so we'll draw big, and then shrink
-    scale_up = 2
-    scale_down = 2
-
-    font_size *= scale_up
-
     # If the cache gets too large, dump it and start over.
     if len(draw_text_cache) > 5000:
         draw_text_cache = {}
@@ -162,100 +260,12 @@ def draw_text(text: str,
     cache_color = f"{r}{g}{b}"
 
     key = f"{text}{cache_color}{font_size}{width}{align}{font_name}{bold}{italic}"
+
     try:
         label = draw_text_cache[key]
     except KeyError:  # doesn't exist, create it
-        label = Text()
 
-        # Figure out the font to use
-        font = None
-
-        # Font was specified with a string
-        if isinstance(font_name, str):
-            font_name = font_name,
-
-        font_names = chain(*[
-            [font_string_name, f"{font_string_name}.ttf"]
-            for font_string_name in font_name
-        ], DEFAULT_FONT_NAMES)
-
-        font_found = False
-        for font_string_name in font_names:
-            try:
-                font = PIL.ImageFont.truetype(font_string_name, int(font_size))
-            except OSError:
-                continue
-            else:
-                font_found = True
-                break
-
-        if not font_found:
-            try:
-                import pyglet.font
-                font_config = pyglet.font.fontconfig.get_fontconfig()
-                result = font_config.find_font('Arial')
-                font = PIL.ImageFont.truetype(result.name, int(font_size))
-            except Exception:
-                # NOTE: Will catch OSError from loading font and missing fontconfig in pyglet
-                pass
-            else:
-                font_found = True
-
-        # Final fallback just getting PIL's default font if possible
-        if not font_found:
-            try:
-                font = PIL.ImageFont.load_default()
-                font_found = True
-            except Exception:
-                pass
-
-        if not font_found:
-            raise RuntimeError("Unable to find a default font on this system. Please specify an available font.")
-
-        # This is stupid. We have to have an image to figure out what size
-        # the text will be when we draw it. Of course, we don't know how big
-        # to make the image. Catch-22. So we just make a small image we'll trash
-        text_image_size = (10, 10)
-        image = PIL.Image.new("RGBA", text_image_size)
-        draw = PIL.ImageDraw.Draw(image)
-
-        # Get size the text will be
-        text_image_size = draw.multiline_textsize(text, font=font)
-        # Add some extra pixels at the bottom to account for letters that drop below the baseline.
-        text_image_size = text_image_size[0], text_image_size[1] + int(font_size * 0.25)
-
-        # Create image of proper size
-        text_height = text_image_size[1]
-        text_width = text_image_size[0]
-
-        image_start_x = 0
-        if width == 0:
-            width = text_image_size[0]
-        else:
-            # Wait! We were given a field width.
-            if align == "center":
-                # Center text on given field width
-                field_width = width * scale_up
-                text_image_size = field_width, text_height
-                image_start_x = (field_width - text_width) // 2
-                width = field_width
-            else:
-                image_start_x = 0
-
-        # Find y of top-left corner
-        image_start_y = 0
-
-        # Create image
-        image = PIL.Image.new("RGBA", text_image_size)
-        draw = PIL.ImageDraw.Draw(image)
-
-        # Convert to tuple if needed, because the multiline_text does not take a
-        # list for a color
-        if isinstance(color, list):
-            color = cast(RGBA, tuple(color))
-        draw.multiline_text((image_start_x, image_start_y), text, color, align=align, font=font)
-        image = image.resize((max(1, width // scale_down), text_height // scale_down), resample=PIL.Image.LANCZOS)
-
+        image = get_text_image(text, color, font_size, width, align, font_name)
         text_sprite = Sprite()
         text_sprite._texture = Texture(key)
         text_sprite.texture.image = image
@@ -264,6 +274,8 @@ def draw_text(text: str,
         text_sprite.height = image.height
 
         from arcade.sprite_list import SpriteList
+
+        label = Text()
         label.text_sprite_list = SpriteList()
         label.text_sprite_list.append(text_sprite)
 
