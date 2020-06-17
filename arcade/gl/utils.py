a2e9a5aca5e001be52dd7f7af12987818cff7a28
Einar Forselv <eforselv@gmail.com>
2020-06-17 20:28:17 +0200

    Move data_to_ctypes into utils
    This can be reused in other opengl wrappers

---
+++ arcade/gl/utils.py
@@ -0,0 +1,19 @@
+from typing import Any, Tuple
+from ctypes import c_byte
+
+
+def data_to_ctypes(data: Any) -> Tuple[int, Any]:
+    """
+    Attempts to convert the data to ctypes if needed by using the buffer protocol.
+
+    Returns the byte size and the data.
+    """
+    if isinstance(data, bytes):
+        return len(data), data
+    else:
+        try:
+            m_view = memoryview(data)
+            c_bytes = c_byte * m_view.nbytes
+            return m_view.nbytes, c_bytes.from_buffer(m_view)
+        except Exception as ex:
+            raise TypeError(f"Failed to convert data to ctypes: {ex}")
