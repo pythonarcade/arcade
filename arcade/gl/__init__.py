d18b86f58e451597f0425fe5c2782c28728631f6
Paul V Craven <paul@cravenfamily.com>
2020-07-03 10:00:01 -0500

    Add a couple imports to arcade.gl needed for typing

--- arcade/gl/__init__.py
+++ arcade/gl/__init__.py
@@ -7,3 +7,5 @@ from .context import Context
 from .types import BufferDescription
 from .exceptions import ShaderException
 from .enums import *
+from .buffer import Buffer
+from .vertex_array import Geometry
