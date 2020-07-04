52f063d4be19827a41ba0aadbe43ce2fcde9456e
Einar Forselv <eforselv@gmail.com>
2020-07-05 01:43:32 +0200

    Expand gl.__init__

--- arcade/gl/__init__.py
+++ arcade/gl/__init__.py
@@ -4,10 +4,18 @@ from .exceptions import ShaderException
 from .enums import *
 from .buffer import Buffer
 from .vertex_array import Geometry
+from .texture import Texture
+from .framebuffer import Framebuffer
+from .program import Program
+from .query import Query
 
 __all__ = [
     'Buffer',
     'BufferDescription',
+    'Context',
+    'Framebuffer',
     'Geometry',
+    'Program',
     'ShaderException',
+    'Texture',
 ]
