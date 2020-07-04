3ab66eef662e211daf99bea87d8125dc1c727762
Paul V Craven <paul@cravenfamily.com>
2020-07-04 18:09:45 -0500

    Update gl.__init__

--- arcade/gl/__init__.py
+++ arcade/gl/__init__.py
@@ -1,11 +1,13 @@
-
-# Thank you Benjamin Moran for writing part of this code!
-# https://bitbucket.org/HigashiNoKaze/pyglet/src/shaders/pyglet/graphics/shader.py
-
-
 from .context import Context
 from .types import BufferDescription
 from .exceptions import ShaderException
 from .enums import *
 from .buffer import Buffer
 from .vertex_array import Geometry
+
+__all__ = [
+    'Buffer',
+    'BufferDescription',
+    'Geometry',
+    'ShaderException',
+]
