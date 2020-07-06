f48bdd8189145b008e3a064c5b0228efd3fef294
Einar Forselv <eforselv@gmail.com>
2020-07-07 01:30:22 +0200

    docstrings: gl.Buffer

--- arcade/gl/buffer.py
+++ arcade/gl/buffer.py
@@ -11,28 +11,29 @@ if TYPE_CHECKING:  # handle import cycle caused by type hinting
 
 
 class Buffer:
-    """OpenGL Buffer object. Buffers store byte data and upload it
-    to graphics memory. They are used for storage og vertex data,
-    element data (vertex indexing), uniform buffer data etc.
-
-    Common bind targets are:  ``GL_ARRAY_BUFFER``, ``GL_ELEMENT_ARRAY_BUFFER``,
-    ``GL_UNIFORM_BUFFER``, ``GL_SHADER_STORAGE_BUFFER``
-
-    It doesn't matter what bind target the buffer has on creation. What
-    matters is how we bind it in rendering calls.
+    """OpenGL buffer object. Buffers store byte data and upload it
+    to graphics memory so shader programs can process the data.
+    They are used for storage of vertex data,
+    element data (vertex indexing), uniform block data etc.
     """
     __slots__ = '_ctx', '_glo', '_size', '_usage', '__weakref__'
-    usages = {
+    _usages = {
         'static': gl.GL_STATIC_DRAW,
         'dynamic': gl.GL_DYNAMIC_DRAW,
         'stream': gl.GL_STREAM_DRAW
     }
 
     def __init__(self, ctx, data: Optional[Any] = None, reserve: int = 0, usage: str = 'static'):
+        """
+        :param Context ctx: The context this buffer belongs to
+        :param Any data: The data this buffer should contain. It can be bytes or any object supporting the buffer protocol.
+        :param int reserve: Create a buffer of a specific byte size
+        :param str usage: A hit of this buffer is ``static`` or ``dynamic`` (can mostly be ignored)
+        """
         self._ctx = ctx
         self._glo = glo = gl.GLuint()
         self._size = -1
-        self._usage = Buffer.usages[usage]
+        self._usage = Buffer._usages[usage]
 
         gl.glGenBuffers(1, byref(self._glo))
         # print(f"glGenBuffers() -> {self._glo.value}")
@@ -57,24 +58,37 @@ class Buffer:
 
     @property
     def size(self) -> int:
-        """The byte size of the buffer"""
+        """
+        The byte size of the buffer.
+
+        :type: int
+        """
         return self._size
 
     @property
     def ctx(self) -> 'Context':
-        """The context this resource belongs to"""
+        """
+        The context this resource belongs to.
+
+        :type: :py:class:`arcade.gl.Context`
+        """
         return self._ctx
 
     @property
     def glo(self) -> gl.GLuint:
-        """The OpenGL resource id"""
+        """
+        The OpenGL resource id
+
+        :type: gl.GLuint
+        """
         return self._glo
 
     @staticmethod
     def release(ctx: 'Context', glo: gl.GLuint):
-        """ Release/delete open gl buffer. """
-        # print(f"*** Buffer {glo} have no more references. Deleting.")
-
+        """
+        Release/delete open gl buffer.
+        This is automatically called when the object is garbage collected.
+        """
         # If we have no context, then we are shutting down, so skip this
         if gl.current_context is None:
             return
@@ -88,8 +102,9 @@ class Buffer:
     def read(self, size=-1, offset=0) -> bytes:
         """Read data from the buffer.
 
-        :param int size: The bytes to read. -1 means the entire buffer
+        :param int size: The bytes to read. -1 means the entire buffer (default)
         :param int offset: Byte read offset
+        :rtype: bytes
         """
         if size == -1:
             size = self._size
@@ -117,7 +132,7 @@ class Buffer:
     def write(self, data: Any, offset: int = 0):
         """Write byte data to the buffer.
 
-        :param bytes data: The byte data to write
+        :param bytes data: The byte data to write. This can be bytes or any object supporting the buffer protocol.
         :param int offset: The byte offset
         """
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self._glo)
@@ -155,7 +170,9 @@ class Buffer:
 
     def orphan(self, size=-1, double: bool = False):
         """
-        Re-allocate the entire buffer memory.
+        Re-allocate the entire buffer memory. This can be used to resize
+        a buffer or for re-specification (orphan the buffer to avoid blocking).
+
         If the current buffer is busy in redering operations
         it will be deallocated by OpenGL when completed.
 
@@ -172,6 +189,13 @@ class Buffer:
         gl.glBufferData(gl.GL_ARRAY_BUFFER, self._size, None, self._usage)
 
     def bind_to_uniform_block(self, binding: int = 0, offset: int = 0, size: int = -1):
+        """Bind this buffer to a uniform block location.
+        In most cases it will be sufficient to only provice a binding location.
+        
+        :param int binding: The binding location
+        :param int offset: byte offset
+        :param int size: size of the buffer to bind.
+        """
         if size < 0:
             size = self.size
 
