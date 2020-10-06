4bf228519ca2288497d0ffa32a8f65c37c23e92b
Maic Siemering <maic@siemering.tech>
2020-10-06 21:19:46 +0200

    GUI Improvements:
    * pressed and hovered texture optional in UIImageButton
    * add option to not register handlers in UIManager creation
    * fix UIElements to have a default position
    * turn off fancy hitbox calculation for UILabels
    * add on_resize and on_update callbacks to UIManager

--- tests/test_gui/test_ui_manager_dispatcher.py
+++ tests/test_gui/test_ui_manager_dispatcher.py
@@ -9,14 +9,47 @@ def test_handler_pushed():
     msg = UIManager(window)
 
     window.assert_has_calls([
-        call.push_handlers(msg.on_draw,
-                           msg.on_mouse_press,
-                           msg.on_mouse_release,
-                           msg.on_mouse_scroll,
-                           msg.on_mouse_motion,
-                           msg.on_key_press,
-                           msg.on_key_release,
-                           msg.on_text,
-                           msg.on_text_motion,
-                           msg.on_text_motion_select)
+        call.push_handlers(
+            msg.on_resize,
+            msg.on_update,
+            msg.on_draw,
+            msg.on_mouse_press,
+            msg.on_mouse_release,
+            msg.on_mouse_scroll,
+            msg.on_mouse_motion,
+            msg.on_key_press,
+            msg.on_key_release,
+            msg.on_text,
+            msg.on_text_motion,
+            msg.on_text_motion_select)
+    ])
+
+def test_handler_not_pushed():
+    window = Mock()
+
+    _ = UIManager(window, attach_callbacks=False)
+
+    assert not window.push_handlers.called
+
+
+def test_handler_removed():
+    window = Mock()
+    msg = UIManager(window)
+
+    msg.unregister_handlers()
+
+    window.assert_has_calls([
+        call.remove_handlers(
+            msg.on_resize,
+            msg.on_update,
+            msg.on_draw,
+            msg.on_mouse_press,
+            msg.on_mouse_release,
+            msg.on_mouse_scroll,
+            msg.on_mouse_motion,
+            msg.on_key_press,
+            msg.on_key_release,
+            msg.on_text,
+            msg.on_text_motion,
+            msg.on_text_motion_select)
     ])
