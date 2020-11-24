ca01458d27118ac7eee8f1c87849ad71acd25e96
Paul V Craven <paul@cravenfamily.com>
2020-11-24 16:15:49 -0600

    Fix shapely geometry collision issue.

--- arcade/geometry.py
+++ arcade/geometry.py
@@ -28,7 +28,11 @@ def are_polygons_intersecting(poly_a: PointList,
     shapely_polygon_a = Polygon(poly_a)
     shapely_polygon_b = Polygon(poly_b)
 
-    return shapely_polygon_a.intersects(shapely_polygon_b)
+    r2 = False
+    r1 = shapely_polygon_a.intersects(shapely_polygon_b)
+    if r1:
+        r2 = shapely_polygon_a.touches(shapely_polygon_b)
+    return r1 and not r2
 
 
 def is_point_in_polygon(x, y, polygon_point_list):
