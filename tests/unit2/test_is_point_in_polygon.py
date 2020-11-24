01a6ba9399a7054c7295fbd333416098ad2e2a74
Paul V Craven <paul@cravenfamily.com>
2020-11-24 14:29:46 -0600

    Fix polygon spelling for polygon tests

--- tests/unit2/test_is_point_in_polygon.py
+++ tests/unit2/test_is_point_in_polygon.py
@@ -2,17 +2,17 @@ import arcade
 
 
 def test_point_in_rectangle():
-    poligon = [
+    polygon = [
         (0, 0),
         (0, 50),
         (50, 50),
         (50, 0),
     ]
-    result = arcade.is_point_in_polygon(25, 25, poligon)
+    result = arcade.is_point_in_polygon(25, 25, polygon)
     assert result is True
 
 
-def test_point_not_in_empty_poligon():
-    poligon = []
-    result = arcade.is_point_in_polygon(25, 25, poligon)
+def test_point_not_in_empty_polygon():
+    polygon = []
+    result = arcade.is_point_in_polygon(25, 25, polygon)
     assert result is False
