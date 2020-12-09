d5bacd06e621d2cbe78bce3cb244c6ea5d22cfd1
Paul V Craven <paul@cravenfamily.com>
2020-12-09 09:35:59 -0600

    Update setup to not allow Python 3.9 for Mac only

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
+import sys
 from setuptools import setup
 
+if sys.platform == "darwin":
+    required_python_version=">=3.6,<3.9"
+else:
+    required_python_version=">=3.6,<3.10"
+
 exec(open("arcade/version.py").read())
-setup(version=VERSION)
+setup(python_requires=required_python_version,
+      version=VERSION)
