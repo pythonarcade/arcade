bd43249d7afd9e5e7e29b84df195a533b22cbc66
Paul V Craven <paul@cravenfamily.com>
2020-12-14 09:04:42 -0600

    Issue #803, update to delete old unused files.

--- make.bat
+++ make.bat
@@ -128,7 +128,7 @@ rem This is a batch file used to sync the documentation for arcade.academy to
 rem the bucket it is hosted on. Doesn't do much good if you don't have
 rem the credentials.
 rem You also need "aws command line" installed.
-aws s3 sync doc/build/html s3://craven-arcade
+aws s3 sync --delete doc/build/html s3://craven-arcade
 
 GOTO end
 
