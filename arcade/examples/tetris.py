1b9ff819e7354a1b7a83296c2651f1b8c0a70659
Josh Grant <rubiconjosh@gmail.com>
2021-01-09 06:28:55 -0800

    renamed rotate_clockwise to rotate_counterclockwise

--- arcade/examples/tetris.py
+++ arcade/examples/tetris.py
@@ -76,7 +76,7 @@ def create_textures():
 texture_list = create_textures()
 
 
-def rotate_clockwise(shape):
+def rotate_counterclockwise(shape):
     """ Rotates a matrix clockwise """
     return [[shape[y][x] for y in range(len(shape))] for x in range(len(shape[0]) - 1, -1, -1)]
 
@@ -195,7 +195,7 @@ class MyGame(arcade.Window):
     def rotate_stone(self):
         """ Rotate the stone, check collision. """
         if not self.game_over and not self.paused:
-            new_stone = rotate_clockwise(self.stone)
+            new_stone = rotate_counterclockwise(self.stone)
             if self.stone_x + len(self.stone) > COLUMN_COUNT - 1:
                 self.stone_x -= len(self.stone)
             if not check_collision(self.board, new_stone, (self.stone_x, self.stone_y)):
