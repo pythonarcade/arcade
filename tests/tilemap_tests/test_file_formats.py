7d0b102605d85b5aa194151f415f7b44d38cc390
Darren Eberly <darren.eberly@gmail.com>
2021-02-21 15:57:19 -0500

    Remove support for zstd compression

--- tests/tilemap_tests/test_file_formats.py
+++ tests/tilemap_tests/test_file_formats.py
@@ -74,33 +74,6 @@ def test_base_64_zlib():
     assert wall_list[2].position == (64, 64)
     assert "boxCrate" in wall_list[2].texture.name
 
-def test_base_64_zstandard():
-    # Read in the tiled map
-    my_map = arcade.tilemap.read_map("../tiled_maps/base_64_zstandard.json")
-
-    assert my_map.tile_size == (128, 128)
-    assert my_map.orientation == "orthogonal"
-    assert my_map.render_order == "right-up"
-    assert my_map.infinite == 0
-    assert my_map.map_size == (10, 10)
-
-    # --- Platforms ---
-    wall_list = arcade.tilemap.process_layer(my_map, 'Blocking Sprites', TILE_SCALING)
-    #
-    # for wall in wall_list:
-    #     print()
-    #     print(wall.position)
-    #     print(wall.texture.name)
-
-    assert wall_list[0].position == (64, 1216)
-    assert "dirtCenter" in wall_list[0].texture.name
-
-    assert wall_list[1].position == (1216, 1216)
-    assert "grassCenter" in wall_list[1].texture.name
-
-    assert wall_list[2].position == (64, 64)
-    assert "boxCrate" in wall_list[2].texture.name
-
 def test_base_64_gzip():
     # Read in the tiled map
     my_map = arcade.tilemap.read_map("../tiled_maps/base_64_gzip.json")
