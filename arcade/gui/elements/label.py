61afbfc8a09588c367f77314fe47140669c843d7
Maic Siemering <maic@vw-dilab.com>
2021-04-13 07:02:29 +0200

    [GUI] Remove hover and focus state, labels are not buttons anymore

--- arcade/gui/elements/label.py
+++ arcade/gui/elements/label.py
@@ -1,46 +1,46 @@
-from typing import Optional
-from uuid import uuid4
+from typing import Optional, Tuple
 
 import arcade
-from arcade.gui.elements import UIClickable
+from arcade.gui import text_utils, UIElement
 from arcade.gui.style import UIStyle
-from arcade.gui.utils import get_text_image
 
 
-class UILabel(UIClickable):
+class UILabel(UIElement):
     """
     :py:class:`arcade.gui.UIElement` for showing text.
     """
 
     def __init__(
-        self,
-        text: str,
-        center_x=0,
-        center_y=0,
-        width: int = 0,
-        align="center",
-        id: Optional[str] = None,
-        style: UIStyle = None,
-        **kwargs
+            self,
+            text: str,
+            center_x=0,
+            center_y=0,
+            min_size: Optional[Tuple] = None,
+            size_hint: Optional[Tuple] = None,
+            id: Optional[str] = None,
+            style: UIStyle = None,
+            **kwargs
     ):
         """
         :param text: Text to show
         :param center_x: center X of element
         :param center_y: center y of element
         :param width: width, 0 will use the width of the rendered text
-        :param align: position of the text, requires a fix width
         :param id: id of :py:class:`arcade.gui.UIElement`
         :param style: style of :py:class:`arcade.gui.UIElement`
         :param kwargs: catches unsupported named parameters
         """
         super().__init__(
-            center_x=center_x, center_y=center_y, id=id, style=style, **kwargs
+            center_x=center_x,
+            center_y=center_y,
+            min_size=min_size,
+            size_hint=size_hint,
+            id=id,
+            style=style,
+            **kwargs
         )
         self.style_classes.append("label")
 
-        self._target_width = width
-
-        self.align = align
         self.text = text
         # self.render already implicitly called through setting text
 
@@ -59,47 +59,27 @@ class UILabel(UIClickable):
     def render(self):
         font_name = self.style_attr("font_name", ["Calibri", "Arial"])
         font_size = self.style_attr("font_size", 12)
+        font_color = self.style_attr("font_color", arcade.color.WHITE)
+        border_width = self.style_attr("border_width", 0)
+        border_color = self.style_attr("border_color", None)
+        bg_color = self.style_attr("bg_color", None)
+        v_align = self.style_attr("v_align", "center")
+        h_align = self.style_attr("h_align", "center")
 
-        font_color = self.style_attr("font_color", arcade.color.GRAY)
-        font_color_hover = self.style_attr("font_color_hover", None)
-        font_color_press = self.style_attr("font_color_press", None)
-
-        if font_color_hover is None:
-            font_color_hover = font_color
-        if font_color_press is None:
-            font_color_press = font_color_hover
-
-        text_image_normal = get_text_image(
+        text_image_normal, text_image_normal_uuid = text_utils.create_text(
             text=self.text,
+            font_name=font_name,
+            font_size=font_size,
             font_color=font_color,
-            font_size=font_size,
-            font_name=font_name,
-            align=self.align,
-            width=int(self._target_width),
-        )
-        text_image_mouse_over = get_text_image(
-            text=self.text,
-            font_color=font_color_hover,
-            font_size=font_size,
-            font_name=font_name,
-            align=self.align,
-            width=int(self._target_width),
-        )
-        text_image_mouse_press = get_text_image(
-            text=self.text,
-            font_color=font_color_press,
-            font_size=font_size,
-            font_name=font_name,
-            align=self.align,
-            width=int(self._target_width),
+            bg_color=bg_color,
+            min_width=self.width - border_width,
+            min_height=self.height - border_width,
+            v_align=v_align,
+            h_align=h_align,
+            border_width=border_width,
+            border_color=border_color,
         )
 
-        self.normal_texture = arcade.Texture(
-            image=text_image_normal, name=str(uuid4()), hit_box_algorithm="None"
-        )
-        self.press_texture = arcade.Texture(
-            image=text_image_mouse_press, name=str(uuid4()), hit_box_algorithm="None"
-        )
-        self.hover_texture = arcade.Texture(
-            image=text_image_mouse_over, name=str(uuid4()), hit_box_algorithm="None"
+        self.texture = arcade.Texture(
+            image=text_image_normal, name=text_image_normal_uuid
         )
