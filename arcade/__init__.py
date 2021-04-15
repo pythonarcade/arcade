f672f776239a18ec353d1c59a6fbe12e0d430df6
pushfoo <36696816+pushfoo@users.noreply.github.com>
2021-04-15 11:03:54 -0400

    Regenerate arcade/__init__.py

--- arcade/__init__.py
+++ arcade/__init__.py
@@ -10,6 +10,9 @@ A Python simple, easy to use module for creating 2D games.
 # Error out if we import Arcade with an incompatible version of Python.
 import platform
 import sys
+import os
+
+from pathlib import Path
 
 if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 6):
     sys.exit("The Arcade Library requires Python 3.6 or higher.")
@@ -31,6 +34,22 @@ def configure_logging(level: int = None):
         ch.setFormatter(logging.Formatter('%(relativeCreated)s %(name)s %(levelname)s - %(message)s'))
         LOG.addHandler(ch)
 
+# The following is used to load ffmpeg libraries.
+# Currently Arcade is only shipping binaries for Mac OS
+# as ffmpeg is not needed for support on Windows and Linux.
+# However it is setup to load ffmpeg if the binaries are present
+# on Windows and Linux. So if you need ffmpeg you can simply
+# drop the binaries in the "lib" folder of Arcade
+lib_location = Path(__file__).parent.absolute()
+lib_location = lib_location / "lib"
+
+if sys.platform == "darwin" or sys.platform.startswith("linux"):
+    if "LD_LIBRARY_PATH" in os.environ:
+        os.environ["LD_LIBRARY_PATH"] += ":" + str(lib_location)
+    else:
+        os.environ["LD_LIBRARY_PATH"] = str(lib_location)
+else:
+    os.environ["PATH"] += str(lib_location)
 
 # noinspection PyPep8
 import pyglet
