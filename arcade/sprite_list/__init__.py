d7109ba228dc3ff8419151ca587b1d8d49e8864f
Einar Forselv <eforselv@gmail.com>
2021-05-15 20:22:43 +0200

    Split up sprite_list module

---
+++ arcade/sprite_list/__init__.py
@@ -0,0 +1,8 @@
+from .sprite_list import SpriteList
+from .spatial_hash import (
+    get_closest_sprite,
+    check_for_collision,
+    check_for_collision_with_list,
+    get_sprites_at_point,
+    get_sprites_at_exact_point,
+)
