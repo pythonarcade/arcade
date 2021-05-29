94972df610283b3f883d2c34cea4a2fa5effcf0e
Darren Eberly <darren.eberly@gmail.com>
2021-05-29 01:09:25 -0400

    Add missed function to sprite_list package

--- arcade/sprite_list/__init__.py
+++ arcade/sprite_list/__init__.py
@@ -3,6 +3,7 @@ from .spatial_hash import (
     get_closest_sprite,
     check_for_collision,
     check_for_collision_with_list,
+    check_for_collision_with_lists,
     get_sprites_at_point,
     get_sprites_at_exact_point,
 )
