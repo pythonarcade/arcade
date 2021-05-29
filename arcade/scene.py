70f238374bdbcd11f63094eab2c5d21f9ef59efd
Darren Eberly <darren.eberly@gmail.com>
2021-05-29 01:47:01 -0400

    Slight rework of some Scene functions

--- arcade/scene.py
+++ arcade/scene.py
@@ -1,6 +1,6 @@
 from typing import Dict, List, Optional, Union
 
-from arcade import SpriteList
+from arcade import Sprite, SpriteList
 from arcade.tilemap import TileMap
 
 
@@ -19,25 +19,39 @@ class Scene:
     def get_sprite_list(self, name: str) -> SpriteList:
         return self.name_mapping[name]
 
+    def add_sprite(self, name: str, sprite: Sprite) -> None:
+        if name in self.name_mapping:
+            self.name_mapping[name].append(sprite) 
+        else:
+            new_list = SpriteList()
+            new_list.append(sprite)
+            self.add_sprite_list(name, new_list)
+
     def add_sprite_list(
         self,
         name: str,
-        sprite_list: SpriteList,
+        sprite_list: Optional[SpriteList] = None,
     ) -> None:
+        if not sprite_list:
+            sprite_list = SpriteList()
         self.name_mapping[name] = sprite_list
         self.sprite_lists.append(sprite_list)
 
     def add_sprite_list_before(
-        self, name: str, sprite_list: SpriteList, before: str
+        self, name: str, before: str, sprite_list: Optional[SpriteList] = None
     ) -> None:
+        if not sprite_list:
+            sprite_list = SpriteList()
         self.name_mapping[name] = sprite_list
         before_list = self.name_mapping[before]
         index = self.sprite_lists.index(before_list) - 1
         self.sprite_lists.insert(index, sprite_list)
 
     def add_sprite_list_after(
-        self, name: str, sprite_list: SpriteList, after: str
+        self, name: str, after: str, sprite_list: Optional[SpriteList] = None
     ) -> None:
+        if not sprite_list:
+            sprite_list = SpriteList()
         self.name_mapping[name] = sprite_list
         after_list = self.name_mapping[after]
         index = self.sprite_lists.index(after_list)
@@ -57,7 +71,7 @@ class Scene:
             key: val for key, val in self.name_mapping.items() if val != sprite_list
         }
 
-    def update(self, names: Optional[Union[str, List[str]]] = None) -> None:
+    def update(self, names: Optional[List[str]] = None) -> None:
         """
         Used to update SpriteLists contained in the scene.
 
@@ -69,18 +83,15 @@ class Scene:
         :param Union[str, List[str]] names: A name or list of names of SpriteLists to update
         """
         if names:
-            if isinstance(names, str):
-                self.name_mapping[names].update()
-            elif isinstance(names, list):
-                for name in names:
-                    self.name_mapping[name].update()
+            for name in names:
+                self.name_mapping[name].update()
             return
 
         for sprite_list in self.sprite_lists:
             sprite_list.update()
 
     def update_animation(
-        self, delta_time: float, names: Optional[Union[str, List[str]]] = None
+        self, delta_time: float, names: Optional[List[str]] = None
     ) -> None:
         """
         Used to update the animation of SpriteLists contained in the scene.
@@ -94,11 +105,8 @@ class Scene:
         :param Union[str, List[str]] names: A name or list of names of SpriteLists to update.
         """
         if names:
-            if isinstance(names, str):
-                self.name_mapping[names].update_animation(delta_time)
-            elif isinstance(names, list):
-                for name in names:
-                    self.name_mapping[name].update_animation(delta_time)
+            for name in names:
+                self.name_mapping[name].update_animation(delta_time)
             return
 
         for sprite_list in self.sprite_lists:
