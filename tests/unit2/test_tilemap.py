0f0a55e406a8c7973e6d9afd96b79b55ce704243
Darren Eberly <darren.eberly@gmail.com>
2021-07-06 23:18:25 -0400

    Fix some tilemap tests

--- tests/unit2/test_tilemap.py
+++ tests/unit2/test_tilemap.py
@@ -1,28 +1,25 @@
 import arcade
-
 from pytiled_parser.common_types import Color
 
 
 def test_one():
-    tmx_map = arcade.tilemap.read_map(":resources:/tiled_maps/test_map_1.json")
+    tile_map = arcade.load_tilemap(":resources:/tiled_maps/test_map_1.json")
 
-    assert tmx_map.map_size.width == 10
-    assert tmx_map.map_size.height == 5
-    assert not tmx_map.infinite
-    assert tmx_map.orientation == "orthogonal"
-    assert tmx_map.render_order == 'right-down'
-    assert len(tmx_map.layers) == 2
-    assert tmx_map.background_color == Color(0, 160, 229, 255)
+    assert tile_map.width == 10
+    assert tile_map.height == 5
+    assert len(tile_map.sprite_lists) == 2
+    assert tile_map.background_color == Color(0, 160, 229, 255)
 
-    platforms_list = arcade.tilemap.process_layer(tmx_map, "Platforms", base_directory="test_data")
-
-    assert platforms_list is not None
+    assert "Platforms" in tile_map.sprite_lists
+    platforms_list = tile_map.sprite_lists["Platforms"]
     assert len(platforms_list) == 10
 
-    background_list = arcade.tilemap.process_layer(tmx_map, "Background", base_directory="test_data")
+    assert "Background" in tile_map.sprite_lists
+    background_list = tile_map.sprite_lists["Background"]
     assert len(background_list) == 2
 
     first_sprite = platforms_list[0]
+    assert first_sprite is not None
     assert first_sprite.center_x == 64
     assert first_sprite.center_y == 64
     assert first_sprite.width == 128
@@ -30,86 +27,74 @@ def test_one():
 
 
 def test_two():
-    tmx_map = arcade.tilemap.read_map(":resources:tiled_maps/test_map_2.json")
+    tile_map = arcade.load_tilemap(":resources:tiled_maps/test_map_2.json")
 
-    assert tmx_map.map_size.width == 10
-    assert tmx_map.map_size.height == 5
-    assert not tmx_map.infinite
-    assert tmx_map.orientation == "orthogonal"
-    assert tmx_map.render_order == 'left-up'
-    assert len(tmx_map.layers) == 3
+    assert tile_map.width == 10
+    assert tile_map.height == 5
+    assert len(tile_map.sprite_lists) == 3
 
-    platforms_list = arcade.tilemap.process_layer(tmx_map, "Platforms")
-
-    assert platforms_list is not None
+    assert "Platforms" in tile_map.sprite_lists
+    platforms_list = tile_map.sprite_lists["Platforms"]
+    assert len(platforms_list) == 10
 
     first_sprite = platforms_list[0]
     assert first_sprite is not None
     assert first_sprite.center_x == 64
     assert first_sprite.center_y == 64
-    # print(first_sprite.center_x, first_sprite.center_y)
-    # print(first_sprite.points)
-
-    assert first_sprite.center_x == 64
-    assert first_sprite.center_y == 64
     assert first_sprite.width == 128
     assert first_sprite.height == 128
 
-    dirt_list = arcade.tilemap.process_layer(tmx_map, "Dirt")
+    assert "Dirt" in tile_map.sprite_lists
+    dirt_list = tile_map.sprite_lists["Dirt"]
 
     first_sprite = dirt_list[0]
     assert first_sprite is not None
-
-    # print(first_sprite.center_x, first_sprite.center_y)
-    # print(first_sprite.points)
-
     first_sprite = dirt_list[1]
     assert first_sprite is not None
-    # print(first_sprite.center_x, first_sprite.center_y)
-    # print(first_sprite.points)
-
     first_sprite = dirt_list[2]
     assert first_sprite is not None
-    # print(first_sprite.center_x, first_sprite.center_y)
-    # print(first_sprite.points)
 
-    coin_list = arcade.tilemap.process_layer(tmx_map, "Coins")
+    assert "Coins" in tile_map.sprite_lists
+    coin_list = tile_map.sprite_lists["Coins"]
+
     first_sprite = coin_list[0]
     assert first_sprite is not None
-    # print(first_sprite.center_x, first_sprite.center_y)
-    # print(first_sprite.points)
-
-    # assert 'Points' in first_sprite.properties
-    # assert first_sprite.properties['Points'] == '10'
 
 
 def test_three():
-    tmx_map = arcade.tilemap.read_map(":resources:tiled_maps/test_map_3.json")
-    assert tmx_map is not None
+    tile_map = arcade.load_tilemap(":resources:tiled_maps/test_map_3.json")
+    assert tile_map is not None
 
-    sprite_list = arcade.tilemap.process_layer(tmx_map, "Moving Platforms", base_directory="test_data")
+    assert "Moving Platforms" in tile_map.sprite_lists
+    sprite_list = tile_map.sprite_lists["Moving Platforms"]
+
     assert sprite_list is not None
-
     assert len(sprite_list) == 1
     sprite = sprite_list[0]
     assert sprite.properties is not None
-    assert 'change_x' in sprite.properties
-    assert 'change_y' in sprite.properties
-    assert sprite.properties['change_x'] == 1
-    assert sprite.properties['change_y'] == 1
+    assert "change_x" in sprite.properties
+    assert "change_y" in sprite.properties
+    assert sprite.properties["change_x"] == 1
+    assert sprite.properties["change_y"] == 1
 
 
 def test_five():
-    tmx_map = arcade.tilemap.read_map(":resources:tiled_maps/test_map_5.json")
-    assert tmx_map is not None
+    tile_map = arcade.load_tilemap(":resources:tiled_maps/test_map_5.json")
+    assert tile_map is not None
 
-    arcade.tilemap.process_layer(tmx_map, 'object_layer')
+    assert len(tile_map.object_lists) == 1
+    assert "object_layer" in tile_map.object_lists
+
 
 def test_sprite_sheet():
-    tmx_map = arcade.tilemap.read_map(":resources:tiled_maps/test_map_6.json")
-    assert tmx_map is not None
+    tile_map = arcade.load_tilemap(":resources:tiled_maps/test_map_6.json")
+    assert tile_map is not None
 
-    sprite_list = arcade.tilemap.process_layer(tmx_map, 'Tile Layer 1')
+    assert len(tile_map.sprite_lists) == 1
+    assert "Tile Layer 1" in tile_map.sprite_lists
+
+    sprite_list = tile_map.sprite_lists["Tile Layer 1"]
+
     assert sprite_list is not None
     assert len(sprite_list) == 14
 
@@ -117,4 +102,3 @@ def test_sprite_sheet():
     assert first_sprite is not None
     assert first_sprite.height == 16
     assert first_sprite.width == 16
-
