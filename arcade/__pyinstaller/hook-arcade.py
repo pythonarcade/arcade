c0a627ba00fd1f7cd01213358200ca4b5cb341ee
Darren Eberly <darren.eberly@gmail.com>
2021-07-14 20:46:02 -0400

    Pyinstaller hook re-work

--- arcade/__pyinstaller/hook-arcade.py
+++ arcade/__pyinstaller/hook-arcade.py
@@ -6,48 +6,42 @@ Official Sample: https://github.com/pyinstaller/hooksample
 
 Note: See setup.cfg for entry points that enable this hook file to be found
 
-For people who want to modify hook-arcade.py from a git checkout of Arcade,
-here is a sequence of steps that will let you test your changes:
-
-    cd arcade  # set top dir of repo as your current working directory
-    venv\Scripts\activate  # activate your venv
-    # edit setup.cfg or hook-arcade.py with your changes
-    pip uninstall arcade
-    pip install .
-    pyinstaller arcade\examples\starting_template_simple.py
-    dist\starting_template_simple\starting_template_simple.exe
-
-Simply put, after you edit setup.cfg or hook-arcade.py, you will need to uninstall and reinstall arcade.
-
-NOTE: The sequence of steps above do NOT support editable installs of Arcade (ex: `pip install -e .`) because
-the paths to binary files can be slightly different.
+For a walkthrough of building an Arcade application with pyinstaller see:
+https://arcade.academy/tutorials/bundling_with_pyinstaller/index.html
 """
 from pathlib import Path
 
+import arcade
 import pymunk
-from PyInstaller.compat import is_darwin, is_unix, is_win  # type: ignore
+from PyInstaller.compat import is_darwin, is_unix, is_win
 
-pymunk_path = Path(pymunk.__file__)
+pymunk_path = Path(pymunk.__file__).parent
+arcade_path = Path(arcade.__file__).parent
 
-hook_path = Path(__file__)
+datas = [
+    (
+        arcade_path / "resources/shaders",
+        "./arcade/resources/shaders",
+    ),
+    (arcade_path / "resources/style", "./arcade/resources/style"),
+    (arcade_path.parent / "resources/fonts", "./arcade/resources/fonts"),
+]
 
 if is_win:
-    datas = [
-        (hook_path.parent.parent.joinpath("resources"), "./arcade/resources"),
-    ]
-
     binaries = [
-        (hook_path.parent.parent.parent.joinpath("pymunk/_chipmunk.pyd"), "."),
+        (pymunk_path / "_chipmunk.pyd", "."),
     ]
-
 elif is_darwin:
-    raise NotImplementedError("Arcade does not support pyinstaller on Mac yet")
-
-elif is_unix:
-    datas = [
-        (hook_path.parent.parent.joinpath("resources"), "./arcade/resources"),
-    ]
-
     binaries = [
-        (pymunk_path.parent / "_chipmunk.abi3.so", "."),
+        (pymunk_path / "_chipmunk.abi3.so", "."),
+        (arcade_path / "lib", "./arcade/lib"),
     ]
+elif is_unix:
+    binaries = [
+        (pymunk_path / "_chipmunk.abi3.so", "."),
+    ]
+else:
+    raise NotImplementedError(
+        "You are running on an unsupported operating system. "
+        "Only Linux, Mac, and Windows are supported."
+    )
