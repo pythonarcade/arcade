de34d835a81cb865612696345aefaa96dbcb7f9f
alexandre zoppis <alexandre.zoppis@grenoble-inp.org>
2021-08-01 18:17:05 +0200

    Replacement of global_options by three parameters: - use_spatial_hash: Optional[bool] = None - hit_box_algorithm: str = "Simple" - hit_box_detail: float = 4.5

--- arcade/tilemap/tilemap.py
+++ arcade/tilemap/tilemap.py
@@ -106,8 +106,10 @@ class TileMap:
         self,
         map_file: Union[str, Path],
         scaling: float = 1.0,
-        layer_options: Optional[Dict[str, Dict[str, Any]]] = None,
-        global_options: Optional[Dict[str, str]] = None
+        use_spatial_hash: Optional[bool] = None,
+        hit_box_algorithm: str = "Simple",
+        hit_box_detail: float = 4.5,
+        layer_options: Optional[Dict[str, Dict[str, Any]]] = None
     ) -> None:
         """
         Given a .json file, this will read in a Tiled map file, and
@@ -138,7 +140,12 @@ class TileMap:
         :param Union[str, Path] map_file: The JSON map file.
         :param float scaling: Global scaling to apply to all Sprites.
         :param Dict[str, Dict[str, Any]] layer_options: Extra parameters for each layer.
-        :param Dict[str, str] global_options: Common options for all layers in the map.
+        :param Optional[bool] use_spatial_hash: If set to True, this will make moving a sprite
+               in the SpriteList slower, but it will speed up collision detection
+               with items in the SpriteList. Great for doing collision detection
+               with static walls/platforms.
+        :param str hit_box_algorithm: One of 'None', 'Simple' or 'Detailed'.
+        :param float hit_box_detail: Float, defaults to 4.5. Used with 'Detailed' to hit box.
         """
 
         # If we should pull from local resources, replace with proper path
@@ -165,8 +172,9 @@ class TileMap:
         if not layer_options:
             layer_options = {}
 
-        if not global_options:
-            global_options = {}
+        global_options = {"use_spatial_hash": use_spatial_hash,
+                          "hit_box_algorithm": hit_box_algorithm,
+                          "hit_box_detail": hit_box_detail}
 
         for layer in self.tiled_map.layers:
             processed: Union[
@@ -758,8 +766,10 @@ class TileMap:
 def load_tilemap(
     map_file: Union[str, Path],
     scaling: float = 1.0,
-    layer_options: Optional[Dict[str, Dict[str, Any]]] = None,
-    global_options: Optional[Dict[str, str]] = None
+    use_spatial_hash: Optional[bool] = None,
+    hit_box_algorithm: str = "Simple",
+    hit_box_detail: float = 4.5,
+    layer_options: Optional[Dict[str, Dict[str, Any]]] = None
 ) -> TileMap:
     """
     Given a .json map file, loads in and returns a `TileMap` object.
@@ -772,10 +782,15 @@ def load_tilemap(
 
     :param Union[str, Path] map_file: The JSON map file.
     :param float scaling: The global scaling to apply to all Sprite's within the map.
+    :param Optional[bool] use_spatial_hash: If set to True, this will make moving a sprite
+               in the SpriteList slower, but it will speed up collision detection
+               with items in the SpriteList. Great for doing collision detection
+               with static walls/platforms.
+    :param str hit_box_algorithm: One of 'None', 'Simple' or 'Detailed'.
+    :param float hit_box_detail: Float, defaults to 4.5. Used with 'Detailed' to hit box.
     :param Dict[str, Dict[str, Any]] layer_options: Layer specific options for the map.
-    :param Dict[str, str] global_options: Common options for all layers in the map.
     """
-    return TileMap(map_file, scaling, layer_options, global_options)
+    return TileMap(map_file, scaling, use_spatial_hash, hit_box_algorithm, hit_box_detail, layer_options)
 
 
 def read_tmx(map_file: Union[str, Path]) -> pytiled_parser.TiledMap:
