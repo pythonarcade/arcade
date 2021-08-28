dcc5a8786b9757014f99213e0f11f9fb132dee2f
Paul Craven <paul@cravenfamily.com>
2021-08-27 20:21:40 -0500

    Update pyglet version, and make 2.6.0.dev7 version.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     url="https://arcade.academy",
     download_url="https://arcade.academy",
     install_requires=[
-        "pyglet==2.0.dev6",
+        "pyglet==2.0.dev7",
         "pillow~=8.3",
         "pymunk~=6.1.0",
         "shapely==1.7.1",
