bf489c2e8aa12eb63c41fb8a995de27f7ee27a25
Einar Forselv <eforselv@gmail.com>
2021-10-02 14:21:56 +0200

    Remove redundant platform check in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@ from os import path
 
 from setuptools import find_namespace_packages, setup
 
-if sys.platform == "darwin":
-    required_python_version = ">=3.6"
-else:
-    required_python_version = ">=3.6"
-
 exec(open("arcade/version.py").read())
 
 
