aa9e51dcf7733b5c6e1519b1695cbfd323036cc2
pushfoo <36696816+pushfoo@users.noreply.github.com>
2021-10-18 09:38:44 -0400

    Add documentation for draw_text and Text object (#1009)

--- arcade/text_pyglet.py
+++ arcade/text_pyglet.py
@@ -12,8 +12,16 @@ from pyglet.math import Mat4
 from arcade.resources import resolve_resource_path
 
 
-def load_font(font_name):
-    """ Load a font for later use """
+def load_font(font_name) -> None:
+    """
+    Load a font for later use.
+
+
+    :param font_name:
+    :raises FileNotFoundError: if the font specified wasn't found
+    :return:
+    """
+    # search resources folder for the named font, and error if it doesn't exist
     if font_name.startswith(":resources:"):
         try:
             file_path = resolve_resource_path(font_name)
@@ -78,7 +86,6 @@ def _draw_label_with_rotation(label: pyglet.text.Label, rotation: float) -> None
 
     :param pyglet.text.Label label: a pyglet label to wrap and draw
     :param float rotation: rotate this many degrees from horizontal around anchor
-    :return:
     """
 
     # raw pyglet draw functions need this context helper inside arcade
@@ -110,6 +117,66 @@ def _draw_label_with_rotation(label: pyglet.text.Label, rotation: float) -> None
 
 
 class Text:
+    """
+    An object-oriented way to draw text to the screen.
+
+    .. tip:: Use this class when performance matters!
+
+       Unlike :py:func:`~arcade.draw_text`, this class does not risk
+       wasting time recalculating and re-setting any text each time
+       :py:meth:`~arcade.Text.draw` is called. This makes it faster
+       while:
+
+       - requiring you to manage instances and drawing yourself
+       - using negligible extra RAM
+
+       The speed advantage scales as more text needs to be drawn
+       to the screen.
+
+    The constructor arguments work identically to those of
+    :py:func:`~arcade.draw_text`. See its documentation for in-depth
+    explanation for how to use each of them.
+
+    :param str text: Initial text to display. Can be an empty string
+    :param float start_x: x position to align the text's anchor point with
+    :param float start_y: y position to align the text's anchor point with
+    :param Color color: Color of the text as a tuple or list of 3 (RGB) or 4 (RGBA) integers
+    :param float font_size: Size of the text in points
+    :param float width: A width limit in pixels
+    :param str align: Horizontal alignment; only used if width was given
+    :param Union[str, Tuple[str, ...]] font_name: A font name, path to a font file, or list of names
+    :param bool bold: Whether to draw the text as bold
+    :param bool italic: Whether to draw the text as italic
+    :param str anchor_x: How to calculate the anchor point's x coordinate
+    :param str anchor_y: How to calculate the anchor point's y coordinate
+    :param bool multiline: If width was set, enables word wrap rather than clipping
+    :param float rotation: rotation in degrees, counter-clockwise from horizontal
+
+    By default, the text is placed so that:
+
+    - the left edge of its bounding box is at ``start_x``
+    - its baseline is at ``start_y``
+
+    The baseline is located along the line the bottom of the text would
+    be written on, excluding letters with tails such as y:
+
+        .. figure:: ../images/text_anchor_y.png
+           :width: 40%
+
+           The blue line is the baseline for the string ``"Python"``
+
+    ``rotation`` allows for the text to be rotated around the anchor
+    point by the passed number of degrees. Positive values rotate
+    counter-clockwise from horizontal, while negative values rotate
+    clockwise:
+
+        .. figure:: ../images/text_rotation_degrees.png
+           :width: 55%
+
+           Rotation around the default anchor (
+           ``anchor_y="baseline"`` and ``anchor_x="left"``)
+
+    """
 
     def __init__(
         self,
@@ -128,6 +195,7 @@ class Text:
         multiline: bool = False,
         rotation: float = 0
     ):
+        """Build a text object"""
 
         if align != "center" and align != "left" and align != "right":
             raise ValueError("The 'align' parameter must be equal to 'left', 'right', or 'center'.")
@@ -155,13 +223,31 @@ class Text:
 
     @property
     def value(self) -> str:
+        """
+        The current value displayed.
+        """
         return self._label.text
 
     @value.setter
     def value(self, value: str):
+        """
+        Set the text of the label.
+
+        :param str value: a string to display.
+        """
         self._label.text = value
 
-    def draw(self):
+    def draw(self) -> None:
+        """
+        Draw this label to the screen at its current ``x`` and ``y`` position.
+
+        .. warning: Cameras affect text drawing!
+            If you want to draw a custom GUI that doesn't move with the
+            game world, you will need a second :py:class:`~arcade.Camera`
+            instance. For information on how to do this, see
+            :ref:`sprite_move_scrolling`.
+
+        """
         _draw_label_with_rotation(self._label, self.rotation)
 
 
@@ -182,23 +268,149 @@ def draw_text(
     rotation: float = 0,
 ):
     """
-    Draws text to the screen using Pyglet's label instead.
+    A simple way for beginners to draw text.
 
-    :param str text: Text to draw
-    :param float start_x:
-    :param float start_y:
-    :param Color color: Color of the text
-    :param float font_size: Size of the text
-    :param float width:
-    :param str align:
-    :param Union[str, Tuple[str, ...]] font_name:
-    :param bool bold:
-    :param bool italic:
-    :param str anchor_x:
-    :param str anchor_y:
-    :param bool multiline:
-    :param bool wrap_lines:
-    :param float rotation:
+    .. warning:: Cameras affect text drawing!
+
+        If you want to draw a custom GUI that doesn't move with the
+        game world, you will need a second camera. For information on
+        how to do this, see :ref:`sprite_move_scrolling`.
+
+    This function lets you start draw text easily with better
+    performance than the old pillow-based text. If you need even higher
+    performance, consider using :py:class:`~arcade.Text`.
+
+    :param str text: Text to display
+    :param float start_x: x position to align the text's anchor point with
+    :param float start_y: y position to align the text's anchor point with
+    :param Color color: Color of the text as a tuple or list of 3 (RGB) or 4 (RGBA) integers
+    :param float font_size: Size of the text in points
+    :param float width: A width limit in pixels
+    :param str align: Horizontal alignment; only used if width was given
+    :param Union[str, Tuple[str, ...]] font_name: A font name, path to a font file, or list of names
+    :param bool bold: Whether to draw the text as bold
+    :param bool italic: Whether to draw the text as italic
+    :param str anchor_x: How to calculate the anchor point's x coordinate
+    :param str anchor_y: How to calculate the anchor point's y coordinate
+    :param bool multiline: If width was set, enables word wrap rather than clipping
+    :param float rotation: rotation in degrees, counter-clockwise from horizontal
+
+    By default, the text is placed so that:
+
+    - the left edge of its bounding box is at ``start_x``
+    - its baseline is at ``start_y``
+
+    The baseline of text is the line it would be written on:
+
+        .. figure:: ../images/text_anchor_y.png
+           :width: 40%
+
+           The blue line is the baseline for the string ``"Python"``
+
+    ``font_name`` can be any of the following:
+
+    - a built-in font in the :ref:`Resources<Resources folder>`
+    - the name of a system font
+    - a path to a font on the system
+    - a `tuple` containing any mix of the previous three
+
+    Each entry provided will be tried in order until one is found. If
+    none of the fonts are found, a default font will be chosen (usually
+    Arial).
+
+    ``anchor_x`` and ``anchor_y`` specify how to calculate the anchor point,
+    which affects how the text is:
+
+    - Placed relative to ``start_x`` and ``start_y``
+    - Rotated
+
+    By default, the text is drawn so that ``start_x`` is at the left of
+    the text's bounding box and ``start_y`` is at the baseline.
+
+    You can set a custom anchor point by passing combinations of the
+    following values for ``anchor_x`` and ``anchor_y``:
+
+    .. list-table:: Values allowed by ``anchor_x``
+        :widths: 20 40 40
+        :header-rows: 1
+
+        * - String value
+          - Practical Effect
+          - Anchor Position
+
+        * - ``"left"`` `(default)`
+          - Text drawn with its left side at ``start_x``
+          - Anchor point at the left side of the text's bounding box
+
+        * - ``"center"``
+          - Text drawn horizontally centered on ``start_x``
+          - Anchor point at horizontal center of text's bounding box
+
+        * - ``"right"``
+          - Text drawn with its right side at ``start_x``
+          - Anchor placed at the right side of the text's bounding box
+
+
+    .. list-table:: Values allowed by ``anchor_y``
+        :widths: 20 40 40
+        :header-rows: 1
+
+        * - String value
+          - Practical Effect
+          - Anchor Position
+
+        * - ``"baseline"`` `(default)`
+          - Text drawn with baseline on ``start_y``.
+          - Anchor placed at the text rendering baseline
+
+        * - ``"top"``
+          - Text drawn with its top aligned with ``start_y``
+          - Anchor point placed at the top of the text
+
+        * - ``"bottom"``
+          - Text drawn with its absolute bottom aligned with ``start_y``,
+            including the space for tails on letters such as y and g
+          - Anchor point placed at the bottom of the text after the
+            space allotted for letters such as y and g
+
+        * - ``"center"``
+          - Text drawn with its vertical center on ``start_y``
+          - Anchor placed at the vertical center of the text
+
+
+    ``rotation`` allows for the text to be rotated around the anchor
+    point by the passed number of degrees. Positive values rotate
+    counter-clockwise from horizontal, while negative values rotate
+    clockwise:
+
+        .. figure:: ../images/text_rotation_degrees.png
+           :width: 55%
+
+           Rotation around the default anchor point (
+           ``anchor_y="baseline"`` and ``anchor_x="left"``)
+
+
+    It can be helpful to think of this function working as follows:
+
+    1. Text layout and alignment are calculated:
+
+        1. The text's characters are laid out within a bounding box
+           according to the current styling options
+
+        2. The anchor point on the text is calculated based on
+           the text value, styling, as well as values for ``anchor_x``
+           and ``anchor_y``
+
+    2. The text is placed so its anchor point is at ``(start_x,
+       start_y))`
+
+    3. The text is rotated around its anchor point before finally
+       being drawn
+
+    This function is less efficient than using :py:class:`~arcade.Text`
+    because some of the steps above can be repeated each time a call is
+    made rather than fully cached as with the class.
+
     """
     # See : https://github.com/pyglet/pyglet/blob/ff30eadc2942553c9de96d6ce564ad1bc3128fb4/pyglet/text/__init__.py#L401
 
@@ -209,6 +421,7 @@ def draw_text(
     label = cache.get(key)
     if align != "center" and align != "left" and align != "right":
         raise ValueError("The 'align' parameter must be equal to 'left', 'right', or 'center'.")
+
     if align != "left":
         multiline = True
 
@@ -243,4 +456,5 @@ def draw_text(
 
 # TODO: maybe remove, as this is invalid
 def create_text(*args, **kwargs):
+    """Legacy stub, returns a text object."""
     return Text("Hello")
