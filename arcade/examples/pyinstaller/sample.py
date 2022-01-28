865b42b474c77b75eaf547460a1795e26e51b492
Paul Vincent Craven <paul@cravenfamily.com>
2022-01-28 08:29:18 -0600

    Fix pysinstaller example broken in last check-in.

--- arcade/examples/pyinstaller/sample.py
+++ arcade/examples/pyinstaller/sample.py
@@ -19,7 +19,7 @@ def resource_path(file):
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
     arcade.set_background_color(BACKGROUND_COLOR)
-    self.clear()
+    arcade.start_render()
     arcade.draw_circle_filled(400, 250, 100, arcade.color.BLACK)
     # load image
     image = arcade.load_texture(resource_path('character.png'))
