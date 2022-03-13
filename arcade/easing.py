88931e6fd7877492da1b38ea30658ac7ff03b109
Aedan Andrews <happydog.aja@gmail.com>
2022-03-13 14:59:32 +1300

    Updated easing.py with new easing functions, plus gave EasingData a reset method.

--- arcade/easing.py
+++ arcade/easing.py
@@ -1,9 +1,9 @@
 """
 Functions used to support easing
 """
+from math import pi, sin, cos
 from dataclasses import dataclass
-from typing import Callable
-from typing import Tuple
+from typing import Callable, Tuple
 from .geometry_generic import get_distance
 
 
@@ -19,6 +19,9 @@ class EasingData:
     end_value: float
     ease_function: Callable
 
+    def reset(self):
+        self.cur_period = self.start_period
+
 
 def linear(percent: float) -> float:
     """
@@ -31,26 +34,108 @@ def _flip(percent: float) -> float:
     return 1.0 - percent
 
 
-def ease_in(percent: float) -> float:
-    """
-    Function for ease-in easing.
-    """
-    return percent * percent
-
-
-def ease_out(percent: float) -> float:
-    """
-    Function for ease-out easing.
-    """
-    return _flip(_flip(percent) * _flip(percent))
-
-
 def smoothstep(percent: float) -> float:
     """
     Function for smoothstep easing.
     """
     percent = percent * percent * (3.0 - 2.0 * percent)
     return percent
+
+
+def ease_in(percent: float) -> float:
+    """
+    Function for quadratic ease-in easing.
+    """
+    return percent * percent
+
+
+def ease_out(percent: float) -> float:
+    """
+    Function for quadratic ease-out easing.
+    """
+    return _flip(_flip(percent) * _flip(percent))
+
+
+def ease_in_out(percent: float) -> float:
+    """
+    Function for quadratic easing in and out.
+    """
+
+    return 2 * percent**2 if percent < 0.5 else 1 - (-2 * percent + 2)**2 / 2
+
+
+def ease_out_elastic(percent: float) -> float:
+    """
+    Function for elastic ease-out easing.
+    """
+    c4 = 2 * pi / 3
+    result = 0.0
+    if percent == 1:
+        result = 1
+    elif percent > 0:
+        result = (2 ** (-10 * percent)) * sin((percent * 10 - 0.75) * c4) + 1
+    return result
+
+
+def ease_out_bounce(percent: float) -> float:
+    """
+    Function for a bouncy ease-out easing.
+    """
+    n1 = 7.5625
+    d1 = 2.75
+
+    if percent < 1 / d1:
+        return n1 * percent * percent
+    elif percent < 2 / d1:
+        percent_modified = percent - 1.5/d1
+        return n1 * percent_modified * percent_modified + 0.75
+    elif percent < 2.5 / d1:
+        percent_modified = percent - 2.25/d1
+        return n1 * percent_modified * percent_modified + 0.9375
+    else:
+        percent_modified = percent - 2.625/d1
+        return n1 * percent_modified * percent_modified + 0.984375
+
+
+def ease_in_back(percent: float) -> float:
+    """
+    Function for ease_in easing which moves back before moving forward.
+    """
+    c1 = 1.70158
+    c3 = c1 + 1
+
+    return c3 * percent * percent * percent - c1 * percent * percent
+
+
+def ease_out_back(percent: float) -> float:
+    """
+    Function for ease_out easing which moves back before moving forward.
+    """
+    c1 = 1.70158
+    c3 = c1 + 1
+
+    return 1 + c3 * pow(percent - 1, 3) + c1 * pow(percent - 1, 2)
+
+
+def ease_in_sin(percent: float) -> float:
+    """
+    Function for ease_in easing using a sin wave
+    """
+    return 1 - cos((percent * pi) / 2)
+
+
+def ease_out_sin(percent: float) -> float:
+    """
+    Function for ease_out easing using a sin wave
+    """
+    return sin((percent * pi) / 2)
+
+
+def ease_in_out_sin(percent: float) -> float:
+    """
+    Function for easing in and out using a sin wave
+    """
+    return -cos(percent * pi)*0.5 + 0.5
 
 
 def easing(percent: float, easing_data: EasingData) -> float:
