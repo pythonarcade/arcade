e6aa5e6f81c5ebf27b5dfb1f2db686b690b17b62
einarf <eforselv@gmail.com>
2022-03-24 00:53:18 +0100

    Window: Make window title optional

--- arcade/application.py
+++ arcade/application.py
@@ -75,23 +75,23 @@ class Window(pyglet.window.Window):
     """
 
     def __init__(
-            self,
-            width: int = 800,
-            height: int = 600,
-            title: str = 'Arcade Window',
-            fullscreen: bool = False,
-            resizable: bool = False,
-            update_rate: Optional[float] = 1 / 60,
-            antialiasing: bool = True,
-            gl_version: Tuple[int, int] = (3, 3),
-            screen: pyglet.canvas.Screen = None,
-            style: Optional[str] = pyglet.window.Window.WINDOW_STYLE_DEFAULT,
-            visible: bool = True,
-            vsync: bool = False,
-            gc_mode: str = "context_gc",
-            center_window: bool = False,
-            samples: int = 4,
-            enable_polling: bool = True
+        self,
+        width: int = 800,
+        height: int = 600,
+        title: Optional[str] = 'Arcade Window',
+        fullscreen: bool = False,
+        resizable: bool = False,
+        update_rate: Optional[float] = 1 / 60,
+        antialiasing: bool = True,
+        gl_version: Tuple[int, int] = (3, 3),
+        screen: pyglet.canvas.Screen = None,
+        style: Optional[str] = pyglet.window.Window.WINDOW_STYLE_DEFAULT,
+        visible: bool = True,
+        vsync: bool = False,
+        gc_mode: str = "context_gc",
+        center_window: bool = False,
+        samples: int = 4,
+        enable_polling: bool = True
     ):
         # In certain environments we can't have antialiasing/MSAA enabled.
         # Detect replit environment
