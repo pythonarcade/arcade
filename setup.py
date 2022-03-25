d08603135d7f17bf86bd6a16cd0ed148844ac346
Einar Forselv <eforselv@gmail.com>
2022-03-25 15:16:39 +0100

    Bump pyglet to 2.0a1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     url="https://api.arcade.academy",
     download_url="https://api.arcade.academy",
     install_requires=[
-        "pyglet==2.0.dev13",
+        "pyglet==2.0a1",
         "pillow~=9.0.0",
         "pymunk~=6.2.1",
         "pytiled-parser==2.0.1",
