a0213acc263dd3d89da9ffb4abd19f3d7e43e79a
Einar Forselv <eforselv@gmail.com>
2022-03-27 20:01:02 +0200

    Examples: timer.py should use Text object

--- arcade/examples/timer.py
+++ arcade/examples/timer.py
@@ -4,7 +4,6 @@ Show a timer on-screen.
 If Python and Arcade are installed, this example can be run from the command line with:
 python -m arcade.examples.timer
 """
-
 import arcade
 
 SCREEN_WIDTH = 800
@@ -20,7 +19,14 @@ class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         self.total_time = 0.0
-        self.output = "00:00:00"
+        self.timer_text = arcade.Text(
+            text="00:00:00",
+            start_x=SCREEN_WIDTH // 2,
+            start_y=SCREEN_HEIGHT // 2 - 50,
+            color=arcade.color.WHITE,
+            font_size=100,
+            anchor_x="center",
+        )
 
     def setup(self):
         """
@@ -31,21 +37,17 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         """ Use this function to draw everything to the screen. """
-
-        # Start the render. This must happen before any drawing
-        # commands. We do NOT need an stop render command.
+        # Clear all pixels in the window
         self.clear()
 
-        # Output the timer text.
-        arcade.draw_text(self.output,
-                         SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 50,
-                         arcade.color.WHITE, 100,
-                         anchor_x="center")
+        # Draw the timer text
+        self.timer_text.draw()
 
     def on_update(self, delta_time):
         """
         All the logic to move, and the game logic goes here.
         """
+        # Accumulate the total time
         self.total_time += delta_time
 
         # Calculate minutes
@@ -57,8 +59,8 @@ class MyGame(arcade.Window):
         # Calculate 100s of a second
         seconds_100s = int((self.total_time - seconds) * 100)
 
-        # Figure out our output
-        self.output = f"{minutes:02d}:{seconds:02d}:{seconds_100s:02d}"
+        # Use string formatting to create a new text string for our timer
+        self.timer_text.text = f"{minutes:02d}:{seconds:02d}:{seconds_100s:02d}"
 
 
 def main():
