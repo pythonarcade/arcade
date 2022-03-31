d3339bb4c7f9edb7e3b77291d8f2060ddcbab093
Darren Eberly <darren.eberly@gmail.com>
2022-03-30 23:21:39 -0400

    Disable context point size

--- arcade/context.py
+++ arcade/context.py
@@ -189,7 +189,7 @@ class ArcadeContext(Context):
         self.pyglet_label_cache: Dict[str, pyglet.text.Label] = {}
 
         self.active_program = None
-        self.point_size = 1.0
+        #self.point_size = 1.0
 
     def reset(self) -> None:
         """
