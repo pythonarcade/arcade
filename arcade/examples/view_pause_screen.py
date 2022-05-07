a3bc7ab7fac2be995b19230626c5c8c82c63b04f
Einar Forselv <eforselv@gmail.com>
2022-05-07 11:34:04 +0200

    Remove dead code in example

--- arcade/examples/view_pause_screen.py
+++ arcade/examples/view_pause_screen.py
@@ -12,12 +12,6 @@ python -m arcade.examples.view_pause_screen
 """
 
 import arcade
-import os
-
-
-file_path = os.path.dirname(os.path.abspath(__file__))
-os.chdir(file_path)
-
 
 WIDTH = 800
 HEIGHT = 600
