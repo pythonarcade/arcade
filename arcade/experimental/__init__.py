e67c722d65ba07f644a260c4b7459821a9b53dcb
Einar Forselv <eforselv@gmail.com>
2022-05-08 23:42:06 +0200

    Import ShadertoyBase in experimental

--- arcade/experimental/__init__.py
+++ arcade/experimental/__init__.py
@@ -3,6 +3,6 @@ Experimental stuff. API may change.
 """
 # flake8: noqa
 from .texture_render_target import RenderTargetTexture
-from .shadertoy import Shadertoy, ShadertoyBuffer
+from .shadertoy import Shadertoy, ShadertoyBuffer, ShadertoyBase
 from .crt_filter import CRTFilter
 from .bloom_filter import BloomFilter
