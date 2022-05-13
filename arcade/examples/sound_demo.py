004df798e6bdbe8613407c9ef0bff412913ee255
Ian <iansedano@gmail.com>
2022-05-13 23:19:30 +0200

    simplyfying sound button creation and clean up

--- arcade/examples/sound_demo.py
+++ arcade/examples/sound_demo.py
@@ -1,11 +1,5 @@
 """
-Starting Template
-
-Once you have learned how to use classes, you can begin your program with this
-template.
-
-If Python and Arcade are installed, this example can be run from the command
-line with:
+Sound Panning Demo
 
 python -m arcade.examples.sound_demo
 
@@ -23,8 +17,11 @@ import arcade
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Starting Template"
+SCREEN_TITLE = "Sound Panning Demo"
 BUTTON_SIZE = 30
+
+SOUND_PANNING = [-1.0, -0.5, 0.0, 0.5, 1.0]
+assert len(SOUND_PANNING) == 5
 
 
 class SoundButton(arcade.SpriteSolidColor):
@@ -74,19 +71,9 @@ class AudioStreamButton(arcade.SpriteSolidColor):
 
 
 class MyGame(arcade.Window):
-    """
-    Main application class.
-
-    NOTE: Go ahead and delete the methods you don't need.
-    If you do need a method, delete the 'pass' and replace it
-    with your own code. Don't leave 'pass' in this program.
-    """
-
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-
         arcade.set_background_color(arcade.color.AMAZON)
-
         self.button_sprites = None
 
     def setup(self):
@@ -102,83 +89,42 @@ class MyGame(arcade.Window):
         self.button_sprites.append(button)
 
         upgrade_sound = ":resources:sounds/upgrade4.wav"
-
-        # fmt: off
-        button_params = [
-        #    sound        , pan  , vol , x                          , y
-            [upgrade_sound, -1.0 , 0.1 , BUTTON_SIZE                , SCREEN_HEIGHT / 2 + 50 ],
-            [upgrade_sound, -0.5 , 0.1 , SCREEN_WIDTH / 4           , SCREEN_HEIGHT / 2 + 50 ],
-            [upgrade_sound,  0   , 0.1 , SCREEN_WIDTH / 2           , SCREEN_HEIGHT / 2 + 50 ],
-            [upgrade_sound,  0.5 , 0.1 , SCREEN_WIDTH / 4 * 3       , SCREEN_HEIGHT / 2 + 50 ],
-            [upgrade_sound,  1.0 , 0.1 , SCREEN_WIDTH - BUTTON_SIZE , SCREEN_HEIGHT / 2 + 50 ],
-            [upgrade_sound, -1.0 , 0.5 , BUTTON_SIZE                , SCREEN_HEIGHT / 2      ],
-            [upgrade_sound, -0.5 , 0.5 , SCREEN_WIDTH / 4           , SCREEN_HEIGHT / 2      ],
-            [upgrade_sound,  0   , 0.5 , SCREEN_WIDTH / 2           , SCREEN_HEIGHT / 2      ],
-            [upgrade_sound,  0.5 , 0.5 , SCREEN_WIDTH / 4 * 3       , SCREEN_HEIGHT / 2      ],
-            [upgrade_sound,  1.0 , 0.5 , SCREEN_WIDTH - BUTTON_SIZE , SCREEN_HEIGHT / 2      ],
-            [upgrade_sound, -1.0 , 1   , BUTTON_SIZE                , SCREEN_HEIGHT / 2 - 50 ],
-            [upgrade_sound, -0.5 , 1   , SCREEN_WIDTH / 4           , SCREEN_HEIGHT / 2 - 50 ],
-            [upgrade_sound,  0   , 1   , SCREEN_WIDTH / 2           , SCREEN_HEIGHT / 2 - 50 ],
-            [upgrade_sound,  0.5 , 1   , SCREEN_WIDTH / 4 * 3       , SCREEN_HEIGHT / 2 - 50 ],
-            [upgrade_sound,  1.0 , 1   , SCREEN_WIDTH - BUTTON_SIZE , SCREEN_HEIGHT / 2 - 50 ],
+        volume_variation = [0.1, 0.5, 1]
+        y_offset = [50, 0, -50]
+        button_x_spread = [
+            BUTTON_SIZE,
+            SCREEN_WIDTH / 4,
+            SCREEN_WIDTH / 2,
+            SCREEN_WIDTH / 4 * 3,
+            SCREEN_WIDTH - BUTTON_SIZE,
         ]
-        # fmt: on
 
-        self.button_sprites.extend([SoundButton(*param) for param in button_params])
+        for vol, y_offset in zip(volume_variation, y_offset):
+            for pan_setting, x_pos in zip(SOUND_PANNING, button_x_spread):
+                self.button_sprites.append(
+                    SoundButton(
+                        upgrade_sound,
+                        pan_setting,
+                        vol,
+                        x_pos,
+                        SCREEN_HEIGHT / 2 + y_offset,
+                    )
+                )
 
     def on_draw(self):
-        """
-        Render the screen.
-        """
-
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
         self.clear()
-
-        # Call draw() on all your sprite lists below
         self.button_sprites.draw()
 
     def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
         self.button_sprites.update()
 
-    def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
-
-        For a full list of keys, see:
-        https://api.arcade.academy/en/latest/arcade.key.html
-        """
-        pass
-
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
     def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
         hit_sprites = arcade.get_sprites_at_point((x, y), self.button_sprites)
         for sprite in hit_sprites:
             button_sprite = typing.cast(SoundButton, sprite)
             if button == arcade.MOUSE_BUTTON_LEFT:
                 button_sprite.play()
-            elif (
-                button == arcade.MOUSE_BUTTON_RIGHT
-            ):  # right click to increase volume on currently playing sound
+            elif button == arcade.MOUSE_BUTTON_RIGHT:
                 if not button_sprite.sound.is_complete():
                     button_sprite.volume_up()
                     button_sprite.stream_position()
@@ -187,15 +133,8 @@ class MyGame(arcade.Window):
                     button_sprite.volume_down()
                     button_sprite.stream_position()
 
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
-
 
 def main():
-    """Main function"""
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     game.setup()
     arcade.run()
--- arcade/examples/sound_speed_demo.py
+++ arcade/examples/sound_speed_demo.py
@@ -1,16 +1,10 @@
 """
-Starting Template
+Sound Speed Demo
 
-Once you have learned how to use classes, you can begin your program with this
-template.
-
-If Python and Arcade are installed, this example can be run from the command
-line with:
-
-python -m arcade.examples.sound_demo
+python -m arcade.examples.sound_speed_demo
 
 The top button is to play a music track.
-The 3 rows of buttons are arranged such that the audio is panned in the
+The 3 rows of buttons are arranged such that the audio is sped up in the
 direction of the button, and the volume increases as you go down the column.
 
 Left click a button to play a sound. 
@@ -23,8 +17,13 @@ import arcade
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Starting Template"
+SCREEN_TITLE = "Sound Speed Demo"
 BUTTON_SIZE = 30
+
+SPEED_VARIATION = [0.1, 0.5, 1.0, 2.0, 4.0]
+assert len(SPEED_VARIATION) == 5
+for speed in SPEED_VARIATION:
+    assert speed > 0
 
 
 class SoundButton(arcade.SpriteSolidColor):
@@ -47,14 +46,6 @@ class SoundButton(arcade.SpriteSolidColor):
 
 
 class MyGame(arcade.Window):
-    """
-    Main application class.
-
-    NOTE: Go ahead and delete the methods you don't need.
-    If you do need a method, delete the 'pass' and replace it
-    with your own code. Don't leave 'pass' in this program.
-    """
-
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
@@ -65,86 +56,44 @@ class MyGame(arcade.Window):
     def setup(self):
         self.button_sprites = arcade.SpriteList()
 
-        upgrade_sound = ":resources:sounds/gameover3.wav"
+        game_over_sound = ":resources:sounds/gameover3.wav"
 
-        # fmt: off
-        button_params = [
-        #    sound        , speed, vol   , x                          , y
-            [upgrade_sound, 0.1 , 0.1   , BUTTON_SIZE                , SCREEN_HEIGHT / 2 + 50 ],
-            [upgrade_sound, 0.5 , 0.1   , SCREEN_WIDTH / 4           , SCREEN_HEIGHT / 2 + 50 ],
-            [upgrade_sound, 1.0 , 0.1   , SCREEN_WIDTH / 2           , SCREEN_HEIGHT / 2 + 50 ],
-            [upgrade_sound, 2.0 , 0.1   , SCREEN_WIDTH / 4 * 3       , SCREEN_HEIGHT / 2 + 50 ],
-            [upgrade_sound, 4.0 , 0.1   , SCREEN_WIDTH - BUTTON_SIZE , SCREEN_HEIGHT / 2 + 50 ],
-            
-            [upgrade_sound, 0.1 , 0.5   , BUTTON_SIZE                , SCREEN_HEIGHT / 2      ],
-            [upgrade_sound, 0.5 , 0.5   , SCREEN_WIDTH / 4           , SCREEN_HEIGHT / 2      ],
-            [upgrade_sound, 1.0 , 0.5   , SCREEN_WIDTH / 2           , SCREEN_HEIGHT / 2      ],
-            [upgrade_sound, 2.0 , 0.5   , SCREEN_WIDTH / 4 * 3       , SCREEN_HEIGHT / 2      ],
-            [upgrade_sound, 4.0 , 0.5   , SCREEN_WIDTH - BUTTON_SIZE , SCREEN_HEIGHT / 2      ],
-            
-            [upgrade_sound, 0.1 , 1     , BUTTON_SIZE                , SCREEN_HEIGHT / 2 - 50 ],
-            [upgrade_sound, 0.5 , 1     , SCREEN_WIDTH / 4           , SCREEN_HEIGHT / 2 - 50 ],
-            [upgrade_sound, 1.0 , 1     , SCREEN_WIDTH / 2           , SCREEN_HEIGHT / 2 - 50 ],
-            [upgrade_sound, 2.0 , 1     , SCREEN_WIDTH / 4 * 3       , SCREEN_HEIGHT / 2 - 50 ],
-            [upgrade_sound, 4.0 , 1     , SCREEN_WIDTH - BUTTON_SIZE , SCREEN_HEIGHT / 2 - 50 ],
+        volume_variation = [0.1, 0.5, 1]
+        y_offset = [50, 0, -50]
+        button_x_spread = [
+            BUTTON_SIZE,
+            SCREEN_WIDTH / 4,
+            SCREEN_WIDTH / 2,
+            SCREEN_WIDTH / 4 * 3,
+            SCREEN_WIDTH - BUTTON_SIZE,
         ]
-        # fmt: on
 
-        self.button_sprites.extend([SoundButton(*param) for param in button_params])
+        for vol, y_offset in zip(volume_variation, y_offset):
+            for speed_setting, x_pos in zip(SPEED_VARIATION, button_x_spread):
+                self.button_sprites.append(
+                    SoundButton(
+                        game_over_sound,
+                        speed_setting,
+                        vol,
+                        x_pos,
+                        SCREEN_HEIGHT / 2 + y_offset,
+                    )
+                )
 
     def on_draw(self):
-        """
-        Render the screen.
-        """
-
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
         self.clear()
-
-        # Call draw() on all your sprite lists below
         self.button_sprites.draw()
 
     def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
         self.button_sprites.update()
 
-    def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
-
-        For a full list of keys, see:
-        https://api.arcade.academy/en/latest/arcade.key.html
-        """
-        pass
-
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
     def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
         hit_sprites = arcade.get_sprites_at_point((x, y), self.button_sprites)
         for sprite in hit_sprites:
             button_sprite = typing.cast(SoundButton, sprite)
             if button == arcade.MOUSE_BUTTON_LEFT:
                 button_sprite.play()
-            elif (
-                button == arcade.MOUSE_BUTTON_RIGHT
-            ):  # right click to increase volume on currently playing sound
+            elif button == arcade.MOUSE_BUTTON_RIGHT:
                 if not button_sprite.sound.is_complete():
                     button_sprite.volume_up()
                     button_sprite.stream_position()
@@ -153,15 +102,8 @@ class MyGame(arcade.Window):
                     button_sprite.volume_down()
                     button_sprite.stream_position()
 
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
-
 
 def main():
-    """Main function"""
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     game.setup()
     arcade.run()
