952d3628176824b70bd6ce90da708f3646be1da7
Ibrahim <cshore.2750mi@gmail.com>
2022-05-14 13:31:36 +0530

    cleaned up and added test

--- tests/test_gui/test_layouting_gridlayout.py
+++ tests/test_gui/test_layouting_gridlayout.py
@@ -74,3 +74,35 @@ def test_place_widget_within_content_rect(window):
     subject.do_layout()
 
     assert dummy1.position == (10, 20)
+
+
+def test_place_widgets_with_col_row_span(window):
+    dummy1 = UIDummy(width=100, height=100)
+    dummy2 = UIDummy(width=100, height=100)
+    dummy3 = UIDummy(width=100, height=100)
+    dummy4 = UIDummy(width=100, height=100)
+    dummy5 = UIDummy(width=200, height=100)
+    dummy6 = UIDummy(width=100, height=200)
+
+    subject = UIGridLayout(
+            column_count=3,
+            row_count=3,
+            )
+
+    subject.add(dummy1, 0, 0)
+    subject.add(dummy2, 0, 1)
+    subject.add(dummy3, 1, 0)
+    subject.add(dummy4, 1, 1)
+    subject.add(dummy5, 0, 2, col_span=2)
+    subject.add(dummy6, 2, 0, row_span=3)
+
+
+    subject.rect = Rect(0, 0, *subject.size_hint_min)
+    subject.do_layout()
+
+    assert dummy1.position == (0, 200)
+    assert dummy2.position == (0, 100)
+    assert dummy3.position == (100, 200)
+    assert dummy4.position == (100, 100)
+    assert dummy5.position == (0, 0)
+    assert dummy6.position == (200, 50)
