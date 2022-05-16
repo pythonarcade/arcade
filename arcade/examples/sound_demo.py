cbe287a46b93706d0e1a9614bb7372761e523e26
pushfoo <36696816+pushfoo@users.noreply.github.com>
2022-05-16 19:36:48 -0400

    Finish sound speed issue (#1198) (#1218)
    * Add sound speed example to sphinx build
    
    * Simplify sound_demo.py to fix style and pyglet crash
    
    * Remove dead code & improve clarity for sound_speed_demo
    
    * Rephrase docstrings for sound_demo

--- arcade/examples/sound_demo.py
+++ arcade/examples/sound_demo.py
@@ -1,14 +1,21 @@
 """
 Sound Panning Demo
 
+If Python and Arcade are installed, this example can be run from the
+command line with:
 python -m arcade.examples.sound_demo
 
-The top button is to play a music track.
-The 3 rows of buttons are arranged such that the audio is panned in the
-direction of the button, and the volume increases as you go down the column.
+Each button plays a sound when clicked.
 
-Left click a button to play a sound. 
-If a sound is playing right click to increase volume, middle click to decrease.
+The top left button plays a streaming music track when pressed. If you
+click it while it's already playing, it will intentionally crash the
+demo to demonstrate how you shouldn't try to play a streaming sound
+that's already playing.
+
+The lower 3 rows of buttons play a non-streaming (static) sound with
+different panning and volume. Going from left to right changes the
+panning, which is how much the sound plays in the left speaker vs the
+right speaker. Lower rows play the sound louder than the higher ones.
 """
 
 import typing
@@ -20,51 +27,53 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Sound Panning Demo"
 BUTTON_SIZE = 30
 
+
 SOUND_PANNING = [-1.0, -0.5, 0.0, 0.5, 1.0]
-assert len(SOUND_PANNING) == 5
+BUTTON_X_POSITIONS = [
+    BUTTON_SIZE,
+    SCREEN_WIDTH / 4,
+    SCREEN_WIDTH / 2,
+    SCREEN_WIDTH / 4 * 3,
+    SCREEN_WIDTH - BUTTON_SIZE,
+]
+
+
+VOLUME_VARIATION = [0.1, 0.5, 1]
+Y_OFFSETS = [50, 0, -50]
 
 
 class SoundButton(arcade.SpriteSolidColor):
-    """Button, click-for-sound"""
+    """
+    A sprite that stores settings about how to play a sound.
 
-    def __init__(self, sound_file, pan, volume, center_x, center_y):
+    This class can load a sound as either a static sound or a streaming
+    sound. Streaming should be used for long files that will only have
+    one instance playing, such as music or ambiance tracks.
+
+    If you try to play a sound created with streaming=True while it is
+    already playing, it will raise an exception! Non-streaming (static)
+    sounds are fine with it, and can have play() called on them as many
+    times as you want.
+    """
+
+    def __init__(
+        self,
+        sound_file,
+        pan=0.5,
+        volume=0.5,
+        center_x=0,
+        center_y=0,
+        streaming=False
+    ):
         super().__init__(BUTTON_SIZE, BUTTON_SIZE, arcade.color.WHITE)
-        self.sound = arcade.Sound(sound_file)
+        self.sound = arcade.Sound(sound_file, streaming=streaming)
         self.pan = pan
         self.volume = volume
         self.center_x = center_x
         self.center_y = center_y
 
     def play(self):
-        """Play"""
         self.sound.play(pan=self.pan, volume=self.volume)
-
-
-class AudioStreamButton(arcade.SpriteSolidColor):
-    """Button, click-for-streaming-sound"""
-
-    def __init__(self, sound_file, pan, volume):
-        super().__init__(BUTTON_SIZE, BUTTON_SIZE, arcade.color.WHITE)
-        self.sound = arcade.Sound(sound_file, streaming=True)
-        self.pan = pan
-        self.volume = volume
-
-    def play(self):
-        """Play"""
-        self.sound.play(volume=self.volume, pan=self.pan)
-
-    def volume_up(self):
-        vol = self.sound.get_volume()
-        self.sound.set_volume(vol + 0.1)
-        print(f"Volume: {self.sound.get_volume()}")
-
-    def volume_down(self):
-        vol = self.sound.get_volume()
-        self.sound.set_volume(vol - 0.1)
-        print(f"Volume: {self.sound.get_volume()}")
-
-    def stream_position(self):
-        print(f"Current position: {self.sound.get_stream_position()}")
 
 
 class MyGame(arcade.Window):
@@ -76,31 +85,27 @@ class MyGame(arcade.Window):
     def setup(self):
         self.button_sprites = arcade.SpriteList()
 
-        y = SCREEN_HEIGHT / 2 + 150
-        volume = 0.1
-        button = AudioStreamButton(
-            ":resources:music/funkyrobot.mp3", pan=-1.0, volume=volume
+        # create the streaming button at the top left
+        self.button_sprites.append(
+            SoundButton(
+                ":resources:music/funkyrobot.mp3",
+                pan=-1.0,
+                volume=0.1,
+                center_x=BUTTON_SIZE,
+                center_y=SCREEN_HEIGHT / 2 + 150,
+                streaming=True
+            )
         )
-        button.center_x = BUTTON_SIZE
-        button.center_y = y
-        self.button_sprites.append(button)
 
-        upgrade_sound = ":resources:sounds/upgrade4.wav"
-        volume_variation = [0.1, 0.5, 1]
-        y_offset = [50, 0, -50]
-        button_x_spread = [
-            BUTTON_SIZE,
-            SCREEN_WIDTH / 4,
-            SCREEN_WIDTH / 2,
-            SCREEN_WIDTH / 4 * 3,
-            SCREEN_WIDTH - BUTTON_SIZE,
-        ]
-
-        for vol, y_offset in zip(volume_variation, y_offset):
-            for pan_setting, x_pos in zip(SOUND_PANNING, button_x_spread):
+        # Position the grid of buttons
+        # The zip function takes pieces from iterables and returns them
+        # as tuples. For more information, see the python doc:
+        # https://docs.python.org/3/library/functions.html#zip
+        for vol, y_offset in zip(VOLUME_VARIATION, Y_OFFSETS):
+            for pan_setting, x_pos in zip(SOUND_PANNING, BUTTON_X_POSITIONS):
                 self.button_sprites.append(
                     SoundButton(
-                        upgrade_sound,
+                        ":resources:sounds/upgrade4.wav",
                         pan_setting,
                         vol,
                         x_pos,
@@ -121,14 +126,6 @@ class MyGame(arcade.Window):
             button_sprite = typing.cast(SoundButton, sprite)
             if button == arcade.MOUSE_BUTTON_LEFT:
                 button_sprite.play()
-            elif button == arcade.MOUSE_BUTTON_RIGHT:
-                if not button_sprite.sound.is_complete():
-                    button_sprite.volume_up()
-                    button_sprite.stream_position()
-            elif button == arcade.MOUSE_BUTTON_MIDDLE:
-                if not button_sprite.sound.is_complete():
-                    button_sprite.volume_down()
-                    button_sprite.stream_position()
 
 
 def main():
--- arcade/examples/sound_speed_demo.py
+++ arcade/examples/sound_speed_demo.py
@@ -1,33 +1,51 @@
 """
 Sound Speed Demo
 
+If Python and Arcade are installed, this example can be run from the
+command line with:
 python -m arcade.examples.sound_speed_demo
 
-The top button is to play a music track.
-The 3 rows of buttons are arranged such that the audio is sped up in the
-direction of the button, and the volume increases as you go down the column.
-
 Left click a button to play a sound. 
-If a sound is playing right click to increase volume, middle click to decrease.
+
+Each button plays the same sound sample in a slightly different way.
+
+The middle buttons play at normal speed. The ones to the left play
+slower, and the ones to the right play faster. The buttons higher on
+the screen are quieter, while the ones further down are louder.
 """
 
 import typing
 
 import arcade
 
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Sound Speed Demo"
 BUTTON_SIZE = 30
 
+
 SPEED_VARIATION = [0.1, 0.5, 1.0, 2.0, 4.0]
-assert len(SPEED_VARIATION) == 5
-for speed in SPEED_VARIATION:
-    assert speed > 0
+BUTTON_X_POSITIONS = [
+    BUTTON_SIZE,
+    SCREEN_WIDTH / 4,
+    SCREEN_WIDTH / 2,
+    SCREEN_WIDTH / 4 * 3,
+    SCREEN_WIDTH - BUTTON_SIZE,
+]
+
+
+VOLUME_VARIATION = [0.1, 0.5, 1]
+Y_OFFSETS = [50, 0, -50]
 
 
 class SoundButton(arcade.SpriteSolidColor):
-    """Button, click-for-sound"""
+    """
+    A sprite that stores settings about how to play a sound.
+
+    You can tell it to play a sound faster or slower, as well as adjust
+    the volume of the sound.
+    """
 
     def __init__(self, sound_file, speed, volume, center_x, center_y):
         super().__init__(BUTTON_SIZE, BUTTON_SIZE, arcade.color.WHITE)
@@ -38,7 +56,6 @@ class SoundButton(arcade.SpriteSolidColor):
         self.center_y = center_y
 
     def play(self):
-        """Play"""
         self.sound.play(speed=self.speed, volume=self.volume)
 
 
@@ -47,30 +64,21 @@ class MyGame(arcade.Window):
         super().__init__(width, height, title)
 
         arcade.set_background_color(arcade.color.AMAZON)
-
         self.button_sprites = None
 
     def setup(self):
         self.button_sprites = arcade.SpriteList()
 
-        game_over_sound = ":resources:sounds/gameover3.wav"
-
-        volume_variation = [0.1, 0.5, 1]
-        y_offset = [50, 0, -50]
-        button_x_spread = [
-            BUTTON_SIZE,
-            SCREEN_WIDTH / 4,
-            SCREEN_WIDTH / 2,
-            SCREEN_WIDTH / 4 * 3,
-            SCREEN_WIDTH - BUTTON_SIZE,
-        ]
-
-        for vol, y_offset in zip(volume_variation, y_offset):
-            for speed_setting, x_pos in zip(SPEED_VARIATION, button_x_spread):
+        # Position the grid of buttons
+        # The zip function takes pieces from iterables and returns them
+        # as tuples. For more information, you can see the python doc:
+        # https://docs.python.org/3/library/functions.html#zip
+        for vol, y_offset in zip(VOLUME_VARIATION, Y_OFFSETS):
+            for speed, x_pos in zip(SPEED_VARIATION, BUTTON_X_POSITIONS):
                 self.button_sprites.append(
                     SoundButton(
-                        game_over_sound,
-                        speed_setting,
+                        ":resources:sounds/gameover3.wav",
+                        speed,
                         vol,
                         x_pos,
                         SCREEN_HEIGHT / 2 + y_offset,
@@ -90,14 +98,6 @@ class MyGame(arcade.Window):
             button_sprite = typing.cast(SoundButton, sprite)
             if button == arcade.MOUSE_BUTTON_LEFT:
                 button_sprite.play()
-            elif button == arcade.MOUSE_BUTTON_RIGHT:
-                if not button_sprite.sound.is_complete():
-                    button_sprite.volume_up()
-                    button_sprite.stream_position()
-            elif button == arcade.MOUSE_BUTTON_MIDDLE:
-                if not button_sprite.sound.is_complete():
-                    button_sprite.volume_down()
-                    button_sprite.stream_position()
 
 
 def main():
