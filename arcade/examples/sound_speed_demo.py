9d2082dcf415e1b8d48096902291725e9d3174ef
Ian Currie <iansedano@gmail.com>
2022-05-16 22:49:34 +0200

    Rename pitch to speed, added example, and tweaked existing sound example (#1216)
    * rename pitch to speed and examples
    
    * simplyfying sound button creation and clean up
    
    * wrapping long comment

--- arcade/examples/sound_speed_demo.py
+++ arcade/examples/sound_speed_demo.py
@@ -1,10 +1,10 @@
 """
-Sound Panning Demo
+Sound Speed Demo
 
-python -m arcade.examples.sound_demo
+python -m arcade.examples.sound_speed_demo
 
 The top button is to play a music track.
-The 3 rows of buttons are arranged such that the audio is panned in the
+The 3 rows of buttons are arranged such that the audio is sped up in the
 direction of the button, and the volume increases as you go down the column.
 
 Left click a button to play a sound. 
@@ -17,78 +17,47 @@ import arcade
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Sound Panning Demo"
+SCREEN_TITLE = "Sound Speed Demo"
 BUTTON_SIZE = 30
 
-SOUND_PANNING = [-1.0, -0.5, 0.0, 0.5, 1.0]
-assert len(SOUND_PANNING) == 5
+SPEED_VARIATION = [0.1, 0.5, 1.0, 2.0, 4.0]
+assert len(SPEED_VARIATION) == 5
+for speed in SPEED_VARIATION:
+    assert speed > 0
 
 
 class SoundButton(arcade.SpriteSolidColor):
     """Button, click-for-sound"""
 
-    def __init__(self, sound_file, pan, volume, center_x, center_y):
+    def __init__(self, sound_file, speed, volume, center_x, center_y):
         super().__init__(BUTTON_SIZE, BUTTON_SIZE, arcade.color.WHITE)
         self.sound = arcade.Sound(sound_file)
-        self.pan = pan
+        self.speed = speed
         self.volume = volume
         self.center_x = center_x
         self.center_y = center_y
 
     def play(self):
         """Play"""
-        self.sound.play(pan=self.pan, volume=self.volume)
+        self.sound.play(speed=self.speed, volume=self.volume)
 
     def draw(self):
         super().draw(self)
 
 
-class AudioStreamButton(arcade.SpriteSolidColor):
-    """Button, click-for-streaming-sound"""
-
-    def __init__(self, sound_file, pan, volume):
-        super().__init__(BUTTON_SIZE, BUTTON_SIZE, arcade.color.WHITE)
-        self.sound = arcade.Sound(sound_file, streaming=True)
-        self.pan = pan
-        self.volume = volume
-
-    def play(self):
-        """Play"""
-        self.sound.play(volume=self.volume, pan=self.pan)
-
-    def volume_up(self):
-        vol = self.sound.get_volume()
-        self.sound.set_volume(vol + 0.1)
-        print(f"Volume: {self.sound.get_volume()}")
-
-    def volume_down(self):
-        vol = self.sound.get_volume()
-        self.sound.set_volume(vol - 0.1)
-        print(f"Volume: {self.sound.get_volume()}")
-
-    def stream_position(self):
-        print(f"Current position: {self.sound.get_stream_position()}")
-
-
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
+
         arcade.set_background_color(arcade.color.AMAZON)
+
         self.button_sprites = None
 
     def setup(self):
         self.button_sprites = arcade.SpriteList()
 
-        y = SCREEN_HEIGHT / 2 + 150
-        volume = 0.1
-        button = AudioStreamButton(
-            ":resources:music/funkyrobot.mp3", pan=-1.0, volume=volume
-        )
-        button.center_x = BUTTON_SIZE
-        button.center_y = y
-        self.button_sprites.append(button)
+        game_over_sound = ":resources:sounds/gameover3.wav"
 
-        upgrade_sound = ":resources:sounds/upgrade4.wav"
         volume_variation = [0.1, 0.5, 1]
         y_offset = [50, 0, -50]
         button_x_spread = [
@@ -100,11 +69,11 @@ class MyGame(arcade.Window):
         ]
 
         for vol, y_offset in zip(volume_variation, y_offset):
-            for pan_setting, x_pos in zip(SOUND_PANNING, button_x_spread):
+            for speed_setting, x_pos in zip(SPEED_VARIATION, button_x_spread):
                 self.button_sprites.append(
                     SoundButton(
-                        upgrade_sound,
-                        pan_setting,
+                        game_over_sound,
+                        speed_setting,
                         vol,
                         x_pos,
                         SCREEN_HEIGHT / 2 + y_offset,
