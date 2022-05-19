7352afa2ffac5ff0c46b9cb917087d8a7e5aa03c
pushfoo <36696816+pushfoo@users.noreply.github.com>
2022-05-19 18:00:12 -0400

    Improve gui_slider formatting and commenting

--- arcade/examples/gui_slider.py
+++ arcade/examples/gui_slider.py
@@ -1,3 +1,16 @@
+"""
+GUI Slider Example
+
+If Python and Arcade are installed, this example can be run from the
+command line with:
+python -m arcade.examples.view_screens_minimal
+
+This example demonstrates how to create a GUI slider and react to
+changes in its value.
+
+There are two other ways of handling update events. For more
+information on this subject, see the gui_flat_button example.
+"""
 import arcade
 from arcade.gui.widgets.slider import UISlider
 from arcade.gui import UIManager, UILabel
@@ -7,21 +20,32 @@ from arcade.gui.events import UIOnChangeEvent
 class UIMockup(arcade.Window):
     def __init__(self):
         super().__init__(800, 600, "UI Mockup", resizable=True)
-        self.manager = UIManager()
-        self.manager.enable()
         arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
 
+        # Required, create a UI manager to handle all UI widgets
+        self.manager = UIManager()
+        self.manager.enable()
+
+        # Create our pair of widgets
         ui_slider = UISlider(value=50, width=300, height=50)
         label = UILabel(text=f"{ui_slider.value:02.0f}")
 
+        # Change the label's text whenever the slider is dragged
+        # See the gui_flat_button example for more information.
         @ui_slider.event()
         def on_change(event: UIOnChangeEvent):
             label.text = f"{ui_slider.value:02.0f}"
             label.fit_content()
 
+        # Create a layout to hold the label and the slider
         ui_anchor_layout = arcade.gui.widgets.layout.UIAnchorLayout()
-        ui_anchor_layout.add(child=ui_slider, anchor_x="center_x", anchor_y="center_y")
+        ui_anchor_layout.add(
+            child=ui_slider,
+            anchor_x="center_x",
+            anchor_y="center_y"
+        )
         ui_anchor_layout.add(child=label, align_y=50)
+
         self.manager.add(ui_anchor_layout)
 
     def on_draw(self):
