f7e242f40a9607a7ed4adb4b9c5765bbde5ce7b2
Einar Forselv <eforselv@gmail.com>
2022-06-22 07:15:24 +0200

    Quick temporary "startproject" command

--- arcade/management/__init__.py
+++ arcade/management/__init__.py
@@ -1,9 +1,21 @@
-import arcade
+from pathlib import Path
+import shutil
 import sys
+import arcade
+
+EXAMPLE_PATH = Path(__file__).parent.parent.resolve() / "examples"
 
 
 def execute_from_command_line():
-    show_info()
+    if len(sys.argv) == 1:
+        show_info()
+        return
+
+    command = sys.argv[1]
+    if command == "startproject":
+        start_project(sys.argv[2])
+    else:
+        print("Unsupported command")
 
 
 def show_info():
@@ -17,3 +29,12 @@ def show_info():
     print('version:', window.ctx.gl_version)
     print('python:', sys.version)
     print('platform:', sys.platform)
+
+
+def start_project(path_str: str):
+    path = Path(path_str)
+    if path.exists():
+        print("File already exists")
+
+    shutil.copy(EXAMPLE_PATH / "starting_template.py", path)
+    print("Created", path)
