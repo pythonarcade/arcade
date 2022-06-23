3592f4c43c8c5c5da16b8d0af1bbb65a1591ef82
Einar Forselv <eforselv@gmail.com>
2022-06-23 09:18:55 +0200

    re-enable index buffer hack until new pyglet release

--- arcade/gl/vertex_array.py
+++ arcade/gl/vertex_array.py
@@ -217,7 +217,7 @@ class VertexArray:
         gl.glBindVertexArray(self.glo)
         if self._ibo is not None:
             # # HACK: re-bind index buffer just in case. pyglet rendering was somehow replacing the index buffer.
-            # gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, self._ibo.glo)
+            gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, self._ibo.glo)
             gl.glDrawElementsInstanced(
                 mode, vertices, self._index_element_type,
                 first * self._index_element_size, instances
