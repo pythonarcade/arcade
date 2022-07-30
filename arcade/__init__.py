b3c2517256b12bae409a31fb079950eef1ed1d8a
Einar Forselv <eforselv@gmail.com>
2022-07-30 15:27:43 +0200

    Remove trim_image

--- arcade/__init__.py
+++ arcade/__init__.py
@@ -185,7 +185,6 @@ from .texture import load_textures
 from .texture import make_circle_texture
 from .texture import make_soft_circle_texture
 from .texture import make_soft_square_texture
-from .texture import trim_image
 
 from .buffered_draw_commands import TShape
 from .buffered_draw_commands import Shape
@@ -567,7 +566,6 @@ __all__ = ['AStarBarrierList',
            'uint24_to_three_byte_color',
            'uint32_to_four_byte_color',
            'unschedule',
-           'trim_image',
            ]
 
 
