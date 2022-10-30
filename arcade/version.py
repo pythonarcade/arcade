a57c2d36354d0b2ac32ff4d39b39c480f8e3365f
Paul Craven <paul@cravenfamily.com>
2022-10-30 17:06:09 -0500

    Work on auto-update version number

--- arcade/version.py
+++ arcade/version.py
@@ -2,12 +2,16 @@ import os
 
 
 # open text file in read mode
-text_file = open(f"{os.path.dirname(__file__)}\VERSION", "r")
+try:
+    text_file = open(f"{os.path.dirname(__file__)}\VERSION", "r")
 
-# read whole file to a string
-data = text_file.read().strip()
+    # read whole file to a string
+    data = text_file.read().strip()
 
-# close file
-text_file.close()
+    # close file
+    text_file.close()
+except:
+    print("ERROR: Unable to load version number.")
+    data = "0.0.0"
 
 VERSION = data
