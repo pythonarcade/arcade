232215f12dcb54ac552e01f49d46ce237ada9835
Einar Forselv <eforselv@gmail.com>
2022-11-28 03:17:31 +0100

    Fix sprite scaling issues

--- arcade/sprite.py
+++ arcade/sprite.py
@@ -555,13 +555,7 @@ class Sprite:
         if new_value != self._width:
             self.clear_spatial_hashes()
             self._point_list_cache = None
-
-            # If there is a hit box, rescale it to the new width
-            if self._points:
-                scale = new_value / self._width
-                old_points = self._points
-                self._points = [(point[0] * scale, point[1]) for point in old_points]
-
+            self._scale = new_value / self.texture.width, self._scale[1]
             self._width = new_value
             self.add_spatial_hashes()
 
@@ -579,13 +573,7 @@ class Sprite:
         if new_value != self._height:
             self.clear_spatial_hashes()
             self._point_list_cache = None
-
-            # If there is a hit box, rescale it to the new width
-            if self._points:
-                scale = new_value / self._height
-                old_points = self._points
-                self._points = [(point[0], point[1] * scale) for point in old_points]
-
+            self._scale = self._scale[0], new_value / self.texture.height
             self._height = new_value
             self.add_spatial_hashes()
 
@@ -612,7 +600,7 @@ class Sprite:
         self._scale = new_value, new_value
         if self._texture:
             self._width = self._texture.width * self._scale[0]
-            self._height = self._texture.height * self._scale[0]
+            self._height = self._texture.height * self._scale[1]
 
         self.add_spatial_hashes()
 
@@ -1066,7 +1054,7 @@ class Sprite:
 
         self._sprite_list.draw(filter=filter, pixelated=pixelated, blend_function=blend_function)
 
-    def draw_hit_box(self, color: Color = BLACK, line_thickness: float = 1) -> None:
+    def draw_hit_box(self, color: Color = BLACK, line_thickness: float = 2.0) -> None:
         """
         Draw a sprite's hit-box.
 
