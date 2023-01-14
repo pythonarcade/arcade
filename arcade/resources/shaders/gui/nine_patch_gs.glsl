55ca4438e73c02e101d23e3fb536c58c8bcb9d81
Einar Forselv <eforselv@gmail.com>
2023-01-14 22:08:49 +0100

    Use new texture coordinates for 9patch

--- arcade/resources/shaders/gui/nine_patch_gs.glsl
+++ arcade/resources/shaders/gui/nine_patch_gs.glsl
@@ -1,18 +1,25 @@
 #version 330
 // Geometry shader emitting 9 patch from point
+// This can be simplified somewhat, but the verbose version are easier to maintain
 
 uniform WindowBlock {
     mat4 projection;
     mat4 view;
 } window;
 
+uniform sampler2D sprite_texture;
 uniform sampler2D uv_texture;
+
 uniform float texture_id;
 
+// Lower left: Border in pixels
 uniform vec2 start;
+// Upper right: Border in pixels
 uniform vec2 end;
 
+// Size of the patch in pixels
 uniform vec2 size;
+// Size of the texture in pixels
 uniform vec2 t_size;
 
 layout(points) in;
@@ -21,11 +28,6 @@ layout(triangle_strip, max_vertices = 36) out;
 out vec2 uv;
 
 void main() {
-    // Get texture coordinates
-    vec4 uv_data = texelFetch(uv_texture, ivec2(texture_id, 0), 0);
-    vec2 tex_offset = uv_data.xy;
-    vec2 tex_size = uv_data.zw;
-
     // Patch points starting from upper left row by row
     vec2 p1 = vec2(0.0, size.y);
     vec2 p2 = vec2(start.x, size.y);
@@ -47,40 +49,50 @@ void main() {
     vec2 p15 = vec2(size.x - (t_size.x - end.x), 0.0);
     vec2 p16 = vec2(size.x, 0.0);
 
-    // Texture coordinates starting from upper left
-    // ----------------------------------
-    // 0.001953125,  0.251953125 (offset)
-    // 0.25       ,  -0.25       (size)
-    // ----------------------------------
-    // upper left  : Vec2(x=0.001953125, y=0.001953125)
-    // upper right : Vec2(x=0.251953125, y=0.001953125)
-    // lower left  : Vec2(x=0.001953125, y=0.251953125)
-    // lower right : Vec2(x=0.251953125, y=0.251953125)
-    // ----------------------------------
-    vec2 start_uv = abs(start / t_size * tex_size);
-    vec2 end_uv = abs((t_size - end) / t_size * tex_size);
-  
-    vec2 t1 = vec2(0.0, tex_size.y) + tex_offset;
-    vec2 t2 = vec2(start_uv.x, tex_size.y) + tex_offset;
-    vec2 t3 = vec2(tex_size.x - end_uv.x, tex_size.y) + tex_offset;
-    vec2 t4 = tex_size + tex_offset;
+    // <AtlasRegion 
+    //     x=1 y=1 
+    //     width=100 height=100
+    //     uvs=(
+    //         0.001953125, 0.001953125,
+    //         0.197265625, 0.001953125,
+    //         0.001953125, 0.197265625,
+    //         0.197265625, 0.197265625,
+    //     )
+    // Get texture coordinates
+    vec2 uv0, uv1, uv2, uv3;
+    getSpriteUVs(uv_texture, int(texture_id), uv0, uv1, uv2, uv3);
+    vec2 atlas_size = textureSize(sprite_texture, 0);
+    // Corner offsets (upper left, upper, right, lower left, lower right)
+    // This is the global texture coordiante offset in the entire atlas
+    float left = start.x;
+    float right = t_size.x - end.x;
+    float top = t_size.y - end.y;
+    float bottom = start.y;
 
-    vec2 t5 = vec2(0.0, tex_size.y + end_uv.y) + tex_offset;
-    vec2 t6 = vec2(start_uv.x, tex_size.y + end_uv.y) + tex_offset;
-    vec2 t7 = vec2(tex_size.x - end_uv.x, tex_size.y + end_uv.y) + tex_offset;
-    vec2 t8 = tex_size + tex_offset + vec2(0.0, end_uv.y);
+    vec2 c1 = vec2(left, top) / atlas_size;
+    vec2 c2 = vec2(right, top) / atlas_size;
+    vec2 c3 = vec2(left, bottom) / atlas_size;
+    vec2 c4 = vec2(right, bottom) / atlas_size;
 
-    float y_offset = tex_offset.y - start_uv.y;
-    vec2 t9 = vec2(0.0, tex_size.y + y_offset) + tex_offset;
-    vec2 t10 = vec2(start_uv.x, tex_size.y + y_offset) + tex_offset;
-    vec2 t11 = vec2(tex_size.x - end_uv.x, tex_size.y + y_offset) + tex_offset;
-    vec2 t12 = tex_size + tex_offset + vec2(0.0, y_offset);
+    vec2 t1 = uv0;
+    vec2 t2 = uv0 + vec2(c1.x, 0.0);
+    vec2 t3 = uv1 - vec2(c2.x, 0.0);
+    vec2 t4 = uv1;
 
-    y_offset = abs(tex_size.y);
-    vec2 t13 = vec2(0.0, tex_size.y + y_offset) + tex_offset;
-    vec2 t14 = vec2(start_uv.x, tex_size.y + y_offset) + tex_offset;
-    vec2 t15 = vec2(tex_size.x - end_uv.x, tex_size.y + y_offset) + tex_offset;
-    vec2 t16 = tex_size + tex_offset + vec2(0.0, y_offset);
+    vec2 t5 = uv0 + vec2(0.0, c1.y);
+    vec2 t6 = uv0 + c1;
+    vec2 t7 = uv1 + vec2(-c2.x, c2.y);
+    vec2 t8 = uv1 + vec2(0.0, c2.y);
+
+    vec2 t9 = uv2 - vec2(0.0, c3.y);
+    vec2 t10 = uv2 + vec2(c3.x, -c3.y);
+    vec2 t11 = uv3 - c4;
+    vec2 t12 = uv3 - vec2(0.0, c4.y);
+
+    vec2 t13 = uv2;
+    vec2 t14 = uv2 + vec2(c3.x, 0.0);
+    vec2 t15 = uv3 - vec2(c4.x, 0.0);
+    vec2 t16 = uv3;
 
     mat4 mvp = window.projection * window.view;
     // First row - two fixed corners + strechy middle - 8 vertices
