47dfe2b0c9722992c37d3a59c45d2f39cac78933
JDLiang <juandavidliang@gmail.com>
2023-02-13 20:05:15 -0800

    Added a zombie character

--- arcade/examples/joystick_controller.py
+++ arcade/examples/joystick_controller.py
@@ -5,8 +5,9 @@ import arcade
 
 # Constants
 SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
+SCREEN_HEIGHT = 1000
 SCREEN_TITLE = "Platformer"
+CHARACTER_SCALING = 0.5
 
 
 class MyGame(arcade.Window):
@@ -19,18 +20,29 @@ class MyGame(arcade.Window):
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+        arcade.set_background_color(arcade.csscolor.AZURE)
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
-        pass
+        # Create the Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
 
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = ":resources:images/animated_characters/zombie/zombie_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 500
+        self.player_sprite.center_y = 500
+        self.player_list.append(self.player_sprite)
+        
     def on_draw(self):
         """Render the screen."""
 
+        # Clear the screen to the background color
         self.clear()
-        # Code to draw the screen goes here
 
+        # Draw our sprites
+        self.player_list.draw()
 
 def main():
     """Main function"""
