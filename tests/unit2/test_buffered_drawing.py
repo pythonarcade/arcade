06feaf7500a288486de38b41d849ac21786bd935
Einar Forselv <eforselv@gmail.com>
2023-02-17 23:20:00 +0100

    Move buffered shapes out of arcade namespace (#1537)
    * Move buffered shapes out of arcade namespace
    
    * The flake8 life

--- tests/unit2/test_buffered_drawing.py
+++ tests/unit2/test_buffered_drawing.py
@@ -1,4 +1,17 @@
 import arcade
+from arcade.shape_list import (
+    ShapeElementList,
+    create_line,
+    create_ellipse_outline,
+    create_ellipse_filled,
+    create_ellipse_filled_with_colors,
+    create_rectangle_filled,
+    create_rectangle_outline,
+    create_ellipse_outline,
+    create_rectangle_filled_with_colors,
+    create_line_generic,
+    create_line_strip,
+)
 import pyglet.gl as gl
 
 SCREEN_WIDTH = 800
@@ -6,27 +19,27 @@ SCREEN_HEIGHT = 600
 
 
 def test_buffered_drawing(window):
-    shape_list = arcade.ShapeElementList()
+    shape_list = ShapeElementList()
 
     center_x = 0
     center_y = 0
     width = 20
     height = 20
-    shape = arcade.create_ellipse_filled(center_x, center_y, width, height, arcade.color.WHITE)
+    shape = create_ellipse_filled(center_x, center_y, width, height, arcade.color.WHITE)
     shape_list.append(shape)
 
     center_x += 40
-    shape = arcade.create_ellipse_outline(center_x, center_y, width, height, arcade.color.RED, border_width=1)
+    shape = create_ellipse_outline(center_x, center_y, width, height, arcade.color.RED, border_width=1)
     shape_list.append(shape)
 
     center_x += 40
-    shape = arcade.create_ellipse_outline(center_x, center_y, width, height, arcade.color.DARK_RED, border_width=1)
+    shape = create_ellipse_outline(center_x, center_y, width, height, arcade.color.DARK_RED, border_width=1)
     shape_list.append(shape)
 
-    shape = arcade.create_line(0, 0, 80, 0, arcade.color.BLUE, line_width=1)
+    shape = create_line(0, 0, 80, 0, arcade.color.BLUE, line_width=1)
     shape_list.append(shape)
 
-    shape = arcade.create_line(0, 0, 80, 0, arcade.color.LIGHT_BLUE, line_width=1)
+    shape = create_line(0, 0, 80, 0, arcade.color.LIGHT_BLUE, line_width=1)
     shape_list.append(shape)
 
     center_x = 0
@@ -36,7 +49,7 @@ def test_buffered_drawing(window):
     outside_color = arcade.color.AERO_BLUE
     inside_color = arcade.color.AFRICAN_VIOLET
     tilt_angle = 45
-    shape = arcade.create_ellipse_filled_with_colors(center_x, center_y,
+    shape = create_ellipse_filled_with_colors(center_x, center_y,
                                                      width, height,
                                                      outside_color, inside_color,
                                                      tilt_angle)
@@ -46,13 +59,13 @@ def test_buffered_drawing(window):
     center_y = -50
     width = 20
     height = 20
-    shape = arcade.create_rectangle_filled(center_x, center_y, width, height,
+    shape = create_rectangle_filled(center_x, center_y, width, height,
                                            arcade.color.WHITE)
     shape_list.append(shape)
-    shape = arcade.create_rectangle_outline(center_x, center_y, width, height,
+    shape = create_rectangle_outline(center_x, center_y, width, height,
                                             arcade.color.BLACK, border_width=1)
     shape_list.append(shape)
-    shape = arcade.create_rectangle_outline(center_x, center_y, width, height,
+    shape = create_rectangle_outline(center_x, center_y, width, height,
                                             arcade.color.AMERICAN_ROSE, border_width=1)
     shape_list.append(shape)
 
@@ -60,15 +73,15 @@ def test_buffered_drawing(window):
     color2 = (219, 166, 123)
     points = (70, 70), (150, 70), (150, 150), (70, 150)
     colors = (color1, color1, color2, color2)
-    shape = arcade.create_rectangle_filled_with_colors(points, colors)
+    shape = create_rectangle_filled_with_colors(points, colors)
     shape_list.append(shape)
 
     points = (0, 0), (150, 150), (0, 150), (0, 250)
-    shape = arcade.create_line_strip(points, arcade.color.AFRICAN_VIOLET)
+    shape = create_line_strip(points, arcade.color.AFRICAN_VIOLET)
     shape_list.append(shape)
 
     points = (0, 0), (75, 90), (60, 150), (90, 250)
-    shape = arcade.create_line_generic(points, arcade.color.ALIZARIN_CRIMSON, gl.GL_TRIANGLE_FAN)
+    shape = create_line_generic(points, arcade.color.ALIZARIN_CRIMSON, gl.GL_TRIANGLE_FAN)
     shape_list.append(shape)
 
     shape_list.center_x = 200
