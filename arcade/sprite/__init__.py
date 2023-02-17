76e2ff184a15175b891545b954888423df9b9d2f
Einar Forselv <eforselv@gmail.com>
2023-02-17 19:19:05 +0100

    Quick sprite module split

---
+++ arcade/sprite/__init__.py
@@ -0,0 +1,77 @@
+import PIL.Image
+
+import arcade
+from arcade.texture import Texture
+from arcade.resources import resolve_resource_path
+from .base import Sprite, PyMunk
+from .animated import (
+    AnimatedTimeBasedSprite,
+    AnimationKeyframe,
+    AnimatedWalkingSprite,
+)
+from .simple import SpriteSolidColor, SpriteCircle
+from .enums import (
+    FACE_LEFT,
+    FACE_RIGHT,
+    FACE_UP,
+    FACE_DOWN,
+)
+
+
+def load_animated_gif(resource_name) -> AnimatedTimeBasedSprite:
+    """
+    Attempt to load an animated GIF as an :class:`AnimatedTimeBasedSprite`.
+
+    Many older GIFs will load with incorrect transparency for every
+    frame but the first. Until the Pillow library handles the quirks of
+    the format better, loading animated GIFs will be pretty buggy. A
+    good workaround is loading GIFs in another program and exporting them
+    as PNGs, either as sprite sheets or a frame per file.
+    """
+
+    file_name = resolve_resource_path(resource_name)
+    image_object = PIL.Image.open(file_name)
+    if not image_object.is_animated:
+        raise TypeError(f"The file {resource_name} is not an animated gif.")
+
+    sprite = AnimatedTimeBasedSprite()
+    for frame in range(image_object.n_frames):
+        image_object.seek(frame)
+        frame_duration = image_object.info['duration']
+        image = image_object.convert("RGBA")
+        texture = Texture(image)
+        texture.origin = f"{resource_name}|{frame}"
+        sprite.textures.append(texture)
+        sprite.frames.append(AnimationKeyframe(0, frame_duration, texture))
+
+    sprite.texture = sprite.textures[0]
+    return sprite
+
+
+def get_distance_between_sprites(sprite1: Sprite, sprite2: Sprite) -> float:
+    """
+    Returns the distance between the center of two given sprites
+
+    :param Sprite sprite1: Sprite one
+    :param Sprite sprite2: Sprite two
+    :return: Distance
+    :rtype: float
+    """
+    return arcade.get_distance(*sprite1._position, *sprite2._position)
+
+
+__all__ = [
+    "Sprite",
+    "PyMunk",
+    "AnimatedTimeBasedSprite",
+    "AnimationKeyframe",
+    "AnimatedWalkingSprite",
+    "load_animated_gif",
+    "get_distance_between_sprites",
+    "SpriteSolidColor",
+    "SpriteCircle",
+    "FACE_LEFT",
+    "FACE_RIGHT",
+    "FACE_UP",
+    "FACE_DOWN",
+]
---
+++ arcade/sprite/animated.py
@@ -0,0 +1,197 @@
+import dataclasses
+import math
+from typing import List
+
+from .base import Sprite
+from arcade import Texture
+from arcade.types import PathOrTexture
+from .enums import (
+    FACE_LEFT,
+    FACE_RIGHT,
+    FACE_UP,
+    FACE_DOWN,
+)
+
+
+@dataclasses.dataclass
+class AnimationKeyframe:
+    """
+    Used in animated sprites.
+    """
+    tile_id: int
+    duration: int
+    texture: Texture
+
+
+class AnimatedTimeBasedSprite(Sprite):
+    """
+    Sprite for platformer games that supports animations. These can
+    be automatically created by the Tiled Map Editor.
+    """
+    def __init__(
+            self,
+            path_or_texture: PathOrTexture = None,
+            center_x: float = 0.0,
+            center_y: float = 0.0,
+            scale: float = 1.0,
+    ):
+        super().__init__(
+            path_or_texture,
+            scale=scale,
+            center_x=center_x,
+            center_y=center_y,
+        )
+        self.cur_frame_idx = 0
+        self.frames: List[AnimationKeyframe] = []
+        self.time_counter = 0.0
+
+    def update_animation(self, delta_time: float = 1 / 60) -> None:
+        """
+        Logic for selecting the proper texture to use.
+        """
+        self.time_counter += delta_time
+        while self.time_counter > self.frames[self.cur_frame_idx].duration / 1000.0:
+            self.time_counter -= self.frames[self.cur_frame_idx].duration / 1000.0
+            self.cur_frame_idx += 1
+            if self.cur_frame_idx >= len(self.frames):
+                self.cur_frame_idx = 0
+            # source = self.frames[self.cur_frame].texture.image.source
+            cur_frame = self.frames[self.cur_frame_idx]
+            # print(f"Advance to frame {self.cur_frame_idx}: {cur_frame.texture.name}")
+            self.texture = cur_frame.texture
+
+
+class AnimatedWalkingSprite(Sprite):
+    """
+    Deprecated Sprite for platformer games that supports walking animations.
+    Make sure to call update_animation after loading the animations so the
+    initial texture can be set. Or manually set it.
+
+    It is highly recommended you create your own version of this class rather than
+    try to use this pre-packaged one.
+
+    For an example, see this section of the platformer tutorial:
+    :ref:`platformer_part_twelve`.
+    """
+    def __init__(
+        self,
+        scale: float = 1.0,
+        center_x: float = 0.0,
+        center_y: float = 0.0,
+    ):
+        super().__init__(
+            None,
+            scale=scale,
+            center_x=center_x,
+            center_y=center_y,
+        )
+        self.state = FACE_RIGHT
+        self.stand_right_textures: List[Texture] = []
+        self.stand_left_textures: List[Texture] = []
+        self.walk_left_textures: List[Texture] = []
+        self.walk_right_textures: List[Texture] = []
+        self.walk_up_textures: List[Texture] = []
+        self.walk_down_textures: List[Texture] = []
+        self.cur_texture_index = 0
+        self.texture_change_distance = 20
+        self.last_texture_change_center_x: float = 0.0
+        self.last_texture_change_center_y: float = 0.0
+
+    def update_animation(self, delta_time: float = 1 / 60) -> None:
+        """
+        Logic for selecting the proper texture to use.
+        """
+        x1 = self.center_x
+        x2 = self.last_texture_change_center_x
+        y1 = self.center_y
+        y2 = self.last_texture_change_center_y
+        distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        texture_list: List[Texture] = []
+
+        change_direction = False
+        if (
+            self.change_x > 0
+            and self.change_y == 0
+            and self.state != FACE_RIGHT
+            and len(self.walk_right_textures) > 0
+        ):
+            self.state = FACE_RIGHT
+            change_direction = True
+        elif (
+            self.change_x < 0
+            and self.change_y == 0
+            and self.state != FACE_LEFT
+            and len(self.walk_left_textures) > 0
+        ):
+            self.state = FACE_LEFT
+            change_direction = True
+        elif (
+            self.change_y < 0
+            and self.change_x == 0
+            and self.state != FACE_DOWN
+            and len(self.walk_down_textures) > 0
+        ):
+            self.state = FACE_DOWN
+            change_direction = True
+        elif (
+            self.change_y > 0
+            and self.change_x == 0
+            and self.state != FACE_UP
+            and len(self.walk_up_textures) > 0
+        ):
+            self.state = FACE_UP
+            change_direction = True
+
+        if self.change_x == 0 and self.change_y == 0:
+            if self.state == FACE_LEFT:
+                self.texture = self.stand_left_textures[0]
+            elif self.state == FACE_RIGHT:
+                self.texture = self.stand_right_textures[0]
+            elif self.state == FACE_UP:
+                self.texture = self.walk_up_textures[0]
+            elif self.state == FACE_DOWN:
+                self.texture = self.walk_down_textures[0]
+
+        elif change_direction or distance >= self.texture_change_distance:
+            self.last_texture_change_center_x = self.center_x
+            self.last_texture_change_center_y = self.center_y
+
+            if self.state == FACE_LEFT:
+                texture_list = self.walk_left_textures
+                if texture_list is None or len(texture_list) == 0:
+                    raise RuntimeError(
+                        "update_animation was called on a sprite that doesn't have a "
+                        "list of walk left textures."
+                    )
+            elif self.state == FACE_RIGHT:
+                texture_list = self.walk_right_textures
+                if texture_list is None or len(texture_list) == 0:
+                    raise RuntimeError(
+                        "update_animation was called on a sprite that doesn't have a list of "
+                        "walk right textures."
+                    )
+            elif self.state == FACE_UP:
+                texture_list = self.walk_up_textures
+                if texture_list is None or len(texture_list) == 0:
+                    raise RuntimeError(
+                        "update_animation was called on a sprite that doesn't have a list of "
+                        "walk up textures."
+                    )
+            elif self.state == FACE_DOWN:
+                texture_list = self.walk_down_textures
+                if texture_list is None or len(texture_list) == 0:
+                    raise RuntimeError(
+                        "update_animation was called on a sprite that doesn't have a list of walk down textures."
+                    )
+
+            self.cur_texture_index += 1
+            if self.cur_texture_index >= len(texture_list):
+                self.cur_texture_index = 0
+
+            self.texture = texture_list[self.cur_texture_index]
+
+        if self._texture is None:
+            print("Error, no texture set")
+        else:
+            self.width = self._texture.width * self.scale
+            self.height = self._texture.height * self.scale
