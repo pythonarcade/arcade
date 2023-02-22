60ea7449eedfcad8da4f5b468f38db2e7c5ccfe6
DragonMoffon <86714785+DragonMoffon@users.noreply.github.com>
2023-02-23 08:31:46 +1300

    Adding a random.glsl for use with glsl code injection (#1569)
    * Adding random.glsl and an example using it. So all who want to do stuff with random white noise inside shaders can. (NOTE: it is pseudo-random noise). This noise is focused on generating visually good-looking noise and there are no assurances on the mathematical quality.
    
    * Flake8 clean up.
    
    * clean up for einarf
    
    * fixed small error in doc string

---
+++ arcade/resources/shaders/lib/random.glsl
@@ -0,0 +1,32 @@
+// Cool High Quality Hash Random
+uint hash( uint x ) { // Actual randomiser
+    x += ( x << 10u );
+    x ^= ( x >>  6u );
+    x += ( x <<  3u );
+    x ^= ( x >> 11u );
+    x += ( x << 15u );
+    return x;
+}
+
+uint hash( uvec2 v ) { return hash( v.x ^ hash(v.y)                         ); } // a version of the hash for 2, 3, and 4 uints
+uint hash( uvec3 v ) { return hash( v.x ^ hash(v.y) ^ hash(v.z)             ); }
+uint hash( uvec4 v ) { return hash( v.x ^ hash(v.y) ^ hash(v.z) ^ hash(v.w) ); }
+
+float floatConstruct( uint m ) { // Takes the hash and turns it into a number between 0 and 1. using bit manipulation magic
+    const uint ieeeMantissa = 0x007FFFFFu; // binary32 mantissa bitmask
+    const uint ieeeOne      = 0x3F800000u; // 1.0 in IEEE binary32
+
+    m &= ieeeMantissa;                     // Keep only mantissa bits (fractional part)
+    m |= ieeeOne;                          // Add fractional part to 1.0
+
+    float  f = uintBitsToFloat( m );       // Range [1:2]
+    return f - 1.0;                        // Range [0:1]
+}
+
+// Pseudo-random value in half-open range [0:1].
+float random( float x ) { return floatConstruct(hash(floatBitsToUint(x))); } // [0-1] randomiser for 1, 2, 3, and 4 floats
+float random( vec2  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+float random( vec3  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+float random( vec4  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+
+// floatBitsToUint and uintBitsToFloat are built in functions that do a non-destructive cast. Equivlent to casting a pointer in C. So basically bit magic.
