032f39f79fafdfb053bf65fc85e0311ad2ac8ca8
Maic Siemering <maic@siemering.tech>
2023-03-04 08:36:10 +0100

    UI: fix label tests for different os rendering (#1587)

--- tests/test_gui/test_uilabel.py
+++ tests/test_gui/test_uilabel.py
@@ -6,7 +6,9 @@ from arcade.gui import UILabel, Rect
 def test_uilabel_inits_with_text_size(window):
     label = UILabel(text="Example")
 
-    assert label.rect.width == pytest.approx(63, abs=6)  # on windows the width differs about 6 pixel
+    assert label.rect.width == pytest.approx(
+        63, abs=6
+    )  # on windows the width differs about 6 pixel
     assert label.rect.height == 19
 
 
@@ -28,21 +30,21 @@ def test_uilabel_allow_multiline_and_uses_text_height(window):
 def test_uilabel_with_border_keeps_previous_size(window):
     label = UILabel(text="Example")
     assert label.rect.width == pytest.approx(63, abs=6)
-    assert label.rect.height == 19
+    assert label.rect.height == pytest.approx(19, abs=6)
 
     label.with_border()
     assert label.rect.width == pytest.approx(63, abs=6)
-    assert label.rect.height == 19
+    assert label.rect.height == pytest.approx(19, abs=6)
 
 
 def test_uilabel_with_padding_keeps_previous_size(window):
     label = UILabel(text="Example")
     assert label.rect.width == pytest.approx(63, abs=6)
-    assert label.rect.height == 19
+    assert label.rect.height == pytest.approx(19, abs=6)
 
     label.with_padding(all=2)
     assert label.rect.width == pytest.approx(63, abs=6)
-    assert label.rect.height == 19
+    assert label.rect.height == pytest.approx(19, abs=6)
 
 
 def test_uilabel_fixes_internal_text_to_pos_0_0(window):
@@ -53,3 +55,4 @@ def test_uilabel_fixes_internal_text_to_pos_0_0(window):
     label = UILabel(text="Example", x=10, y=10)
     assert label.label.position == (0, 0)
     assert label.position == (10, 10)
+
