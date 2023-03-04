6c0500b105ede56aa4e414e90a7e809b5d85f387
Ibrahim <cshore.2750mi@gmail.com>
2023-03-05 02:49:47 +0530

    Step 2: Switching to Menu View

--- doc/tutorials/menu/index.rst
+++ doc/tutorials/menu/index.rst
@@ -7,9 +7,70 @@ Making a Menu with Arcade's GUI
 ===============================
 
 Step 1: Open a Window
--------------
-First, let's start with a blank window.
+---------------------
 
-.. literalinclude:: gpu_particle_burst_01.py
-    :caption: menu_01.py
+First, let's start a blank window with a view.
+
+.. literalinclude:: menu_01.py
+    :caption: Opening a Window
     :linenos:
+
+Step 2: Switching to Menu View
+-------------------------------
+
+For this section we will switch the current view of the window to the menu view.
+
+
+Imports
+-------
+
+First we will import the arcade gui:
+
+.. literalinclude:: menu_02.py
+    :caption: Importing arcade.gui
+    :lines: 4-6
+    :emphasize-lines: 5
+
+Modify the MainView
+-------------------
+
+We are going to add a button to change the view. For drawing a button we would need a UIManager.
+
+.. literalinclude:: menu_02.py
+    :caption: Intialising the Manager
+    :lines: 15-19
+    :emphasize-lines: 18
+
+After initialising the manager we need to enable it when the view is shown and disable it when the view is hiddien.
+
+.. literalinclude:: menu_02.py
+    :caption: Enabling the Manager
+    :lines: 42-48
+    :emphasize-lines: 46-47
+
+.. literalinclude:: menu_02.py
+    :caption: Disabling the Manager
+    :lines: 37-39
+
+We also need to draw the childrens of the menu in `on_draw`.
+
+ .. literalinclude:: menu_02.py
+    :caption: Drawing Children's of the Manager
+    :lines: 49-56
+    :emphasize-lines: 54-55
+
+Now we have successfully setup the manager, only thing left it to add the button. We are using `UIAnchorLayout` to position the button.
+
+  .. literalinclude:: menu_02.py
+    :caption: Initialising the Button
+    :lines: 20-36
+
+
+Initialise the Menu View
+------------------------
+
+We make a boiler plate view just like we did in Step-1.
+
+.. literalinclude:: menu_02.py
+    :caption: Initialise the Menu View
+    :lines: 57-70
---
+++ doc/tutorials/menu/menu_02.py
@@ -0,0 +1,83 @@
+"""
+Menu.
+"""
+import arcade
+import arcade.gui
+
+# Screen title and size
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Making a Menu"
+
+
+class MainView(arcade.View):
+    """ Main application class. """
+    def __init__(self):
+        super().__init__()
+
+        self.manager = arcade.gui.UIManager()
+
+        switch_menu_button = arcade.gui.UIFlatButton(text="Switch to Menu", width=250)
+
+        # Initialise the button with an on_click event.
+        @switch_menu_button.event("on_click")
+        def on_click_switch_button(event):
+            menu_view = MenuView()
+            self.window.show_view(menu_view)
+
+        # Use the anchor to position the button on the screen.
+        self.anchor = self.manager.add(arcade.gui.UIAnchorLayout())
+
+        self.anchor.add(
+            anchor_x="center_x",
+            anchor_y="center_y",
+            child=switch_menu_button,
+        )
+
+    def on_hide_view(self):
+        # Disable the UIManager when the view is hidden.
+        self.manager.disable()
+
+    def on_show_view(self):
+        """ This is run once when we switch to this view """
+        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+
+        # Enable the UIManager when the view is showm. 
+        self.manager.enable()
+
+    def on_draw(self):
+        """ Render the screen. """
+        # Clear the screen
+        self.clear()
+
+        # Draw the manager.
+        self.manager.draw()
+
+
+class MenuView(arcade.View):
+    def __init__(self):
+        super().__init__()
+
+    def on_show_view(self):
+        """ This is run once when we switch to this view """
+
+        # Makes the background darker
+        arcade.set_background_color([rgb - 50 for rgb in arcade.color.DARK_BLUE_GRAY])
+
+    def on_draw(self):
+        """ Render the screen. """
+        # Clear the screen
+        self.clear()
+
+
+def main():
+    """ Main function """
+
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    main_view = MainView()
+    window.show_view(main_view)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
