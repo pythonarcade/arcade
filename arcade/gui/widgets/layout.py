8ae1d1a6fd911f0d61010681d23b5ffa4ac36356
Mohammad Ibrahim <74553450+Ibrahim2750mi@users.noreply.github.com>
2023-03-09 01:49:39 +0530

    Add usage in the docstring for UIAnchorLayout (#1616)
    * Add usage in the docstring for UIAnchorLayout

--- arcade/gui/widgets/layout.py
+++ arcade/gui/widgets/layout.py
@@ -22,6 +22,13 @@ class UIAnchorLayout(UILayout):
     - anchor_y: str = None - uses `self.default_anchor_y` as default
     - align_y: float = 0
 
+    Usage::
+
+        manager = UIManager()
+        manager.enable()
+        anchor = manager.add(UIAnchorLayout())
+        anchor.add(child=child, ...)
+
     """
 
     default_anchor_x = "center"
