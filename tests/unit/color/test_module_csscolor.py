82abee25618a1ffb83797c2f39f7eed29ad79a96
pushfoo <36696816+pushfoo@users.noreply.github.com>
2023-03-14 02:48:21 -0400

    Make tests pass by accounting for extra imports

--- tests/unit/color/test_module_csscolor.py
+++ tests/unit/color/test_module_csscolor.py
@@ -2,4 +2,5 @@
 def test_csscolors():
     from arcade import csscolor
     names = csscolor.__dict__.keys()
-    assert 156 == len(names)
+    # number of colors + 1 import
+    assert 156 + 1 == len(names)
