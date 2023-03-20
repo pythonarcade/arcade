cfded9a9a3e1873a302c1d6bc44be5878c970d10
pushfoo <36696816+pushfoo@users.noreply.github.com>
2023-03-20 07:56:38 -0400

    Update Color annotation in arcade.gui.surface.Surface

--- arcade/gui/surface.py
+++ arcade/gui/surface.py
@@ -6,7 +6,7 @@ from arcade import Texture
 from arcade.color import TRANSPARENT_BLACK
 from arcade.gl import Framebuffer
 from arcade.gui.nine_patch import NinePatchTexture
-from arcade.types import Color, Point, Rect
+from arcade.types import Point, Rect, RGBALike
 
 
 class Surface:
@@ -83,7 +83,7 @@ class Surface:
     def height(self) -> int:
         return self._size[1]
 
-    def clear(self, color: Color = TRANSPARENT_BLACK):
+    def clear(self, color: RGBALike = TRANSPARENT_BLACK):
         """Clear the surface"""
         self.fbo.clear(color=color)
 
