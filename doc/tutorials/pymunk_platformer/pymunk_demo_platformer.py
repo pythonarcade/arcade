b3890d74bd1ea9fa6794456962187d0f7e504070
Ibrahim <cshore.2750mi@gmail.com>
2023-03-26 04:27:59 +0530

    Remove use of non existent method

--- doc/tutorials/pymunk_platformer/pymunk_demo_platformer.py
+++ doc/tutorials/pymunk_platformer/pymunk_demo_platformer.py
@@ -93,7 +93,7 @@ class PlayerSprite(arcade.Sprite):
         # Hit box will be set based on the first image used. If you want to specify
         # a different hit box, you can do it like the code below.
         # self.set_hit_box([[-22, -64], [22, -64], [22, 28], [-22, 28]])
-        self.set_hit_box(self.texture.hit_box_points)
+        # self.set_hit_box(self.texture.hit_box_points)
 
         # Default to face-right
         self.character_face_direction = RIGHT_FACING
