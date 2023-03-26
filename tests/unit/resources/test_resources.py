c17b54c2ad5348b52e43097bf674309e176f6b66
Einar Forselv <eforselv@gmail.com>
2023-03-26 21:44:17 +0200

    Skeleton for resource tests

---
+++ tests/unit/resources/test_resources.py
@@ -0,0 +1,14 @@
+"""
+Validate the resources module.
+* Check that all resources are listed in the __init__.py file
+* Check that all resources listed actually exists
+"""
+import arcade
+
+
+def test_resource_listing_exists():
+    pass
+
+
+def test_resource_listing_is_complete():
+    pass
