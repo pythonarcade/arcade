aa854d697b6004c409991aab382ca343f8f74335
Mohammad Ibrahim <74553450+Ibrahim2750mi@users.noreply.github.com>
2023-04-04 19:25:26 +0530

    Unbreak broken tutorials and pytest for tutorials (#1653)
    * Unbreak card-game tutorial
    
    * Remove print statement filling up terminal
    
    * Fix using wrong glsl file for step 3
    
    * Initial fix to unbreak the pymunk platformer tutorial
    
    * Fix docs failing to build
    
    * Actually fix docs failing to build
    
    Correct wrong line range
    
    * Remove use of non existent method
    
    Wrt step 11 pymunk platformer, it used the processs layer method.
    
    * Add pytest for tutorials
    
    Also fixed using absolute path in tutorials
    
    * Fix `index.rst` for shader toy glow
    
    * Remove use of non existent method
    
    * Fix `index.rst` for gpu particle burst
    
    * Remove inclusion of 0 in the range for sprite circle size
    
    * Change to correct dir when running tutorials
    
    * Cd into ../ before continuing the for loop
    
    * Change dir when running test and not when finding
    
    * Parameterize the test with another parameter
    
    * Fix argvalue
    
    * Update shadertoy_demo_1.py to remove usage of abs path
    
    * Update shadertoy_demo_3.py to remove usage of pathlib
    
    * Update shadertoy_demo_2.py to remove usage of pathlib
    
    * Update shadertoy_demo.py to remove usage of pathlib
    
    * Update index.rst to remove usage of pathlib
    
    * Remove pathlib from gpu_particle_burst tutorial files
    
    * Fix flake8 spacing & indentation errors on gpu_particle_burst tutorial
    
    * Fix flake8 errors in gpu_particle_burst tutorial & ensure lines stay within doc build view
    
    * Remove pathlib from raycasting tutorial
    
    * Address pcraven's drawing order comment on #1653
    
    * Clean up unused imports in raycasting tutorial
    
    * Fix compute shader example by removing pathlib & using auto-closing file handles
    
    * Remove pathlib from crt filter examples
    
    * Fix flake8 / overly long lines in crt filter tutorials
    
    * Rephrase top comment
    
    * Update highlight lines & some phrasing for gpu particle burst tutorial
    
    * Fix line numbers & some phrasing for raycasting tutorial
    
    ---------
    
    Co-authored-by: pushfoo <36696816+pushfoo@users.noreply.github.com>

---
+++ tests/integration/tutorials/run_all_tutorials.py
@@ -0,0 +1,67 @@
+"""
+Run All tutorials
+
+If Python and Arcade are installed, this tutorial can be run from the command line with:
+python -m tests.test_tutorials.run_all_tutorials
+"""
+import glob
+import subprocess
+import os
+from pathlib import Path
+
+TUTORIAL_SUBDIR = "../../../doc/tutorials/"
+
+
+def _get_short_name(fullpath):
+    return os.path.splitext(os.path.basename(fullpath))[0]
+
+def _get_tutorials(start_path):
+    query_path = os.path.join(start_path, "*.py")
+    tutorials = glob.glob(query_path)
+    tutorials = [_get_short_name(e) for e in tutorials]
+    tutorials = [e for e in tutorials if e != "run_all_tutorials"]
+    tutorials = [e for e in tutorials if not e.startswith('_')]
+    tutorials = [f"doc.tutorials.{start_path.name}." + e for e in tutorials if not e.startswith('_')]
+    return tutorials
+
+def run_tutorials(indices_in_range = None, index_skip_list = None):
+    """Run all tutorials in the doc/tutorials directory"""
+    for tutorial_subdir in [path for path in list(Path.cwd().joinpath(TUTORIAL_SUBDIR).iterdir()) if path.is_dir()]:
+        tutorials = _get_tutorials(tutorial_subdir)
+        tutorials.sort()
+        print(f"Found {len(tutorials)} tutorials in {tutorial_subdir}")
+
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        print(file_path)
+        os.chdir(file_path+"/../..")
+        # run tutorials
+        for (idx, tutorial) in enumerate(tutorials):
+            if indices_in_range is not None and idx not in indices_in_range:
+                continue
+            if index_skip_list is not None and idx in index_skip_list:
+                continue
+            print(f"=================== tutorial {idx + 1:3} of {len(tutorials)}: {tutorial}")
+            # print('%s %s (index #%d of %d)' % ('=' * 20, tutorial, idx, len(tutorials) - 1))
+
+            # Directly call venv, necessary for github action runner
+            cmd = 'python -m ' + tutorial
+
+            # print(cmd)
+            result = subprocess.check_output(cmd, shell=True)
+            if result:
+                print(f"ERROR: Got a result of: {result}.")
+
+def all_tutorials():
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(file_path)
+
+    # Set an environment variable that will just run on_update() and on_draw()
+    # once, then quit.
+    os.environ['ARCADE_TEST'] = "TRUE"
+
+    indices_in_range = None
+    index_skip_list = None
+    run_tutorials(indices_in_range, index_skip_list)
+
+
+all_tutorials()
