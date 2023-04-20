ae863d71159491b8726c5b1e1497fcf6085f3fa3
Grant Hur <22hurg@sjchrisitan.org>
2023-04-20 10:25:29 -0700

    .

--- arcade/text.py
+++ arcade/text.py
@@ -18,6 +18,7 @@ __all__ = ["load_font",
             "draw_text"
             ]
 
+
 def load_font(path: Union[str, Path]) -> None:
     """
     Load fonts in a file (usually .ttf) adding them to a global font registry.
