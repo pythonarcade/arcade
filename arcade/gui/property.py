3698b0f7c95198dc47bce3cab3742ffdd2190db9
Grant Hur <22hurg@sjchrisitan.org>
2023-04-28 09:55:50 -0700

    Attempt to fix property

--- arcade/gui/property.py
+++ arcade/gui/property.py
@@ -163,7 +163,7 @@ class _ObservableDict(dict):
         dict.setdefault(self, *largs)
         self.dispatch()
 
-    def update(self, *largs: Iterable) -> None:
+    def update(self, *largs: Union[Iterable, MutableMapping]) -> None:
         dict.update(self, *largs)
         self.dispatch()
 
@@ -192,7 +192,7 @@ class _ObservableList(list):
     def dispatch(self) -> None:
         self.prop.dispatch(self.obj(), self)
 
-    def __setitem__(self, key: Union[int, slice], value: Any) -> None:
+    def __setitem__(self, key: Union[int, slice, list], value: Any) -> None:
         list.__setitem__(self, key, value)
         self.dispatch()
 
