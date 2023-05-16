cc3999564930c41ab21cfae3545059307b74fe20
pushfoo <36696816+pushfoo@users.noreply.github.com>
2023-05-16 15:03:09 -0400

    Document & type annotate HitBox

--- arcade/hitbox/base.py
+++ arcade/hitbox/base.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from math import cos, radians, sin
-from typing import Any, Tuple
+from typing import Any, Tuple, Optional
 
 from PIL.Image import Image
 
@@ -9,6 +9,12 @@ from arcade.types import Point, PointList
 
 
 __all__ = ["HitBoxAlgorithm", "HitBox", "RotatableHitBox"]
+
+
+# Speed / typing workaround:
+# 1. Makes sure we don't reallocate new empty tuples
+# 2. Allows HitBox & subclass typing annotation to work cleanly
+_EMPTY_TUPLE = tuple()
 
 
 class HitBoxAlgorithm:
@@ -63,6 +69,19 @@ class HitBoxAlgorithm:
 
 
 class HitBox:
+    """
+    A basic hit box class supporting scaling.
+
+    It includes support for rescaling as well as shorthand properties
+    for boundary values along the X and Y axes. For rotation support,
+    use :py:meth:`.create_rotatable` to create an instance of
+    :py:class:`RotatableHitBox`.
+
+    :param points: The unmodified points bounding the hit box
+    :param position: The center around which the points will be offset
+    :param scale: The X and Y scaling factors to use when offsetting the
+        points
+    """
     def __init__(
         self,
         points: PointList,
@@ -78,15 +97,27 @@ class HitBox:
         self._bottom = None
         self._top = None
 
-        self._adjusted_points = None
+        # This empty tuple will be replaced the first time
+        # get_adjusted_points is called
+        self._adjusted_points: PointList = _EMPTY_TUPLE
         self._adjusted_cache_dirty = True
 
     @property
-    def points(self):
+    def points(self) -> PointList:
+        """
+        The raw, unadjusted points of this hit box.
+
+        These are the points as originally passed before offsetting, scaling,
+        and any operations subclasses may perform, such as rotation.
+        """
         return self._points
 
     @property
-    def position(self):
+    def position(self) -> Point:
+        """
+        The center point used to offset the final adjusted positions.
+        :return:
+        """
         return self._position
 
     @position.setter
@@ -95,31 +126,48 @@ class HitBox:
         self._adjusted_cache_dirty = True
 
     @property
-    def left(self):
+    def left(self) -> float:
+        """
+        Calculates the leftmost adjusted x position of this hit box
+        """
         points = self.get_adjusted_points()
         x_points = [point[0] for point in points]
         return min(x_points)
 
     @property
-    def right(self):
+    def right(self) -> float:
+        """
+        Calculates the rightmost adjusted x position of this hit box
+        """
         points = self.get_adjusted_points()
         x_points = [point[0] for point in points]
         return max(x_points)
 
     @property
-    def top(self):
+    def top(self) -> float:
+        """
+        Calculates the topmost adjusted y position of this hit box
+        """
         points = self.get_adjusted_points()
         y_points = [point[1] for point in points]
         return max(y_points)
 
     @property
-    def bottom(self):
+    def bottom(self) -> float:
+        """
+        Calculates the bottommost adjusted y position of this hit box
+        """
         points = self.get_adjusted_points()
         y_points = [point[1] for point in points]
         return min(y_points)
 
     @property
-    def scale(self):
+    def scale(self) -> Tuple[float, float]:
+        """
+        The X & Y scaling factors for the points in this hit box.
+
+        These are used to calculate the final adjusted positions of points.
+        """
         return self._scale
 
     @scale.setter
@@ -131,11 +179,27 @@ class HitBox:
         self,
         angle: float = 0.0,
     ) -> RotatableHitBox:
+        """
+        Create a rotatable instance of this hit box.
+
+        The internal point list is transferred directly instead of
+        deepcopied, so care should be taken if using a mutable internal
+        representation.
+
+        :param angle: The angle to rotate points by (0 by default)
+        :return:
+        """
         return RotatableHitBox(
             self._points, position=self._position, scale=self._scale, angle=angle
         )
 
-    def get_adjusted_points(self):
+    def get_adjusted_points(self) -> PointList:
+        """
+        Return the positions of points, scaled and offset from the center.
+
+        Unlike the boundary helper properties (left, etc), this method will
+        only recalculate the values if something was changed.
+        """
         if not self._adjusted_cache_dirty:
             return self._adjusted_points
 
