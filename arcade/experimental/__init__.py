20eb8f94b895b162b812153a27848b45c9eddb51
Grant Hur <fifttim@gmail.com>
2023-05-17 17:27:12 -0700

    accidentally added a space

--- arcade/experimental/__init__.py
+++ arcade/experimental/__init__.py
@@ -2,7 +2,6 @@
 Experimental stuff. API may change.
 """
 from .video_player import VideoPlayer, VideoPlayerView
-
 from .texture_render_target import RenderTargetTexture
 from .shadertoy import Shadertoy, ShadertoyBuffer, ShadertoyBase
 from .crt_filter import CRTFilter
