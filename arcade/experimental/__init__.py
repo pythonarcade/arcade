8df8bd65491a163fde791da3e4d4006c8e9c0d55
Mr Ward <award@kerikerihigh.ac.nz>
2023-05-19 09:08:57 +1200

    remove error on cv2 not found

--- arcade/experimental/__init__.py
+++ arcade/experimental/__init__.py
@@ -7,14 +7,11 @@ from .shadertoy import Shadertoy, ShadertoyBuffer, ShadertoyBase
 from .crt_filter import CRTFilter
 from .bloom_filter import BloomFilter
 
-def raiseModuleNotFoundError():
-    raise ModuleNotFoundError("cv2 is not installed. Run 'pip install opencv-python'")
-
 # Keep cv2 an optional dependency
 try:
     from .video_cv2 import VideoPlayerCV2, CV2PlayerView
 except ImportError:
-    VideoPlayerCV2 = CV2PlayerView = raiseModuleNotFoundError
+    pass
 
 __all__ = [
     "VideoPlayer",
