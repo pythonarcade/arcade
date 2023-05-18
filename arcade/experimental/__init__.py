41271ebb030d31dcc40a61e3287ede4d0132bc97
Mr Ward <award@kerikerihigh.ac.nz>
2023-05-18 23:29:39 +1200

    exception handling

--- arcade/experimental/__init__.py
+++ arcade/experimental/__init__.py
@@ -2,12 +2,19 @@
 Experimental stuff. API may change.
 """
 from .video_player import VideoPlayer, VideoPlayerView
-from .video_cv2 import VideoPlayerCV2, CV2PlayerView
 from .texture_render_target import RenderTargetTexture
 from .shadertoy import Shadertoy, ShadertoyBuffer, ShadertoyBase
 from .crt_filter import CRTFilter
 from .bloom_filter import BloomFilter
 
+def raiseModuleNotFoundError():
+    raise ModuleNotFoundError("cv2 is not installed. Run 'pip install opencv-python'")
+
+# Keep cv2 an optional dependency
+try:
+    from .video_cv2 import VideoPlayerCV2, CV2PlayerView
+except ImportError:
+    VideoPlayerCV2 = CV2PlayerView = raiseModuleNotFoundError
 
 __all__ = [
     "VideoPlayer",
