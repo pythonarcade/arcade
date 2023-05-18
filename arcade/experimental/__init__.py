e2c1dd544bb5093ee78f9d5319958d65f21cc92c
Grant Hur <22hurg@sjchrisitan.org>
2023-05-17 17:30:09 -0700

    .

--- arcade/experimental/__init__.py
+++ arcade/experimental/__init__.py
@@ -1,7 +1,7 @@
 """
 Experimental stuff. API may change.
 """
-from .video_player import VideoPlayer
+from .video_player import VideoPlayer, VideoPlayerView
 from .texture_render_target import RenderTargetTexture
 from .shadertoy import Shadertoy, ShadertoyBuffer, ShadertoyBase
 from .crt_filter import CRTFilter
@@ -10,6 +10,7 @@ from .bloom_filter import BloomFilter
 
 __all__ = [
     "VideoPlayer",
+    "VideoPlayerView",
     "RenderTargetTexture",
     "Shadertoy",
     "ShadertoyBuffer",
