a833c58208a90a5088b7de2e82b3b301037d15e4
cspotcode <cspotcode@gmail.com>
2023-07-28 17:04:10 -0400

    WIP

--- benchmarks/arcade-spritelist-vs-pyglet-batch/README.md
+++ benchmarks/arcade-spritelist-vs-pyglet-batch/README.md
@@ -1,5 +1,14 @@
-To run this benchmark, from the root directory:
+To run this benchmark, have the companion pyglet branch installed:
 
 ```shell
-hyperfine --runs 3 --parameter-list bench pyglet,arcade 'python ./benchmarks/arcade-spritelist-vs-pyglet-batch/bench.py {bench}' --show-output
+git clone https://github.com/cspotcode/pyglet
+cd pyglet
+git checkout cspotcode/geosprite_opt
+pip install -e .
+```
+
+Then from the root directory of your `arcade` clone:
+
+```shell
+hyperfine --runs 3 --parameter-list bench pyglet_sprite,pyglet_geosprite,arcade 'python ./benchmarks/arcade-spritelist-vs-pyglet-batch/bench.py {bench}' --show-output
 ```
--- benchmarks/arcade-spritelist-vs-pyglet-batch/bench.py
+++ benchmarks/arcade-spritelist-vs-pyglet-batch/bench.py
@@ -2,14 +2,24 @@ import math
 import sys
 import arcade
 import pyglet
+import pyglet.experimental.geoshader_sprite
 import random
 import time
+import statprof
+import psutil
+
+# Does not work on Windows.  Might work on Linux
+use_statprof = False
 
 print(sys.argv)
+is_pyglet_geosprite = False
 if sys.argv[1] == 'arcade':
     is_arcade = True
-elif sys.argv[1] == 'pyglet':
+elif sys.argv[1] == 'pyglet_sprite':
     is_arcade = False
+elif sys.argv[1] == 'pyglet_geosprite':
+    is_arcade = False
+    is_pyglet_geosprite = True
 else:
     raise Exception('')
 
@@ -23,26 +33,21 @@ WALLS_COUNT = 10
 
 BULLET_VELOCITY_MIN = 1/60
 BULLET_VELOCITY_MAX = 10/60
-BULLET_COUNT = 1000
+BULLET_COUNT = 9000
 
-SIMULATE_MINUTES = 1
+SIMULATE_MINUTES = 3
 SIMULATE_FPS = 60
 
 # Predictable randomization so that each benchmark is identical
 rng = random.Random(0)
 
-bullets = arcade.SpriteList() if is_arcade else []
-# walls = arcade.SpriteList()
+bullets = arcade.SpriteList(use_spatial_hash=False) if is_arcade else []
 
 window = arcade.Window()
 
 # Seed chosen manually to create a wall distribution that looked good enough,
 # like something I might create in a game.
 rng.seed(2)
-# for i in range(0, WALLS_COUNT):
-#     wall = arcade.SpriteSolidColor(rng.randint(WALL_DIM_MIN, WALL_DIM_MAX), rng.randint(WALL_DIM_MIN, WALL_DIM_MAX), color=arcade.color.BLACK)
-#     wall.position = rng.randint(0, SCREEN_WIDTH), rng.randint(0, SCREEN_HEIGHT)
-#     walls.append(wall)
 
 png_path = 'benchmarks/arcade-spritelist-vs-pyglet-batch/image.png'
 arcade_texture = arcade.load_texture(png_path)
@@ -61,9 +66,13 @@ def create_bullet_arcade():
 pyglet_batch = pyglet.graphics.Batch()
 pyglet_image = pyglet.image.load(png_path)
 
+class MyPygletSprite(pyglet.experimental.geoshader_sprite.Sprite):
+    __slots__ = ['velocity']
+
+PygletSpriteImpl = MyPygletSprite if is_pyglet_geosprite else pyglet.sprite.Sprite
 def create_bullet_pyglet():
     # Create a new bullet
-    new_bullet = pyglet.sprite.Sprite(pyglet_image, batch=pyglet_batch, subpixel=True)
+    new_bullet = PygletSpriteImpl(img=pyglet_image, batch=pyglet_batch, subpixel=True)
     new_bullet.position = (rng.randint(0, SCREEN_WIDTH), rng.randint(0, SCREEN_HEIGHT), 0)
     speed = rng.random() * (BULLET_VELOCITY_MAX - BULLET_VELOCITY_MIN) + BULLET_VELOCITY_MIN
     angle = rng.random() * math.pi * 2
@@ -76,11 +85,13 @@ def create_bullet_pyglet():
 def move_bullets_arcade():
     # Move all bullets
     for bullet in bullets:
-        bullet.position = (bullet.position[0] + bullet.velocity[0], bullet.position[1] + bullet.velocity[1])
+        x, y = bullet.position
+        bullet.position = (x + bullet.velocity[0], y + bullet.velocity[1])
 def move_bullets_pyglet():
     # Move all bullets
     for bullet in bullets:
-        bullet.position = (bullet.position[0] + bullet.velocity[0], bullet.position[1] + bullet.velocity[1], 0)
+        x, y, _ = bullet.position
+        bullet.position = (x + bullet.velocity[0], y + bullet.velocity[1], 0)
 
 
 def draw_bullets_arcade():
@@ -96,6 +107,9 @@ draw_bullets = draw_bullets_arcade if is_arcade else draw_bullets_pyglet
 for i in range(0, BULLET_COUNT):
     bullets.append(create_bullet())
 
+if use_statprof:
+    statprof.start()
+
 for i in range(0, int(SIMULATE_MINUTES * 60 * SIMULATE_FPS)):
     pyglet.clock.tick()
 
@@ -106,6 +120,11 @@ for i in range(0, int(SIMULATE_MINUTES * 60 * SIMULATE_FPS)):
     window.dispatch_event('on_draw')
 
     window.clear(color=arcade.color.WHITE)
-    # walls.draw()
     draw_bullets()
     window.flip()
+
+if use_statprof:
+    statprof.stop()
+    statprof.display()
+
+print(psutil.Process().memory_info().rss / (1024 * 1024))
