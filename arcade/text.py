8bc49a12ea28fe4dbc9fc1cc0ee1fc2e98e9e641
Grant Hur <fifttim@gmail.com>
2023-08-07 17:08:33 -0700

    Update text.py

--- arcade/text.py
+++ arcade/text.py
@@ -198,7 +198,7 @@ class Text:
         if align != "center" and align != "left" and align != "right":
             raise ValueError("The 'align' parameter must be equal to 'left', 'right', or 'center'.")
 
-        if align is "center":
+        if align == "center":
             multiline = True
 
         adjusted_font = _attempt_font_name_resolution(font_name)
