ecdd6aaea5e1edda89845626398308769962e1fd
pushfoo <36696816+pushfoo@users.noreply.github.com>
2023-09-07 06:09:02 -0400

    Add build ClassDocumenter build warning fix + doc
    * Add monkeypatch for ClassDocumenter to avoid name conflict warning
    
    * Add doc explaining why

--- doc/conf.py
+++ doc/conf.py
@@ -274,6 +274,27 @@ def on_autodoc_process_bases(app, name, obj, options, bases):
     bases[:] = [base for base in bases if base is not object]
 
 class ClassDocumenter(sphinx.ext.autodoc.ClassDocumenter):
+    """A replacement for the default autodocumenter.
+
+    .. warning:: You must monkeypatch the baseclass with this!
+
+                 .. code-block:: python
+
+                    sphinx.ext.autodoc.ClassDocumenter = ClassDocumenter
+
+    Why? New ClassDocumenter subclasses appear to be registered for
+    specific names. For example, ``.. autointenum::`` would be declared
+    as follows::
+
+        class IntEnumDocumenter(ClassDocumenter):
+            objtype = 'intenum'
+            # Full class omitted, taken from the extension tutorial:
+            # https://www.sphinx-doc.org/en/master/development/tutorials/autodoc_ext.html#writing-the-extension
+
+    However, this documenter is for the default name, so passing it to
+    `app.app_autodocumenter` will produce a warning about a conflict.
+    Arcade's build config promotes warnings to errors, breaking build.
+    """
     def add_directive_header(self, sig: str) -> None:
         r = super().add_directive_header(sig)
         # Strip empty `Bases: `, will be empty when only superclass is `object`
@@ -297,7 +318,9 @@ class Visitor(docutils.nodes.SparseNodeVisitor):
 
 def setup(app):
     app.add_css_file("css/custom.css")
-    app.add_autodocumenter(ClassDocumenter)
+    # IMPORTANT: We can't use app.add_autodocumenter!
+    # See the docstring of ClassDocumenter above for why.
+    sphinx.ext.autodoc.ClassDocumenter = ClassDocumenter
     app.connect('source-read', source_read)
     app.connect('build-finished', post_process)
     app.connect("autodoc-process-docstring", warn_undocumented_members)
