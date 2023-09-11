cf9f2e55e5c1412190d0d710d64ae4b416a99e65
Paul <36696816+pushfoo@users.noreply.github.com>
2023-09-11 10:45:30 -0400

    Add temp fix for unit test failure (#1895)

--- tests/unit/test_key.py
+++ tests/unit/test_key.py
@@ -2,4 +2,7 @@
 def test_key():
     from arcade import key
     names = key.__dict__.keys()
-    assert 214 == len(names)
+    # temp fix which will be replaced with fuller, more meaningful
+    # tests in the coming days. The values are as follows:
+    # 214 key constants + from __future__ import annotations
+    assert 215 == len(names)
