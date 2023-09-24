a0a0c02506e8eccd573b89dbc4ebcc59b3715b0b
Einar Forselv <eforselv@gmail.com>
2023-09-24 23:25:40 +0200

    Basic asserts in geometry testing

--- tests/unit/gl/test_opengl_geometry.py
+++ tests/unit/gl/test_opengl_geometry.py
@@ -5,12 +5,21 @@ from arcade.gl import geometry
 
 
 def test_quad_2d_fs(ctx):
-    geometry.quad_2d_fs()
+    geo = geometry.quad_2d_fs()
+    assert geo.ctx == ctx
+    assert geo.num_vertices == 4
+    assert geo._mode == ctx.TRIANGLE_STRIP
 
 
 def test_quad_2d(ctx):
-    geometry.quad_2d()
+    geo = geometry.quad_2d()
+    assert geo.ctx == ctx
+    assert geo.num_vertices == 4
+    assert geo._mode == ctx.TRIANGLE_STRIP
 
 
 def test_screen_rectangle(ctx):
-    geometry.screen_rectangle(0, 100, 0, 100)
+    geo = geometry.screen_rectangle(0, 100, 0, 100)
+    assert geo.ctx == ctx
+    assert geo.num_vertices == 4
+    assert geo._mode == ctx.TRIANGLE_STRIP
