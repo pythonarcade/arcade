acd403b9a81095f2edb3ee5f8f027e86ed28bce9
DragonMoffon <happydog.aja@gmail.com>
2023-12-24 16:12:46 +1300

    Improved Orthographic and Perspective Unit Tests

--- tests/unit/camera/test_orthographic_camera.py
+++ tests/unit/camera/test_orthographic_camera.py
@@ -6,52 +6,31 @@ from arcade import camera, Window
 def test_orthographic_projector_use(window: Window):
     # Given
     from pyglet.math import Mat4
-    cam_default = camera.OrthographicProjector()
+    camera_default = camera.OrthographicProjector()
 
-    view_matrix = Mat4((
-        1.0,    0.0,    0.0, 0.0,
-        0.0,    1.0,    0.0, 0.0,
-        0.0,    0.0,    1.0, 0.0,
-        -400.0, -300.0, 0.0, 1.0
-    ))
-    proj_matrix = Mat4((
-        1.0/400.0, 0.0,   0.0,   0.0,
-        0.0,       1/300, 0.0,   0.0,
-        0.0,       0.0,   -0.01, 0.0,
-        0.0,       0.0,   0.0,   1.0
-    ))
+    view_matrix = camera_default._generate_view_matrix()
+    proj_matrix = camera_default._generate_projection_matrix()
 
     # When
-    cam_default.use()
+    camera_default.use()
 
     # Then
-    assert window.current_camera is cam_default
+    assert window.current_camera is camera_default
     assert window.ctx.view_matrix == view_matrix
     assert window.ctx.projection_matrix == proj_matrix
 
 
 def test_orthographic_projector_activate(window: Window):
     # Given
-    from pyglet.math import Mat4
-    cam_default = camera.OrthographicProjector()
+    camera_default: camera.OrthographicProjector = camera.OrthographicProjector()
 
-    view_matrix = Mat4((
-        1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        -400.0, -300.0, 0.0, 1.0
-    ))
-    proj_matrix = Mat4((
-        1.0 / 400.0, 0.0, 0.0, 0.0,
-        0.0, 1 / 300, 0.0, 0.0,
-        0.0, 0.0, -0.01, 0.0,
-        0.0, 0.0, 0.0, 1.0
-    ))
+    view_matrix = camera_default._generate_view_matrix()
+    proj_matrix = camera_default._generate_projection_matrix()
 
     # When
-    with cam_default.activate() as cam:
+    with camera_default.activate() as cam:
         # Initially
-        assert window.current_camera is cam is cam_default
+        assert window.current_camera is cam is camera_default
         assert window.ctx.view_matrix == view_matrix
         assert window.ctx.projection_matrix == proj_matrix
 
@@ -61,7 +40,7 @@ def test_orthographic_projector_activate(window: Window):
 
 def test_orthographic_projector_map_coordinates(window: Window):
     # Given
-    cam_default = camera.OrthographicProjector()
+    camera_default = camera.OrthographicProjector()
 
     # When
     mouse_pos_a = (100.0, 100.0)
@@ -69,15 +48,15 @@ def test_orthographic_projector_map_coordinates(window: Window):
     mouse_pos_c = (230.0, 800.0)
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((100.0, 0.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_c) == pytest.approx((230.0, 800.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((100.0, 0.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_c) == pytest.approx((230.0, 800.0, 0.0))
 
 
 def test_orthographic_projector_map_coordinates_move(window: Window):
     # Given
-    cam_default = camera.OrthographicProjector()
-    default_view = cam_default.view
+    camera_default = camera.OrthographicProjector()
+    default_view = camera_default.view
 
     mouse_pos_a = (window.width//2, window.height//2)
     mouse_pos_b = (100.0, 100.0)
@@ -86,8 +65,8 @@ def test_orthographic_projector_map_coordinates_move(window: Window):
     default_view.position = (0.0, 0.0, 0.0)
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((0.0, 0.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((-300.0, -200.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((0.0, 0.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((-300.0, -200.0, 0.0))
 
     # And
 
@@ -95,14 +74,14 @@ def test_orthographic_projector_map_coordinates_move(window: Window):
     default_view.position = (100.0, 100.0, 0.0)
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((-200.0, -100.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((-200.0, -100.0, 0.0))
 
 
 def test_orthographic_projector_map_coordinates_rotate(window: Window):
     # Given
-    cam_default = camera.OrthographicProjector()
-    default_view = cam_default.view
+    camera_default = camera.OrthographicProjector()
+    default_view = camera_default.view
 
     mouse_pos_a = (window.width//2, window.height//2)
     mouse_pos_b = (100.0, 100.0)
@@ -112,8 +91,8 @@ def test_orthographic_projector_map_coordinates_rotate(window: Window):
     default_view.position = (0.0, 0.0, 0.0)
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((0.0, 0.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((-200.0, 300.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((0.0, 0.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((-200.0, 300.0, 0.0))
 
     # And
 
@@ -122,14 +101,14 @@ def test_orthographic_projector_map_coordinates_rotate(window: Window):
     default_view.position = (100.0, 100.0, 0.0)
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((-253.553390, 170.710678, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((-253.553390, 170.710678, 0.0))
 
 
 def test_orthographic_projector_map_coordinates_zoom(window: Window):
     # Given
-    cam_default = camera.OrthographicProjector()
-    default_view = cam_default.view
+    camera_default = camera.OrthographicProjector()
+    default_view = camera_default.view
 
     mouse_pos_a = (window.width, window.height)
     mouse_pos_b = (100.0, 100.0)
@@ -138,8 +117,8 @@ def test_orthographic_projector_map_coordinates_zoom(window: Window):
     default_view.zoom = 2.0
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((window.width*0.75, window.height*0.75, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((250.0, 200.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((window.width*0.75, window.height*0.75, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((250.0, 200.0, 0.0))
 
     # And
 
@@ -148,5 +127,18 @@ def test_orthographic_projector_map_coordinates_zoom(window: Window):
     default_view.zoom = 0.25
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((window.width*2.0, window.height*2.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((-1200.0, -800.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((window.width*2.0, window.height*2.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((-1200.0, -800.0, 0.0))
+
+
+def test_perspective_projector_map_coordinates_depth(window):
+    # Given
+    camera_default = camera.PerspectiveProjector()
+    default_view = camera_default.view
+
+    mouse_pos_a = (window.width, window.height)
+    mouse_pos_b = (100.0, 100.0)
+
+    # When
+
+    # Then
--- tests/unit/camera/test_perspective_camera.py
+++ tests/unit/camera/test_perspective_camera.py
@@ -5,34 +5,39 @@ from arcade import camera, Window
 
 def test_perspective_projector_use(window: Window):
     # Given
-    from pyglet.math import Mat4
-    cam_default = camera.PerspectiveProjector()
+    camera_default = camera.PerspectiveProjector()
 
-    view_matrix = Mat4((
-        1.0,    0.0,    0.0, 0.0,
-        0.0,    1.0,    0.0, 0.0,
-        0.0,    0.0,    1.0, 0.0,
-        -400.0, -300.0, 0.0, 1.0
-    ))
-    proj_matrix = Mat4((
-        0.7500000000000002,                0.0,                  0.0,  0.0,
-                       0.0, 1.0000000000000002,                  0.0,  0.0,
-                       0.0,                0.0,  -1.0002000200020003, -1.0,
-                       0.0,                0.0, -0.20002000200020004,  0.0
-    ))
+    view_matrix = camera_default._generate_projection_matrix()
+    proj_matrix = camera_default._generate_projection_matrix()
 
     # When
-    cam_default.use()
+    camera_default.use()
 
     # Then
-    assert window.current_camera is cam_default
+    assert window.current_camera is camera_default
     assert window.ctx.view_matrix == view_matrix
     assert window.ctx.projection_matrix == proj_matrix
 
+def test_perspective_projector_activate(window: Window):
+    # Given
+    camera_default = camera.PerspectiveProjector()
+
+    view_matrix = camera_default._generate_view_matrix()
+    proj_matrix = camera_default._generate_projection_matrix()
+
+    # When
+    with camera_default.activate() as cam:
+        # Initially
+        assert window.current_camera is cam is camera_default
+        assert window.ctx.view_matrix == view_matrix
+        assert window.ctx.projection_matrix == proj_matrix
+
+    # Finally
+    assert window.current_camera is window.default_camera
 
 def test_perspective_projector_map_coordinates(window: Window):
     # Given
-    cam_default = camera.PerspectiveProjector()
+    camera_default = camera.PerspectiveProjector()
 
     # When
     mouse_pos_a = (100.0, 100.0)
@@ -40,15 +45,15 @@ def test_perspective_projector_map_coordinates(window: Window):
     mouse_pos_c = (230.0, 800.0)
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((100.0, 0.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_c) == pytest.approx((230.0, 800.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((100.0, 0.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_c) == pytest.approx((230.0, 800.0, 0.0))
 
 
 def test_perspective_projector_map_coordinates_move(window: Window):
-    # Given
-    cam_default = camera.PerspectiveProjector()
-    default_view = cam_default.view
+    # GivenT
+    camera_default = camera.PerspectiveProjector()
+    default_view = camera_default.view
 
     mouse_pos_a = (window.width//2, window.height//2)
     mouse_pos_b = (100.0, 100.0)
@@ -57,8 +62,8 @@ def test_perspective_projector_map_coordinates_move(window: Window):
     default_view.position = (0.0, 0.0, 0.0)
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((0.0, 0.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((-300.0, -200.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((0.0, 0.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((-300.0, -200.0, 0.0))
 
     # And
 
@@ -66,14 +71,14 @@ def test_perspective_projector_map_coordinates_move(window: Window):
     default_view.position = (100.0, 100.0, 0.0)
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((-200.0, -100.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((-200.0, -100.0, 0.0))
 
 
 def test_perspective_projector_map_coordinates_rotate(window: Window):
     # Given
-    cam_default = camera.PerspectiveProjector()
-    default_view = cam_default.view
+    camera_default = camera.PerspectiveProjector()
+    default_view = camera_default.view
 
     mouse_pos_a = (window.width//2, window.height//2)
     mouse_pos_b = (100.0, 100.0)
@@ -83,8 +88,8 @@ def test_perspective_projector_map_coordinates_rotate(window: Window):
     default_view.position = (0.0, 0.0, 0.0)
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((0.0, 0.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((-200.0, 300.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((0.0, 0.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((-200.0, 300.0, 0.0))
 
     # And
 
@@ -93,14 +98,14 @@ def test_perspective_projector_map_coordinates_rotate(window: Window):
     default_view.position = (100.0, 100.0, 0.0)
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((-253.553390, 170.710678, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((100.0, 100.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((-253.553390, 170.710678, 0.0))
 
 
 def test_perspective_projector_map_coordinates_zoom(window: Window):
     # Given
-    cam_default = camera.PerspectiveProjector()
-    default_view = cam_default.view
+    camera_default = camera.PerspectiveProjector()
+    default_view = camera_default.view
 
     mouse_pos_a = (window.width, window.height)
     mouse_pos_b = (100.0, 100.0)
@@ -109,8 +114,8 @@ def test_perspective_projector_map_coordinates_zoom(window: Window):
     default_view.zoom = 2.0
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((window.width*0.75, window.height*0.75, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((250.0, 200.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((window.width*0.75, window.height*0.75, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((250.0, 200.0, 0.0))
 
     # And
 
@@ -119,5 +124,18 @@ def test_perspective_projector_map_coordinates_zoom(window: Window):
     default_view.zoom = 0.25
 
     # Then
-    assert cam_default.map_coordinate(mouse_pos_a) == pytest.approx((window.width*2.0, window.height*2.0, 0.0))
-    assert cam_default.map_coordinate(mouse_pos_b) == pytest.approx((-1200.0, -800.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_a) == pytest.approx((window.width*2.0, window.height*2.0, 0.0))
+    assert camera_default.map_coordinate(mouse_pos_b) == pytest.approx((-1200.0, -800.0, 0.0))
+
+
+def test_perspective_projector_map_coordinates_depth(window):
+    # Given
+    camera_default = camera.PerspectiveProjector()
+    default_view = camera_default.view
+
+    mouse_pos_a = (window.width, window.height)
+    mouse_pos_b = (100.0, 100.0)
+
+    # When
+
+    # Then
