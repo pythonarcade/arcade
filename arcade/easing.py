dd3ebdf13cfb13ec50053292c0efa6754a7c61b2
DigiDuncan <digiduncan@gmail.com>
2024-02-25 00:01:56 -0500

    add some return type annotations

--- arcade/easing.py
+++ arcade/easing.py
@@ -147,7 +147,7 @@ def easing(percent: float, easing_data: EasingData) -> float:
         easing_data.ease_function(percent)
 
 
-def ease_angle(start_angle: float, end_angle: float, *, time=None, rate=None, ease_function: Callable = linear):
+def ease_angle(start_angle: float, end_angle: float, *, time=None, rate=None, ease_function: Callable = linear) -> EasingData:
     """
     Set up easing for angles.
     """
