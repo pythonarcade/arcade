0bc39d5e8b219e4548f435e620638e0ebb378f01
DragonMoffon <happydog.aja@gmail.com>
2024-03-03 23:10:22 +1300

    fixed typo

--- arcade/resources/__init__.py
+++ arcade/resources/__init__.py
@@ -98,7 +98,7 @@ def resolve(path: Union[str, Path]) -> Path:
     try:
         path = Path(path.resolve(strict=True))
     except AttributeError:
-        # WARNING: This is due to an issue caused by Nuitka overriding strings into janky path objec
+        # WARNING: This is due to an issue caused by Nuitka overriding strings into janky path object
         path = Path(path.absolute())
 
     # Always return absolute paths
