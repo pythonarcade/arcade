57670af424cb604193ada62714127cab409b3ab6
Einar Forselv <eforselv@gmail.com>
2024-03-09 18:57:20 +0100

    Working test_strafe

--- tests/unit/camera/test_camera_controller_methods.py
+++ tests/unit/camera/test_camera_controller_methods.py
@@ -4,38 +4,32 @@ from arcade import camera, Window
 import arcade.camera.controllers as controllers
 
 
-def test_strafe(window: Window):
+def test_strafe():
     # Given
     camera_data = camera.CameraData()
     directions = ((1.0, 0.0), (0.0, 1.0), (-1.0, 0.0), (0.0, -1.0), (0.5, 0.5))
 
     # When
     camera_data.forward = (0.0 ,0.0, -1.0)
-    camera_data.up = (0.0, 0.0, 1.0)
+    camera_data.up = (0.0, 1.0, 0.0)
 
     # Then
     for dirs in directions:
         controllers.strafe(camera_data, dirs)
-        assert (
-            camera_data.position == (dirs[0], dirs[1], 0.0),
-            f"Strafe failed to move the camera data correctly, {dirs}"
-        )
+        assert camera_data.position == (dirs[0], dirs[1], 0.0), f"Strafe failed to move the camera data correctly, {dirs}"
         camera_data.position = (0.0, 0.0, 0.0)
 
     # Given
-    camera_data.forward = (0.0, 0.0, -1.0)
-    camera_data.up = (0.0, 0.0, 1.0)
+    camera_data.forward = (1.0, 0.0, 0.0)
+    camera_data.up = (0.0, 1.0, 0.0)
 
     for dirs in directions:
         controllers.strafe(camera_data, dirs)
-        assert (
-            camera_data.position == (0.0, dirs[1], dirs[0]),
-            f"Strafe failed to move the camera data correctly, {dirs}"
-        )
+        assert camera_data.position == (0.0, dirs[1], dirs[0]), f"Strafe failed to move the camera data correctly, {dirs}"
         camera_data.position = (0.0, 0.0, 0.0)
 
 
-def test_rotate_around_forward(window: Window):
+def test_rotate_around_forward():
     # TODO
 
     # Given
