45c9e3a7fac76008cc0eadc724bbc6edabe04211
Maic Siemering <maic@siemering.tech>
2024-03-25 23:22:25 +0100

    GUI: Add UIView

---
+++ arcade/gui/view.py
@@ -0,0 +1,23 @@
+from arcade import View
+from arcade.gui import UIManager
+
+
+class UIView(View):
+    """This view provides basic GUI setup.
+
+    This is a convenience class, which adds the UIManager to the view under `self.ui`.
+    The UIManager is enabled when the view is shown and disabled when the view is hidden.
+
+    If you override this class, don't forget to call super().on_show_view() and super().on_hide_view().
+
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.ui = UIManager()
+
+    def on_show_view(self):
+        self.ui.enable()
+
+    def on_hide_view(self):
+        self.ui.disable()
