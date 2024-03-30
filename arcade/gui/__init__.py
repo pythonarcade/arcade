61a007057e8ae562106e63aaa43c4be368acb1ef
Maic Siemering <maic@siemering.tech>
2024-03-30 19:05:38 +0100

    Expose UISlider in arcade.gui

--- arcade/gui/__init__.py
+++ arcade/gui/__init__.py
@@ -11,38 +11,38 @@ from arcade.gui.events import UIMouseMovementEvent
 from arcade.gui.events import UIMousePressEvent
 from arcade.gui.events import UIMouseReleaseEvent
 from arcade.gui.events import UIMouseScrollEvent
-from arcade.gui.events import UIOnClickEvent
+from arcade.gui.events import UIOnActionEvent
 from arcade.gui.events import UIOnChangeEvent
+from arcade.gui.events import UIOnClickEvent
 from arcade.gui.events import UIOnUpdateEvent
 from arcade.gui.events import UITextEvent
-from arcade.gui.events import UIOnActionEvent
 from arcade.gui.events import UITextMotionEvent
 from arcade.gui.events import UITextMotionSelectEvent
-from arcade.gui.style import UIStyleBase, UIStyledWidget
-from arcade.gui.surface import Surface
-from arcade.gui.nine_patch import NinePatchTexture
-from arcade.gui.ui_manager import UIManager
-from arcade.gui.widgets import UILayout
-from arcade.gui.widgets.layout import (
-    UIBoxLayout,
-    UIAnchorLayout,
-    UIGridLayout
-)
-from arcade.gui.widgets import UIDummy, Rect
-from arcade.gui.widgets import UIInteractiveWidget
-from arcade.gui.widgets.text import UILabel, UIInputText, UITextArea, UITextWidget
-from arcade.gui.widgets.toggle import UITextureToggle
-from arcade.gui.widgets.image import UIImage
-from arcade.gui.widgets import UISpace
-from arcade.gui.widgets.dropdown import UIDropdown
-from arcade.gui.widgets import UISpriteWidget
-from arcade.gui.widgets.buttons import UITextureButton, UITextureButtonStyle, UIFlatButton
-from arcade.gui.widgets.slider import UISlider, UISliderStyle
-from arcade.gui.widgets import UIWidget
-from arcade.gui.property import ListProperty, DictProperty, Property, bind, unbind
 from arcade.gui.mixins import UIDraggableMixin
 from arcade.gui.mixins import UIMouseFilterMixin
 from arcade.gui.mixins import UIWindowLikeMixin
+from arcade.gui.nine_patch import NinePatchTexture
+from arcade.gui.property import ListProperty, DictProperty, Property, bind, unbind
+from arcade.gui.style import UIStyleBase, UIStyledWidget
+from arcade.gui.surface import Surface
+from arcade.gui.ui_manager import UIManager
+from arcade.gui.widgets import UIDummy, Rect
+from arcade.gui.widgets import UIInteractiveWidget
+from arcade.gui.widgets import UILayout
+from arcade.gui.widgets import UISpace
+from arcade.gui.widgets import UISpriteWidget
+from arcade.gui.widgets import UIWidget
+from arcade.gui.widgets.buttons import (
+    UITextureButton,
+    UITextureButtonStyle,
+    UIFlatButton,
+)
+from arcade.gui.widgets.dropdown import UIDropdown
+from arcade.gui.widgets.image import UIImage
+from arcade.gui.widgets.layout import UIBoxLayout, UIAnchorLayout, UIGridLayout
+from arcade.gui.widgets.slider import UIBaseSlider, UISlider, UITextureSlider, UISliderStyle
+from arcade.gui.widgets.text import UILabel, UIInputText, UITextArea, UITextWidget
+from arcade.gui.widgets.toggle import UITextureToggle
 
 __all__ = [
     "UIAnchorLayout",
@@ -76,7 +76,9 @@ __all__ = [
     "UIOnActionEvent",
     "UIOnChangeEvent",
     "UIOnClickEvent",
+    "UIBaseSlider",
     "UISlider",
+    "UITextureSlider",
     "UISliderStyle",
     "UIStyleBase",
     "UIStyledWidget",
