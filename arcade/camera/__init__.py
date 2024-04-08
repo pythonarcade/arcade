6687fa039f71ca7226ef91507b15901264c3c6de
Paul <36696816+pushfoo@users.noreply.github.com>
2024-04-08 17:15:08 -0400

    Delete SimpleCamera (#2054)
    * Remove SimpleCamera from arcade.camera's __init__.py
    
    * Remove arcade/camera/simple_camera.py
    
    * Update platformer tutorial step 7's .rst to account for Camera2D changes
    
    * Use consistent code style for the class in step 7

--- arcade/camera/__init__.py
+++ arcade/camera/__init__.py
@@ -14,7 +14,6 @@ from arcade.camera.data_types import (
 from arcade.camera.orthographic import OrthographicProjector
 from arcade.camera.perspective import PerspectiveProjector
 
-from arcade.camera.simple_camera import SimpleCamera
 from arcade.camera.camera_2d import Camera2D
 
 import arcade.camera.grips as grips
@@ -28,7 +27,6 @@ __all__ = [
     'OrthographicProjector',
     'PerspectiveProjectionData',
     'PerspectiveProjector',
-    'SimpleCamera',
     'Camera2D',
     'grips'
 ]
