1a1d1bf75b33020cf4c43f2e741259f702215398
DigiDuncan <digiduncan@gmail.com>
2024-04-08 04:01:23 -0400

    add window center properties

--- arcade/application.py
+++ arcade/application.py
@@ -915,6 +915,18 @@ class Window(pyglet.window.Window):
         """
         pass
 
+    @property
+    def center(self) -> tuple[float, float]:
+        return (self.width / 2, self.height / 2)
+
+    @property
+    def center_x(self) -> float:
+        return self.width / 2
+
+    @property
+    def center_y(self) -> float:
+        return self.height / 2
+
 
 def open_window(
     width: int,
