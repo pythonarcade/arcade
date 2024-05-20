22406462d4dec7dd2a7756a630b33974485e255d
pushfoo <36696816+pushfoo@users.noreply.github.com>
2024-05-20 16:56:26 -0400

    Replace commented-out __repr__ with comment on __str__

--- arcade/types/rect.py
+++ arcade/types/rect.py
@@ -306,12 +306,8 @@ class Rect(NamedTuple):
                 "width": self.width,
                 "height": self.height}
 
-    # def __repr__(self) -> str:
-    #     return (
-    #         f"<{self.__class__.__name__} "
-    #         f"left={self.left} right={self.right} "
-    #     )
-
+    # Since __repr__ is handled automatically by NamedTuple, we focus on
+    # human-readable spot-check values for __str__ instead.
     def __str__(self) -> str:
         return (
             f"<{self.__class__.__name__} LRBT({self.left}, {self.right}, {self.bottom}, {self.top})"
