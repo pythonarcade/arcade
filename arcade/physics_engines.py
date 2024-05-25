d4db1f93a6de4a52f7af08923a1dfa28d25b905a
FriendlyGecko <68018798+FriendlyGecko@users.noreply.github.com>
2024-05-25 13:36:06 +0300

    Fixed so it will pass tests
    I changed the default jump_delay to 0 so that the test will properly see a true value for can_jump with multijump.
    
    I argue that 10 ticks is a better number than 0 as a default but that will require updating the test.

--- arcade/physics_engines.py
+++ arcade/physics_engines.py
@@ -422,7 +422,7 @@ class PhysicsEnginePlatformer:
         else:
             return False
 
-    def enable_multi_jump(self, allowed_jumps: int, jump_delay: int = 10):
+    def enable_multi_jump(self, allowed_jumps: int, jump_delay: int = 0):
         """
         Enables multi-jump.
         allowed_jumps should include the initial jump.
