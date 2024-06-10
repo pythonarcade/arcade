268cfa408c8fcb6ff2a2a13d10a77d9e02c70974
pushfoo <36696816+pushfoo@users.noreply.github.com>
2024-06-10 04:26:59 -0400

    Clearly document history and intent of vfs.py
    * Add a top-level docstring explaining the build loop
    
    * Add docstrings explaining intended usage in build scripts for IDE users

--- util/vfs.py
+++ util/vfs.py
@@ -1,3 +1,32 @@
+"""Virtual filesystem to prevent infinite sphinx rebuild loops.
+
+TL;DR: Fix bad Sphinx cache busts by only writing when data would change
+
+The helpers in this module aggregate writes to virtual files. When a
+sync to disk is requested, the on-disk contents of each path are read
+before writing. If the written data would be identical to the current
+contents, the write operation is skipped for that file.
+
+Normally, any change to project .rst files triggers the sphinx-autobuild
+used in Arcade's ./make.py serve command. However, Arcade's doc build
+generates .rst files for the items below:
+
+* API pages
+* resource listings
+* A quick index file
+
+This behavior predates our adoption of sphinx-autobuild. Since writing
+is the trigger condition instead of content change, it can trigger an
+infinite loop as follows:
+
+1. Sphinx runs our auto-generation scripts
+2. sphinx-autobuild detects the modification to the files
+3. The process repeats infinitely
+
+We can prevent this loop by limiting writes. This module achieves this
+by reading each file before write and aborting if its contents would be
+unchanged.
+"""
 import os
 from contextlib import suppress
 from pathlib import Path
@@ -5,12 +34,19 @@ from typing import Union
 
 
 class Vfs:
+    """In-memory file system with sync support.
+
+    Intended use is as follows:
+
+    1. Create a Vfs object
+    2. For any file a build script would change:
+
+       1. Use vfs.open("file/path/here")
+       2. Use the pathlib.Path-ish methods of the file, especially write
+
+    3. Once done, call vfs_instance.write() to sync to disk
     """
-    Virtual filesystem: write files in-memory, then sync to real fs when done.
-    Used to avoid touching files that would not change.
-    This avoids invalidating sphinx cache and causing endless rebuild loops w/
-    sphinx-autobuild.
-    """
+
     def __init__(self):
         self.files: dict[str, VirtualFile] = dict()
         self.files_to_delete: set[Path] = set()
@@ -29,9 +65,16 @@ class Vfs:
             self.files_to_delete.add(p)
 
     def write(self):
-        """
-        Sync all files of this Vfs to the real filesystem, and delete any files
-        from previous builds.
+        """Sync all files of this Vfs to the real filesystem.
+
+        This performs the following actions:
+
+        1. For each file, call disk sync to:
+
+           1. Read the file's current contents
+           2. Abort write if the data inside would not change
+
+        2. Delete old files
         """
         file_paths = [file.path for file in self.files.values()]
         for file in self.files.values():
