c0de1ddda93cb5726fc13fc9bcfcbbe18a174841
Einar Forselv <eforselv@gmail.com>
2024-06-28 02:18:07 +0200

    Example batch

--- arcade/examples/shapes.py
+++ arcade/examples/shapes.py
@@ -12,11 +12,11 @@ import arcade
 import random
 
 # Set up the constants
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 SCREEN_TITLE = "Shapes!"
 
-NUMBER_OF_SHAPES = 250
+NUMBER_OF_SHAPES = 200
 
 
 class Shape:
