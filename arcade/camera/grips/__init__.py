658a6d38c5413b0e39091129c203e0d47059b7d9
DragonMoffon <happydog.aja@gmail.com>
2024-06-29 23:09:15 +1200

    Create constraint grips for the Camera

--- arcade/camera/grips/__init__.py
+++ arcade/camera/grips/__init__.py
@@ -5,6 +5,14 @@ from arcade.camera.grips.rotate import (
     rotate_around_right,
 )
 from arcade.camera.grips.screen_shake_2d import ScreenShake2D
+from arcade.camera.grips.constrain import (
+    constrain_x, constrain_y, constrain_z,
+    constrain_xy, constrain_yz, constrain_xz,
+    constrain_xyz,
+    constrain_boundary_x, constrain_boundary_y, constrain_boundary_z,
+    constrain_boundary_xy, constrain_boundary_yz, constrain_boundary_xz,
+    constrain_boundary_xyz
+)
 
 
 __all__ = (
@@ -13,4 +21,18 @@ __all__ = (
     "rotate_around_up",
     "rotate_around_forward",
     "ScreenShake2D",
+    "constrain_x",
+    "constrain_y",
+    "constrain_z",
+    "constrain_xy",
+    "constrain_yz",
+    "constrain_xz",
+    "constrain_xyz",
+    "constrain_boundary_x",
+    "constrain_boundary_y",
+    "constrain_boundary_z",
+    "constrain_boundary_xy",
+    "constrain_boundary_yz",
+    "constrain_boundary_xz",
+    "constrain_boundary_xyz"
 )
