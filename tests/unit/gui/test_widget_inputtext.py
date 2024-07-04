0f0d28ba387792c4149974c3b73d4e3be32c76bf
Maic Siemering <maic@siemering.tech>
2024-07-04 17:14:24 +0200

    feat(gui): add activate/deactivate to UIInputText (#2196)
    * feat(gui): add activate/deactivate to UIInputText
    * fix(gui): remove start_render calls

--- tests/unit/gui/test_widget_inputtext.py
+++ tests/unit/gui/test_widget_inputtext.py
@@ -1,9 +1,59 @@
 from arcade.gui import UIInputText
 
 
-def test_uilabel_support_multiline(uimanager):
-    # WHEN
+def test_deactivated_by_default(uimanager):
+    # GIVEN
     widget = UIInputText()
 
     # THEN
-    assert widget is not None
+    assert widget.active is False
+
+
+def test_activated_after_click(uimanager):
+    # GIVEN
+    widget = UIInputText()
+    uimanager.add(widget)
+
+    # WHEN
+    uimanager.click(*widget.rect.center)
+
+    # THEN
+    assert widget.active is True
+
+
+def test_deactivated_after_off_click(uimanager):
+    # GIVEN
+    widget = UIInputText()
+    uimanager.add(widget)
+    widget.activate()
+
+    # WHEN
+    uimanager.click(200, 200)
+
+    # THEN
+    assert widget.active is False
+
+
+def test_captures_text_when_active(uimanager):
+    # GIVEN
+    widget = UIInputText()
+    uimanager.add(widget)
+    widget.activate()
+
+    # WHEN
+    uimanager.type_text("Hello")
+
+    # THEN
+    assert widget.text == "Hello"
+
+
+def test_does_not_capture_text_when_inactive(uimanager):
+    # GIVEN
+    widget = UIInputText()
+    uimanager.add(widget)
+
+    # WHEN
+    uimanager.type_text("Hello")
+
+    # THEN
+    assert widget.text == ""
