234d207dcf09ba81105c76acf38ce611712558f9
pushfoo <36696816+pushfoo@users.noreply.github.com>
2024-07-08 11:55:05 -0400

    Make tests more specific

--- tests/unit/text/test_text_error_handling.py
+++ tests/unit/text/test_text_error_handling.py
@@ -5,7 +5,7 @@ import arcade
 
 def test_text_instance_raise_multiline_error(window):
     with pytest.raises(ValueError) as e:
-        _ = arcade.Text("Initial text", 0, 0, multiline=True)
+        _ = arcade.Text("Initial text", 0, 0, width=0, multiline=True)
 
     assert e.value.args[0] == "The 'width' parameter must be set to a non-zero value when 'multiline' is True, but got 0."
 
@@ -17,7 +17,7 @@ def test_text_instance_raise_multiline_error(window):
 
 def test_text_function_raise_multiline_error(window):
     with pytest.raises(ValueError) as e:
-        _ = arcade.draw_text("Initial text", 0, 0, multiline=True)
+        _ = arcade.draw_text("Initial text", 0, 0, width=0, multiline=True)
 
     assert e.value.args[0] == "The 'width' parameter must be set to a non-zero value when 'multiline' is True, but got 0."
 
