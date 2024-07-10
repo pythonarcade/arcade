62ade79ee8420c53b09a690de49d3ff8c6e34641
Einar Forselv <eforselv@gmail.com>
2024-07-10 23:13:04 +0200

    Remove font loading on arcade init

--- arcade/examples/gui_widgets.py
+++ arcade/examples/gui_widgets.py
@@ -10,6 +10,9 @@ import arcade.gui.widgets.buttons
 import arcade.gui.widgets.layout
 import arcade.gui.widgets.text
 
+# Load fonts bumbled with arcade such as the Kenney fonts
+arcade.resources.load_system_fonts()
+
 
 class MyView(arcade.View):
     def __init__(self):
