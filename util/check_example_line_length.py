c74826d402dd59b7f1e48d458b80d2560cae8ac0
Einar Forselv <eforselv@gmail.com>
2024-07-10 09:04:28 +0200

    Document some scripts in util/

--- util/check_example_line_length.py
+++ util/check_example_line_length.py
@@ -1,41 +1,43 @@
-import os
-import glob
-import shutil
-import re
+"""
+Utility for finding long lines in example.
 
+This causes issues when displaying in docs because the code will
+visually be cut off on the right side.
+"""
+import re
 from pathlib import Path
 
+EXAMPLE_ROOT = Path(__file__).resolve().parent.parent / "arcade" / "examples"
 
-def search_files(file_pattern, reg_pattern):
+
+def search_files(path: Path, reg_pattern):
     # Validate HTML files
-    html_files = glob.glob(file_pattern, recursive=True)
+    paths = path.glob("**/*.py") 
 
     regex = re.compile(reg_pattern)
     grand_total = 0
     file_count = 0
-    for file_name in html_files:
+
+    for path in paths:
         file_count += 1
         line_no = 0
-        with open(file_name, encoding="utf8") as f:
+        with open(path, encoding="utf8") as f:
             for line in f:
                 line_no += 1
                 result = regex.search(line)
                 if result:
-                    print(f"  {file_name}:{line_no}: " + line.strip())
+                    print(f"  {path.relative_to(EXAMPLE_ROOT)}:{line_no}: " + line.strip())
                     grand_total += 1
+
     print(f"{grand_total} across {file_count} files.")
     return grand_total
 
 
-def generic_test(title, file_pattern, text_pattern):
-    print()
-    print(title)
-    search_files(file_pattern, text_pattern)
-
-
 def main():
-    # Remove old directory
-    generic_test("LINE_LENGTH", "../arcade/examples/**/*.py", "^.{115}.*$")
+    # Look for lines in examples with line length > 115
+    print()
+    print("LINE_LENGTH")
+    search_files(path=EXAMPLE_ROOT, reg_pattern="^.{115}.*$")
 
 
 main()
