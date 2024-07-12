b97e17f3deb91198bbed27855b3c0c3ab713c02a
ryyst <ryyst@tuta.io>
2024-07-12 19:57:27 +0300

    Docs: Redesign the top-left logo area
    New theme now uses much bigger logo, stacked in a column with the
    headers.

--- doc/conf.py
+++ doc/conf.py
@@ -161,7 +161,7 @@ html_css_files = [
 ]
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/favicon-32x32.png'
+html_logo = '_static/android-chrome-192x192.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
