ab6d88d87705b954ce658764ac6f7d4bc21920ee
ryyst <ryyst@tuta.io>
2024-07-16 22:19:53 +0300

    Docs: Move all new color & layout style changes into their own files
    Very rudimentary and basic namespacing for better maintainability.

--- doc/conf.py
+++ doc/conf.py
@@ -298,7 +298,11 @@ class Visitor(docutils.nodes.SparseNodeVisitor):
 
 
 def setup(app):
+    # Separate stylesheets loosely by category.
+    app.add_css_file("css/colors.css")
+    app.add_css_file("css/layout.css")
     app.add_css_file("css/custom.css")
+
     app.add_js_file("js/custom.js")
 
     # IMPORTANT: We can't use app.add_autodocumenter!
