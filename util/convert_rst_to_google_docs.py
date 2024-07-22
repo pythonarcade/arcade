46bb3fd05cad6e3c3e5455ef2669bab4fb82ba41
pushfoo <36696816+pushfoo@users.noreply.github.com>
2024-07-21 20:50:14 -0400

    Make converter script friendlier via typer
    * Use typer for convenience
    
    * Make the converter script use targetable arguments instead of globbing in Python
    
    * Restructure to be a bit friendlier + annotated
    
    * Clean up imports

--- util/convert_rst_to_google_docs.py
+++ util/convert_rst_to_google_docs.py
@@ -3,9 +3,14 @@ Attempt to convert the docstrings in the Arcade codebase from RST to Google Docs
 
 https://github.com/pythonarcade/arcade/issues/1797
 """
+from __future__ import annotations
 
-import glob
 import re
+from dataclasses import dataclass
+from pathlib import Path
+
+import typer
+from typing_extensions import Annotated
 
 fn_signature_w_docstring_regexp = re.compile(
     r"""
@@ -29,6 +34,7 @@ fn_signature_w_docstring_regexp = re.compile(
     )
     """,
     re.X | re.DOTALL)
+
 docstring_rtype_regexp = re.compile(
     r"""
         \ +
@@ -36,35 +42,70 @@ docstring_rtype_regexp = re.compile(
     """,
     re.X | re.DOTALL)
 
-count = 0
-for file in [*glob.glob('arcade/*.py'), *glob.glob('arcade/**/*.py')]:
-    with open(file, "r") as f:
-        content = f.read()
-    pos = 0
-    while True:
-        match = fn_signature_w_docstring_regexp.search(content, pos=pos)
-        if match:
-            offset = 0
-            match2 = docstring_rtype_regexp.search(match.group('docstring'))
-            if match2:
-                # print(match.groupdict() | match2.groupdict())
-                # print(match.group('fn_signature') + match.group('docstring'))
-                count += 1
-                if match2:
-                    # Remove rtype annotation from docstring
-                    range = match.start('docstring') + match2.start(), match.start('docstring') + match2.end()
-                    offset -= range[1] - range[0]
-                    content = content[:range[0]] + content[range[1]:]
-                if match2 and not match.group('rtype_anno'):
-                    print(file, match.group('fn_name'))
-                    insert = f" -> {match2.group('rtype')}"
-                    pos = match.end('fn_signature_before')
-                    content = content[:pos] + insert + content[pos:]
-                    offset += len(insert)
+app = typer.Typer()
 
-            pos = match.end() + offset
-        else:
-            break
-    with open(file, "w") as f:
-        f.write(content)
-print(count)
+
+@dataclass
+class ReplacementContext:
+    num_files_updated: int = 0
+    num_rtypes_removed: int = 0
+    fix: bool = False
+
+    def process_file(self, path: str | Path) -> None:
+        resolved = Path(path).expanduser().resolve()
+        content = resolved.read_text()
+        edited = False
+
+        file_position = 0  # Index in file
+
+        while True:
+            match = fn_signature_w_docstring_regexp.search(content, pos=file_position)
+            if match:
+                offset = 0
+                rtype_match = docstring_rtype_regexp.search(match.group('docstring'))
+                if rtype_match:
+                    # print(match.groupdict() | rtype_match.groupdict())
+                    # print(match.group('fn_signature') + match.group('docstring'))
+                    if rtype_match:
+                        # Remove rtype annotation from docstring
+                        range = match.start('docstring') + rtype_match.start(), match.start('docstring') + rtype_match.end()
+                        offset -= range[1] - range[0]
+                        # TODO: optimize if needed
+                        content = content[:range[0]] + content[range[1]:]
+                        edited = True
+                    if rtype_match and not match.group('rtype_anno'):
+                        print(str(resolved), ":", match.group('fn_name'))
+                        insert = f" -> {rtype_match.group('rtype')}"
+                        file_position = match.end('fn_signature_before')
+                        content = content[:file_position] + insert + content[file_position:]
+                        offset += len(insert)
+
+                    self.num_rtypes_removed += 1
+                file_position = match.end() + offset
+            else:
+                break
+        if self.fix:
+            resolved.write_text(content)
+        self.num_files_updated += int(edited)
+
+
+@app.command()
+def main(
+        paths: Annotated[list[Path], typer.Argument(help='A file or files to process.')],
+        fix: Annotated[bool, typer.Option(help="Whether to apply fixes or merely check.")] = False
+):
+    context = ReplacementContext(fix=fix)
+    for path in paths:
+       context.process_file(path)
+
+    if fix:
+        prefix = "Wrote"
+    else:
+        prefix = "Detected"
+
+    print(f"{prefix} {context.num_files_updated=}")
+    print(f"{prefix} {context.num_rtypes_removed=}")
+
+
+if __name__ == "__main__":
+    app()
