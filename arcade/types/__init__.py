ef870eae8e0b6528632db9fb915e4b0ab87d0111
Einar Forselv <eforselv@gmail.com>
2024-07-24 00:27:07 +0200

    Add missing docstrings (#2306)
    * data_types
    
    * static
    
    * sprites
    
    * sprites
    
    * tilemap

--- arcade/types/__init__.py
+++ arcade/types/__init__.py
@@ -221,7 +221,13 @@ PathOrTexture = PathOr["Texture"]
 
 
 class TiledObject(NamedTuple):
+    """Object in a tilemaps"""
+
     shape: Union[Point, PointList, tuple[int, int, int, int]]
+    """Shape of the object"""
     properties: Properties | None = None
+    """Properties of the object"""
     name: str | None = None
+    """Name of the object"""
     type: str | None = None
+    """Type of the object"""
