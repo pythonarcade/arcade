b46dadc3ee5042e5aac62e8abf81713aa241252f
DigiDuncan <digiduncan@gmail.com>
2024-09-15 01:14:16 -0400

    add __all__ to InputManger

--- arcade/future/input/__init__.py
+++ arcade/future/input/__init__.py
@@ -4,3 +4,17 @@
 from .inputs import ControllerAxes, ControllerButtons, Keys, MouseAxes, MouseButtons
 from .manager import ActionState, InputManager
 from .input_mapping import Action, ActionMapping, Axis, AxisMapping
+
+__all__ = [
+    "ControllerAxes",
+    "ControllerButtons",
+    "Keys",
+    "MouseAxes",
+    "MouseButtons",
+    "ActionState",
+    "InputManager",
+    "Action",
+    "ActionMapping",
+    "Axis",
+    "AxisMapping"
+]
