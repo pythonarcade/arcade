a144c7ffa4421578a0ee995af30b9b78d1360c8d
DigiDuncan <digiduncan@gmail.com>
2024-10-27 21:10:16 -0400

    fix test

--- tests/unit/color/test_module_color.py
+++ tests/unit/color/test_module_color.py
@@ -3,4 +3,4 @@ def test_colors():
     from arcade import color
     names = color.__dict__.keys()
     # number of colors + 1 real import + 1 annotations
-    assert 1013 + 1 + 1 == len(names)
+    assert 1016 + 1 + 1 == len(names)
