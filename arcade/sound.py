5ec096b0a89b715799a89daf5b1a5defab756454
Einar Forselv <eforselv@gmail.com>
2025-01-12 03:34:00 +0100

    Use pyglet.options.audio directly

--- arcade/sound.py
+++ arcade/sound.py
@@ -13,11 +13,9 @@ from pyglet.media import Source
 from arcade.resources import resolve
 
 if os.environ.get("ARCADE_SOUND_BACKENDS"):
-    pyglet.options["audio"] = tuple(  # type: ignore
-        v.strip() for v in os.environ["ARCADE_SOUND_BACKENDS"].split(",")
-    )
+    pyglet.options.audio = tuple(v.strip() for v in os.environ["ARCADE_SOUND_BACKENDS"].split(","))
 else:
-    pyglet.options["audio"] = ("openal", "xaudio2", "directsound", "pulse", "silent")  # type: ignore
+    pyglet.options.audio = ("openal", "xaudio2", "directsound", "pulse", "silent")
 
 import pyglet.media as media
 
