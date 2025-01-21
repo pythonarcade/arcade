5b3f64d4cb71bd3feacb70f2f9d2f193de9affd3
pushfoo <36696816+pushfoo@users.noreply.github.com>
2025-01-21 04:01:05 -0500

    Add unknown file handling and clean up more

--- util/create_resources_listing.py
+++ util/create_resources_listing.py
@@ -15,7 +15,10 @@ from collections import defaultdict
 from collections.abc import Mapping
 from dataclasses import dataclass, fields, field
 from functools import lru_cache, cache
+from io import StringIO
+from itertools import chain, cycle
 from pathlib import Path
+from tokenize import String
 from typing import List, Callable, Protocol, Sequence, Iterable, Iterator, Any
 import logging
 
@@ -457,31 +460,65 @@ SUFFIX_TO_VIDEO_TYPE = {
     '.avi': 'avi'
 }
 
-
-@lru_cache(maxsize=None)
-def _css_classes(css_classes: tuple[str, ...]) -> str:
-    return " ".join(css_classes)
-
-
-def css_classes(css_classes: Iterable[str]) -> str:
-    return _css_classes(tuple(css_classes))
+def smash_iterable(i: str | Iterable[str]):
+    if isinstance(i, str):
+        return i
+    else:
+        return ' '.join(i)
 
 
-def code_literal(inner: str, classes: Sequence = ('literal',)) -> str:
-    classes_str = css_classes(classes)
-    #return f"<code class=\"{classes_str}\">{inner}</code>"
-    return f"<code class=\"{classes_str}\">{inner}</code>"
+_sphinx_option_handlers: dict[str, Callable] = defaultdict(lambda: str)
+_sphinx_option_handlers.update({
+    'class': smash_iterable,
+    'widths': smash_iterable
+})
+
+
+def sphinx_directive(
+        name: str,
+        *arguments: str,
+        options: Mapping[str, str | int | Iterable] | None = None,
+        body: str | Iterable | None = None
+) -> str:
+    lines = [f".. {name}:: {' '.join(arguments)}\n"]
+
+    if options:
+        for name, value in options.items():
+            converter = _sphinx_option_handlers[name]
+            lines.append(
+                f"   :{name}: {converter(value)}\n")
+        lines.append("\n")
+    if body:
+        if isinstance(body, str):
+            body = (body,)
+        # We could use extend but this is nice for debugging
+        for i, value in enumerate(body):
+            lines.append(indent("   ", value))
+        lines.append("\n\n")
+
+    return ''.join(lines)
+
+
+def code_block(
+        inner: str,
+        language: str | None = None,
+        options: Mapping[str, str | int | Iterable] | None = None
+) -> str:
+    return sphinx_directive(
+        "code-block", language if language else None, "\n",
+        options=options,
+        body=inner
+    )
+
 
 def quote(s: str) -> str:
     return f"&quot;{s}&quot;"
 
-def code_str(inner: str, classes: Sequence = ('literal', 'arcade-ez-copy')) -> str:
-    return code_literal(quote(inner), classes=classes)
 
+def highlight_copyable(out, inner: str) -> None:
+    out.write(f".. code-block:: python\n\n")
+    out.write(f"   {inner!r}\n\n", "")
 
-def highlight_copyable(out, inner: str, indent: str ="        ") -> None:
-    out.write(textwrap.indent(f".. code-block:: python\n\n", indent))
-    out.write(textwrap.indent(f"   {inner!r}\n\n", ""))
 
 # Regex because it's easier to make complicated
 BRITTLE_CAP_WORD_REGEX = re.compile(r"[A-Z][a-z0-9]*")
@@ -498,6 +535,21 @@ BRITTLE_FONT_NAME_REGEX = re.compile(
     """, re.X)
 
 
+def indent(  # pending: post-3.0 refactor  # why would indent come after the text?!
+        spacing: str,
+        to_indent: str,
+        as_row: bool = False
+) -> str:
+    """Readable ergonomics for text wrapping."""
+    if not as_row:
+        return textwrap.indent(to_indent, spacing)
+    raw = StringIO(to_indent)
+    new = StringIO()
+    it = chain((spacing,), cycle((' ' * len(spacing),)))
+    for prefix, line in zip(it, raw.readlines()):
+        new.write(textwrap.indent(line, prefix))
+
+    return new.getvalue()
 
 def html_copyable(
         name: str,
@@ -515,43 +567,6 @@ def html_copyable(
     if indent:
         return textwrap.indent(raw, indent)
     return raw
-
-
-def smash_iterable(i: str | Iterable[str]):
-    if isinstance(i, str):
-        return i
-    else:
-        return ' '.join(i)
-
-
-_sphinx_option_handlers: dict[str, Callable] = defaultdict(lambda: str)
-_sphinx_option_handlers.update({
-    'class': smash_iterable,
-    'widths': smash_iterable
-})
-
-def sphinx_directive(
-        name: str,
-        *arguments: str,
-        options: Mapping[str, str | int | Iterable] | None = None,
-        body: str | Iterable | None = None
-) -> str:
-    lines = [f".. {name}:: {' '.join(arguments)}"]
-
-    for name, value in options.items():
-        converter = _sphinx_option_handlers[name]
-        lines.append(
-            f"   :{name}: {converter(value)}")
-    lines.append("\n")
-    if body:
-        if isinstance(body, str):
-            lines.append(body)
-        else:
-            # We could use extend but this is nice for debugging
-            for i, value in enumerate(body):
-                lines.append(f"   {value}")
-
-    return '\n'.join(lines)
 
 
 def process_resource_files(out, file_list: List[Path]):
@@ -577,11 +592,11 @@ def process_resource_files(out, file_list: List[Path]):
             start_row = " "
 
         resource_copyable = f"{create_resource_path(path)}"
-        code_html = code_str(resource_copyable)
+
 
         if suffix in [".png", ".jpg", ".gif", ".svg"]:
             out.write(f"    {start_row} - .. raw:: html\n\n")
-            out.write(html_copyable(path.name, resource_copyable, "             "))
+            out.write(indent("             ", html_copyable(path.name, resource_copyable)))
 
             tile_rst_code = sphinx_directive(
                 'image', f'../../{resource_path}',
@@ -592,10 +607,10 @@ def process_resource_files(out, file_list: List[Path]):
                     ),
                     # lazy helps avoid GitHub and readthedocs from 429ing us ("chill pls")
                     'loading': 'lazy',
-                    'name': resource_path
+                    'name': resource_copyable
                 }
             )
-            out.write(textwrap.indent(tile_rst_code, "        "))
+            out.write(indent("        ", tile_rst_code))
 
             size_info = None
             if suffix == ".svg":
@@ -611,25 +626,36 @@ def process_resource_files(out, file_list: List[Path]):
             if size_info is None:
                 size_info = "Could not read size info"
             out.write(f"        *({size_info})*\n")
-            out.write("\n")
+            out.write("\n\n")
 
         elif suffix in SUFFIX_TO_AUDIO_TYPE:
             file_path = FMT_URL_REF_EMBED.format(resource_path)
             src_type=SUFFIX_TO_AUDIO_TYPE[suffix]
             out.write(f"    {start_row} - .. raw:: html\n\n")
             out.write(html_copyable(path.name, resource_copyable, "             "))
-            out.write(f"            <audio class=\"resource-thumb\" controls><source src='{file_path}' type='audio/{src_type}'></audio>\n")
+
+            out.write(f"        .. raw:: html\n\n")
+            out.write(indent("              ",
+                      f"<audio class=\"resource-thumb\" controls>\n"
+                      f"  <source src='{file_path}' type='audio/{src_type}'>\n"
+                      f"</audio>\n\n"))
 
         elif suffix in SUFFIX_TO_VIDEO_TYPE:
             file_path = FMT_URL_REF_EMBED.format(resource_path)
             src_type = SUFFIX_TO_VIDEO_TYPE[suffix]
             out.write(f"    {start_row} - .. raw:: html\n\n")
-            out.write(html_copyable(path.name, resource_copyable, "             "))
-            out.write(f"            <video class=\"resource-thumb\" style=\"max-width: 100%\" controls><source src='{file_path}' type='video/{src_type}'></video>\n")
+            out.write(indent(
+                      f"             ", html_copyable(path.name, resource_copyable)))
+            out.write(f"        .. raw:: html\n\n")
+            out.write(indent(
+                      f"              ",
+                      f"<video class=\"resource-thumb\" controls>\n"
+                      f"  <source src='{file_path}' type='video/{src_type}'>\n"
+                      f"</video>\n\n"))
 
-        elif suffix == ".glsl":
-            file_path = FMT_URL_REF_PAGE.format(resource_path)
-            out.write(f"    {start_row} - `{code_html} <{file_path}>`_\n")
+        # elif suffix == ".glsl":
+        #     file_path = FMT_URL_REF_PAGE.format(resource_path)
+        #     out.write(f"    {start_row} - `{code_html} <{file_path}>`_\n")
         # Fonts
         elif suffix == ".ttf":
             # The worst code you've ever seen ; v ; 7  # pending: post-3.0 cleanup
@@ -648,13 +674,13 @@ def process_resource_files(out, file_list: List[Path]):
 
 
             style_string = ", ".join(styles or ("Regular",))
-            print("row: ", face_name, style_string, code_html)
+            # print("row: ", face_name, style_string, code_html)
 
             out.write(f"    {start_row} - .. code-block:: python\n\n")
             out.write(f"          {raw_name!r}\n\n")
 
             out.write(f"      - {style_string}\n\n")
-
+            # out.write(indent(f"        ", code_block(resource_copyable, language='python')))
             out.write(f"      - .. code-block:: python\n\n")
             out.write(f"          {resource_copyable!r}\n\n")
 
@@ -668,10 +694,18 @@ def process_resource_files(out, file_list: List[Path]):
 
             icon = "tiled_icon_digi_pls_replace.png"
             out.write(f"        .. image:: images/{icon}\n")
-            out.write(f"                     :class: resource-thumb\n\n")
+            out.write(f"           :class: resource-thumb\n\n")
 
         else:
-            out.write(f"    {start_row} - {code_html}\n")
+            out.write(f"    {start_row} - .. raw:: html\n\n")
+            out.write(indent("             ", html_copyable(path.name, resource_copyable)))
+            out.write(indent("             ",
+                # SVG styling and alignment seems odd, so we're doing this the flexbox way
+                f"<div class=\"resource-thumb file-icon unknown-file-type\">\n"
+                f"  <p>???</p>\n"
+                f"  <p>Unknown Filetype</p>\n"
+                f"</div>\n\n"
+            ))
         # The below doesn't work because of how raw HTML / Sphinx images interact:
         # out.write(f"            <br /><code class='literal'>{resource_copyable}</code>\n")
         cell_count += 1
@@ -690,6 +724,7 @@ def resources():
     do_heading(out, 0, "Built-In Resources")
 
     out.write("\n")
+    out.write("Linking test: :ref:`:resources:gui_basic_assets/window/panel_green.png:`.\n")
     out.write("Every file below is included when you :ref:`install Arcade <install>`. This includes the images,\n"
               "sounds, fonts, and other files to help you get started quickly. You can still download them\n"
               "separately, but Arcade's resource handle system will usually be easier.\n")
