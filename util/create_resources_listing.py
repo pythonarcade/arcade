595135b04d50171d515ab81188f63dd003ec4ec0
pushfoo <36696816+pushfoo@users.noreply.github.com>
2025-01-25 04:34:56 -0500

    Add anti-preload hints

--- util/create_resources_listing.py
+++ util/create_resources_listing.py
@@ -703,7 +703,8 @@ def process_resource_files(
 
             out.write(f"        .. raw:: html\n\n")
             out.write(indent("           ",
-                      f"<{kind} class=\"resource-thumb\" controls>\n"
+                      # Using preload="none" is gentler on GitHub and readthedocs
+                      f"<{kind} class=\"resource-thumb\" controls preload=\"none\">\n"
                       f"  <source src='{file_path}' type='{kind}/{mime_suffix}'>\n"
                       f"</{kind}>\n\n"))
 
