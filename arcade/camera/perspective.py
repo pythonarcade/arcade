78739d8f8c432c31aba1fb4ae6197777e96ce244
pushfoo <36696816+pushfoo@users.noreply.github.com>
2025-03-22 05:05:02 -0400

    Fix a loose reference to pyglet.math.Vec3 by prefixing the full path

--- arcade/camera/perspective.py
+++ arcade/camera/perspective.py
@@ -171,7 +171,7 @@ class PerspectiveProjector(Projector):
         """Convert world coordinates to pixel screen coordinates.
 
         If a 2D :py:class:`~pyglet.math.Vec2` is provided instead of a 3D
-        :py:class:`Vec3`, then one will be calculated to the best of the
+        :py:class:`~pyglet.math.Vec3`, then one will be calculated to the best of the
         method's ability.
 
         Args:
