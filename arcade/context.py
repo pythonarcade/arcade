90a203c8718c8cfaae03454ba83ca183e9d31feb
Einar Forselv <eforselv@gmail.com>
2025-06-07 22:11:30 +0200

    unbuffered ellipse shader: remove geometry shader

--- arcade/context.py
+++ arcade/context.py
@@ -159,7 +159,6 @@ class ArcadeContext(Context):
         self.shape_ellipse_filled_unbuffered_program: Program = self.load_program(
             vertex_shader=":system:shaders/shapes/ellipse/filled_unbuffered_vs.glsl",
             fragment_shader=":system:shaders/shapes/ellipse/filled_unbuffered_fs.glsl",
-            geometry_shader=":system:shaders/shapes/ellipse/filled_unbuffered_geo.glsl",
         )
         self.shape_ellipse_outline_unbuffered_program: Program = self.load_program(
             vertex_shader=":system:shaders/shapes/ellipse/outline_unbuffered_vs.glsl",
@@ -258,11 +257,8 @@ class ArcadeContext(Context):
             ],
             mode=self.TRIANGLE_STRIP,
         )
-        # ellipse/circle filled
-        self.shape_ellipse_unbuffered_buffer = self.buffer(reserve=8)
-        self.shape_ellipse_unbuffered_geometry: Geometry = self.geometry(
-            [BufferDescription(self.shape_ellipse_unbuffered_buffer, "2f", ["in_vert"])]
-        )
+        # ellipse/circle filled. Empty geometry. We generate it on the fly in the vertex shader.
+        self.shape_ellipse_unbuffered_geometry: Geometry = self.geometry()
         # ellipse/circle outline
         self.shape_ellipse_outline_unbuffered_buffer = self.buffer(reserve=8)
         self.shape_ellipse_outline_unbuffered_geometry: Geometry = self.geometry(
