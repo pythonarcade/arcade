9e6933d61725df2a1498292dad71f511ae979d2a
Darren Eberly <darren.eberly@proton.me>
2025-08-22 16:46:26 -0400

    Put any remaining geo shaders behind webgl check

--- arcade/context.py
+++ arcade/context.py
@@ -116,14 +116,18 @@ class ArcadeContext(Context):
         self.sprite_list_program_no_geo["index_data"] = 6
 
         # Geo shader single sprite program
-        self.sprite_program_single = self.load_program(
-            vertex_shader=":system:shaders/sprites/sprite_single_vs.glsl",
-            geometry_shader=":system:shaders/sprites/sprite_list_geometry_no_cull_geo.glsl",
-            fragment_shader=":system:shaders/sprites/sprite_list_geometry_fs.glsl",
-        )
-        self.sprite_program_single["sprite_texture"] = 0
-        self.sprite_program_single["uv_texture"] = 1
-        self.sprite_program_single["spritelist_color"] = 1.0, 1.0, 1.0, 1.0
+        if gl_api != "webgl":
+            self.sprite_program_single = self.load_program(
+                vertex_shader=":system:shaders/sprites/sprite_single_vs.glsl",
+                geometry_shader=":system:shaders/sprites/sprite_list_geometry_no_cull_geo.glsl",
+                fragment_shader=":system:shaders/sprites/sprite_list_geometry_fs.glsl",
+            )
+            self.sprite_program_single["sprite_texture"] = 0
+            self.sprite_program_single["uv_texture"] = 1
+            self.sprite_program_single["spritelist_color"] = 1.0, 1.0, 1.0, 1.0
+        else:
+            self.sprite_program_single = None  # type: ignore
+
         # Non-geometry shader single sprite program
         self.sprite_program_single_simple = self.load_program(
             vertex_shader=":system:shaders/sprites/sprite_single_simple_vs.glsl",
@@ -186,24 +190,30 @@ class ArcadeContext(Context):
         self.atlas_resize_program["texcoords_old"] = 2
         self.atlas_resize_program["texcoords_new"] = 3
 
-        # NOTE: These should not be created when WebGL is used
-        # SpriteList collision resources
-        # Buffer version of the collision detection program.
-        self.collision_detection_program = self.load_program(
-            vertex_shader=":system:shaders/collision/col_trans_vs.glsl",
-            geometry_shader=":system:shaders/collision/col_trans_gs.glsl",
-        )
-        # Texture version of the collision detection program.
-        self.collision_detection_program_simple = self.load_program(
-            vertex_shader=":system:shaders/collision/col_tex_trans_vs.glsl",
-            geometry_shader=":system:shaders/collision/col_tex_trans_gs.glsl",
-        )
-        self.collision_detection_program_simple["pos_angle_data"] = 0
-        self.collision_detection_program_simple["size_data"] = 1
-        self.collision_detection_program_simple["index_data"] = 2
+        if gl_api != "webgl":
+            # SpriteList collision resources
+            # Buffer version of the collision detection program.
+            self.collision_detection_program = self.load_program(
+                vertex_shader=":system:shaders/collision/col_trans_vs.glsl",
+                geometry_shader=":system:shaders/collision/col_trans_gs.glsl",
+            )
+            # Texture version of the collision detection program.
+            self.collision_detection_program_simple = self.load_program(
+                vertex_shader=":system:shaders/collision/col_tex_trans_vs.glsl",
+                geometry_shader=":system:shaders/collision/col_tex_trans_gs.glsl",
+            )
+            self.collision_detection_program_simple["pos_angle_data"] = 0
+            self.collision_detection_program_simple["size_data"] = 1
+            self.collision_detection_program_simple["index_data"] = 2
 
-        self.collision_buffer = self.buffer(reserve=1024 * 4)
-        self.collision_query = self.query(samples=False, time=False, primitives=True)
+            self.collision_buffer = self.buffer(reserve=1024 * 4)
+            self.collision_query = self.query(
+                samples=False, time=False, primitives=True
+            )
+        else:
+            self.collision_detection_program = None
+            self.collision_buffer = None
+            self.collision_query = None
 
         # General Utility
 
