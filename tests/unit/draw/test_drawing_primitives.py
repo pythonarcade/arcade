71b233beb25536693b39ffac222f832d21dec853
DragonMoffon <happydog.aja@gmail.com>
2025-12-26 16:09:21 +1300

    reset window viewport from drawing command

--- tests/unit/draw/test_drawing_primitives.py
+++ tests/unit/draw/test_drawing_primitives.py
@@ -6,6 +6,7 @@ def test_draw_primitives(window):
     Render the screen.
     """
     window.background_color = arcade.color.WHITE
+    window.viewport = (0, 0, window.width, window.height)
     window.clear()
 
     # Draw a grid
