55e21e7b694fa027ad030a4d61e596306288570c
Paul V Craven <paul.craven@optimizley.com>
2026-05-07 15:52:58 -0500

    Fix caret behavior on input field activation

--- arcade/gui/widgets/text.py
+++ arcade/gui/widgets/text.py
@@ -590,7 +590,11 @@ class UIInputText(UIStyledWidget[UIInputTextStyle], UIInteractiveWidget):
     def _on_active_changed(self):
         """Handle the active state change of the input
         text field to care about loosing active state."""
-        if not self._active:
+        if self._active:
+            self.trigger_full_render()
+            self.caret.on_activate()
+            self.caret.position = len(self.doc.text)
+        else:
             self.deactivate()
 
     def _apply_style(self):
